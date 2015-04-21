6b2b39c777720b67b2e4a8d5d8b5c316f6e2ab6c
tuuli <tuuli.pollanen@gmail.com>
2015-04-21 11:15:20 +0200

    renamed the action page model to reflect the name change to campaign page

--- app/models/action_page.rb
+++ app/models/action_page.rb
@@ -1,5 +1,5 @@
-class ActionPage < ActiveRecord::Base
-  attr_accessor :action_page_id, :title, :slug, :active, :featured
+class CampaignPage < ActiveRecord::Base
+  attr_accessor :campaign_page_id, :title, :slug, :active, :featured
 
   belongs_to :language
   belongs_to :campaign
