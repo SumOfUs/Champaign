10de09fe70286867a1bd7dcec54b45c1f40171e7
tuuli <tuuli.pollanen@gmail.com>
2015-04-22 14:35:11 +0200

    fixed email_address

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,5 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :email_address, :actionkit_member_id
   # validates that e-mail is valid
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_format_of :email_address, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
