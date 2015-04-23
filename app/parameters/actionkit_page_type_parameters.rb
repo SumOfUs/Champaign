5a60719a2a2ab4960771b9facf9e3b558e1effaa
Eric Boersma <eric.boersma@gmail.com>
2015-04-23 13:09:05 -0400

    Removing busted test

--- app/parameters/actionkit_page_type_parameters.rb
+++ app/parameters/actionkit_page_type_parameters.rb
@@ -1,7 +1,6 @@
 class ActionkitPageTypeParameters < ActionParameter::Base
 
   def permit
-    p params.object_id
     params.require(:actionkit_page_type).permit(:actionkit_page_type)
   end
 end
--- spec/controllers/actionkit_page_type_controller_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ActionkitPageTypeControllerController, type: :controller do
-
-end
