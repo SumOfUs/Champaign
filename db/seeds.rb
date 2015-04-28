c9c703469bb079fe32bfc53abf1f533588cc6238
tuuli <tuuli.pollanen@gmail.com>
2015-04-28 17:52:08 +0200

    Adding seeds.rb that has mostly been used for testing DB inserts at this point - it doesn't contain any useful preliminary data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,40 +10,44 @@
   actionkit_page_type = ActionkitPageType.create!({actionkit_page_type: 'petition'})
 
 # 2. make languages
-  language = Languages.create!({language_code: 'EN/US', language_name: 'American English'})
+  language = Language.create!({language_code: 'EN/US', language_name: 'American English'})
 
 # 3  make campaigns
-  campaign = Campaigns.create!({campaign_name: 'Test campaigns'})
+  campaign = Campaign.create!({campaign_name: 'Test campaigns'})
 
 # 4. make widget types
 
-  widget_type = WidgetTypes.create!({
-      widget_name: 'test_widget',
-      specifications: {
-          width: 'integer',
-          height: 'integer'
-      },
-      partial_path: '/test_widget',
-      form_partial_path: '/form/test_widget',
-      action_table_name: 'test_widget_results',
-      active: false
-    })
+  widget_type = WidgetType.create!({
+    widget_name: 'test_widget',
+    specifications: {
+        width: 'integer',
+        height: 'integer'
+    },
+    partial_path: '/test_widget',
+    form_partial_path: '/form/test_widget',
+    action_table_name: 'test_widget_results',
+    active: false
+  })
 
 # 5. make a campaign page
-  campaign_page = CampaignPages.create!({
-      language_id: language.id,
-      actionkit_page_id: actionkit_page.id,
-      title: 'Test campaign page',
-      slug: 'test_campaign_page',
-      active: false,
-      featured: false
-    })
 
-# 6. make campaign pages widgets
-  campaign_page_widget = CampaignPagesWidgets.create!()
-# 7. make actionkit pages
+  campaign_page = CampaignPage.create!({
+    language_id: (Language.find_by language_code: 'EN/US').id,
+    title: 'Test page',
+    slug: 'test_name',
+    active: false,
+    featured: false
+  })
+
+# 6. create a widget for the campaign page
+
+  campaign_page_widget = campaign_page.campaign_pages_widget.create!({
+    widget_type_id: (WidgetType.find_by widget_name: "test_widget").id,
+    content: {"text_body":"Sign this petition to save the jumping spiders!"},
+    page_display_order: 1,
+  })
+
+# 7. create an actionkit page match for the campaign page widget
+ campaign_page_widget.create_actionkit_page({actionkit_id: 123, actionkit_page_type_id: (ActionkitPageType.find_by actionkit_page_type: 'petition').id})
 
 # 8. make members 
-
-actionkit_page_type = ActionkitPageType.create(actionkit_page_type: "petition")
-actionkit_page = ActionkitPage.create({campaign_page_id: 1234567890, actionkit_page_type_id: 2468})
