ec9d6955b8dfcdd9a28febb90931fa51298ecb01
tuuli <tuuli.pollanen@gmail.com>
2015-05-04 14:52:28 +0200

    Remove migration to delete path fields that were deleted from the original migrations. Only new migrations from now on.

--- db/migrate/20150429140640_remove_path_fields_from_widget_types.rb
+++
@@ -1,6 +0,0 @@
-class RemovePathFieldsFromWidgetTypes < ActiveRecord::Migration
-  def change
-    remove_column :widget_types, :partial_path
-    remove_column :widget_types, :form_partial_path
-  end
-end
