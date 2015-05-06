91752ab9fc5668370f5b63749232cc4e3c834a2b
Eric Boersma <eric.boersma@gmail.com>
2015-05-06 11:08:11 -0400

    I can display a page using widgets!

--- app/controllers/campaign_pages_controller.rb
+++ app/controllers/campaign_pages_controller.rb
@@ -2,12 +2,11 @@ class CampaignPagesController < ApplicationController
 
   def new
     @campaign_page = CampaignPage.new
-    @templates = Template.where :active => true
-
+    @templates = Template.where active: true
+    # @widget_types = template.widget_types
   end
 
   def create
-
     permitted_params = CampaignPageParameters.new(params).permit
     if permitted_params[:slug].nil?
       permitted_params[:slug] = permitted_params[:title].parameterize
@@ -16,18 +15,12 @@ class CampaignPagesController < ApplicationController
     permitted_params[:featured] = false
     permitted_params[:language_id] = 1
     page = CampaignPage.create! permitted_params
-    template = Template.find params[:template]
-    widgets = template.widget_types
+    widgets = params[:widgets]
     i = 0
-    widgets.each do |widget|
-      page.campaign_pages_widget.create!(widget_type_id: widget.id,
-                                         content: {
-                                          "Json" => "object", 
-                                          "gets" => "populated", 
-                                          "from" => "page_form.",
-                                          "This field needs" => "to be unique",
-                                          "so here's a UUID" => SecureRandom.uuid
-                                          },
+    widgets.each do |widget_type_name, widget_data|
+      widget_type_id = widget_data.delete('widget_type')
+      page.campaign_pages_widget.create!(widget_type_id: widget_type_id,
+                                         content: widget_data,
                                          page_display_order: i)
       i += 1
     end
