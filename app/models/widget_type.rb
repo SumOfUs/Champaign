70da3be57554d2019160b4f11423186c14a6f09f
Eric Boersma <eric.boersma@gmail.com>
2015-05-08 11:20:32 -0400

    Adding comments to widget type model to explain partial paths

--- app/models/widget_type.rb
+++ app/models/widget_type.rb
@@ -6,10 +6,17 @@ class WidgetType < ActiveRecord::Base
 
   has_and_belongs_to_many :templates
 
+  # Specifies where this widget type's form partial is stored in the project.
+  # Used with a render call to specify where the file should be loaded
+  # Forms are intended to be used for editing or creating widgets.
   def form_partial_path
     "widgets/#{self.widget_name.parameterize.underscore}/form.slim"
   end
 
+  # Specifies where this widget type's display partial is stored in the project.
+  # Used with a render call to specify where the file should be loaded
+  # Display partials are intented to be used for displaying the content stored in
+  # a widget.
   def display_partial_path
     "widgets/#{self.widget_name.parameterize.underscore}/display.slim"
   end
