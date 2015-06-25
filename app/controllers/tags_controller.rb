078c4f61ea6f6155484e91130ee5253ed1e1b46a
Eric Boersma <eric.boersma@gmail.com>
2015-06-25 10:15:03 -0400

    Removing Tags Controller because it wound up being unnecessary

--- app/controllers/tags_controller.rb
+++
@@ -1,7 +0,0 @@
-class TagsController < ActionController::Base
-  def search
-    search_term = params[:search]
-    possible_tags = Tag.where('tag_name ILIKE :query', query: "%#{search_term}%").limit(5)
-    render json: possible_tags
-  end
-end
