35fa62859ade3156373487a89048a652093e4a44
tuuli <tuuli.pollanen@gmail.com>
2015-06-29 12:23:02 -0700

    Fixed event handling in the image cropper so that it works after loading a new template.

--- app/assets/javascripts/image_cropper.js
+++ app/assets/javascripts/image_cropper.js
@@ -26,8 +26,19 @@ create_cropper_parameters = function(ratio_width, ratio_height, image_id) {
     return cropper_parameters;
 }
 
+replace_image = function(new_src) {
+    $("#image_preview").html($("<img>", {
+        id: "image", 
+        class: "img-responsive center-block",
+        alt: "Enter an image URL to begin cropping!",
+        src: new_src
+    }));
+    handle_image_load();
+}
+
 handle_image_load = function() {
-    $("#image").load(function() {
+    $("#image").on('load', function() {
+        // the image ratio is 14x10
         image_cropper_parameters = create_cropper_parameters(14, 10, 'image');
         image_cropper_parameters.update = function (coordinates) {
             var cropper_width = $(".cropper").width()
@@ -40,21 +51,23 @@ handle_image_load = function() {
         }
         //Load new cropper frame on top of the new image
         new Cropper($(this).get(0), image_cropper_parameters);
-    }); 
+    })
 }
 
-replace_image = function(new_src) {
-    $("#image_preview").html($("<img>", {
-        id: "image", 
-        class: "img-responsive center-block",
-        alt: "Enter an image URL to begin cropping!",
-        src: new_src
-    }));
-    //prepare a cropper frame on top of the image when it loads
-    handle_image_load();
+attach_image_handlers = function() {
+    //get file data when the user uploads an image with the upload button
+    $('#widgets_image_image_upload').change(function(){
+        read_url(this);
+    });
+
+    //loads the image into image preview when image_url is changed:
+    $('#widgets_image_image_url').change(function() {
+        //append new image with src from the url to image preview (date is appended to deal with caching)
+        replace_image($('#widgets_image_image_url').val()+'?'+new Date().getTime())
+    });
 }
 
-function read_url(input) {
+read_url = function(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = (function(theFile) {
@@ -67,18 +80,15 @@ function read_url(input) {
     }
 }
 
-$(document).ready(function (){
-    //lay out initial cropper on top of the image loaded by default
+// attaches handlers after ajax request for changing template changes
+$(document).ajaxComplete(function(){
     handle_image_load();
+    attach_image_handlers();
+})
 
-    //bind logic into image upload button
-    $("#widgets_image_image_upload").change(function(){
-        read_url(this);
-    });
+//attaches handlers for loading the initial/default template
+$('#image_widget_form').ready(function (){
+    handle_image_load();
+    attach_image_handlers();
 
-    //loads the image into image preview when image_url is changed:
-    $("#widgets_image_image_url").change(function() {
-        //append new image to image preview
-        replace_image($("#widgets_image_image_url").val()+'?'+new Date().getTime())
-    });
 })
