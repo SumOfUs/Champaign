d60cb9ce5af19dc34278a3ad04b5741e2e366576
Neal Donnelly <NealJMD@gmail.com>
2015-07-02 17:14:02 -0600

    fix problems with PetitionWidget spec

--- spec/models/petition_widget_spec.rb
+++ spec/models/petition_widget_spec.rb
@@ -25,31 +25,49 @@ RSpec.describe PetitionWidget, type: :model do
   subject { widget }
   it { should be_valid }
 
+  describe 'inheritance' do
+
+    it 'should be able to create a widget from the Widget class' do
+      w2 = Widget.new(params.merge({type: "PetitionWidget"}))
+      expect(w2).to be_valid
+      expect(w2.content['form_button_text']).to eq content[:form_button_text]
+    end
+
+  end
+
   describe :content do
 
+    it "should have the initialized values" do
+      expect(widget.content['form_button_text']).to eq content[:form_button_text]
+    end
+
     it "should be invalid without a required field" do
-      widget.content[:petition_text] = nil
+
+      widget.content.delete('petition_text')
+      # widget.content['petition_text'] = nil
+      expect(widget.content['petition_text']).to be_nil
       expect(widget).not_to be_valid
     end
 
     it "should be invalid with petition_text too short" do
-      widget.content[:petition_text] = "meh"
+      widget.content['petition_text'] = "meh"
       expect(widget).not_to be_valid
     end
 
     it "should be valid changin a non-required field" do
-      widget.content[:form_button_text] = "Go!"
+      widget.content['form_button_text'] = "Go!"
       expect(widget).to be_valid
     end
 
     it "should be valid without a non-required field" do
-      widget.content.delete(:form_button_text)
+      widget.content.delete('form_button_text')
+      # widget.content['form_button_text'] = nil
+      expect(widget.content['form_button_text']).to be_nil
       expect(widget).to be_valid
-      expect(widget.content[:form_button_text]).to be_nil
     end
 
     it "should enforce string types" do
-      widget.content[:petition_text] = 123
+      widget.content['petition_text'] = 123
       expect(widget).not_to be_valid
     end
   end
