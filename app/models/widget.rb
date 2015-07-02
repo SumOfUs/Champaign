5c76dde893005cf232432770a4488283f74a94ec
Neal Donnelly <NealJMD@gmail.com>
2015-07-02 15:37:41 -0600

    add simple spec for widget base class

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -2,7 +2,10 @@ class Widget < ActiveRecord::Base
 
   belongs_to :campaign_page, inverse_of: :campaign_pages_widgets
 
-  validates_presence_of :content, :page_display_order, :campaign_page_id, :widget_type_id
-  validates_uniqueness_of  :page_display_order, :scope => :campaign_page_id
+  validates :page_display_order, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  # validates_uniqueness_of  :page_display_order, :scope => :campaign_page_id
+
+  types = %w(TextWidget PetitionWidget)
+  validates :type, presence: true, inclusion: types
 
 end
