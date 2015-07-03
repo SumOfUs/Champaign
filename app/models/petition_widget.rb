ad09a56eb51ea042e1f652d391b7ae368f4d92d1
Neal Donnelly <NealJMD@gmail.com>
2015-07-03 13:53:34 -0600

    respect 'required' designation but allow keys outside schema

--- app/models/petition_widget.rb
+++ app/models/petition_widget.rb
@@ -1,4 +1,4 @@
 class PetitionWidget < Widget
   JSON_SCHEMA = Rails.root.join('db','json','petition_widget.json_schema').to_s
-  validates :content, presence: true, json: { schema: JSON_SCHEMA, options: { strict: true } }
+  validates :content, presence: true, json: { schema: JSON_SCHEMA }
 end
