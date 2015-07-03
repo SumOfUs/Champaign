37b19ff2554cd74aaf37516137379bb8bb71f4be
Neal Donnelly <NealJMD@gmail.com>
2015-07-02 18:21:08 -0600

    added basic spec for campaign page with nested widget creation

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,6 +1,6 @@
 class Widget < ActiveRecord::Base
 
-  belongs_to :campaign_page, inverse_of: :campaign_pages_widgets
+  belongs_to :campaign_page
 
   validates :page_display_order, presence: true, numericality: { only_integer: true, greater_than: 0 }
   # validates_uniqueness_of  :page_display_order, :scope => :campaign_page_id
