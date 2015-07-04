fed8329034dea5486584d8225d773782fcb7035e
Neal Donnelly <NealJMD@gmail.com>
2015-07-03 18:30:20 -0600

    change CampaignPagesController#create to pass tests, scrap its tags/campaigns/json processing logic

--- spec/controllers/campaign_pages_controller_spec.rb
+++ spec/controllers/campaign_pages_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe CampaignPagesController, type: :controller do
   let(:text_widget_params_2) { attributes_for :text_widget }
   # let(:bad_widget_params) { attributes_for :text_widget, content: {} }
   let(:widget_params) { [petition_widget_params, text_widget_params_1, text_widget_params_2] }
-  let(:page_params) { attributes_for :widgetless_page, language: english }
+  let(:page_params) { attributes_for :widgetless_page, language_id: english.id }
   let(:page_widget_params) { page_params.merge({widgets_attributes: widget_params}) }
   # let(:simple_page) { CampaignPage.new(page_params) }
   # let(:existing_page) { p = CampaignPage.new(page_widget_params); p.save!; p }
@@ -24,18 +24,18 @@ RSpec.describe CampaignPagesController, type: :controller do
     describe 'create' do
 
       it 'should be able to create a page without widgets' do
-        expect{ post :create, page_params }.to change{ CampaignPage.count }.by 1
-        expect(response).to be_successful
+        expect{ post :create, campaign_page: page_params }.to change{ CampaignPage.count }.by 1
+        expect(response).to redirect_to(CampaignPage.last)
       end
 
       it 'should be able to create a page with widgets' do
-        expect{ post :create, page_widget_params }.to change{ CampaignPage.count }.by 1
-        expect(response).to be_successful
+        expect{ post :create, campaign_page: page_widget_params }.to change{ CampaignPage.count }.by 1
+        expect(response).to redirect_to(CampaignPage.last)
       end
 
       it 'should be able to create widgets with a page' do
-        expect{ post :create, page_widget_params }.to change{ CampaignPage.count }.by 3
-        expect(response).to be_successful
+        expect{ post :create, campaign_page: page_widget_params }.to change{ Widget.count }.by 3
+        expect(response).to redirect_to(CampaignPage.last)
       end
     end
   end
