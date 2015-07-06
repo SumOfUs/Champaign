f6762e3510c300a639e88c1bc8f395de5bc26340
Omar Sahyoun <osahyoun@gmail.com>
2015-07-06 13:47:27 +0100

    Refacotring controller.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -10,36 +10,46 @@ class CampaignsController < ApplicationController
   end
 
   def create
-    permitted_params = CampaignParameters.new(params).permit
     @campaign = Campaign.create permitted_params
     redirect_to :campaigns, notice: 'Campaign Created'
   end
 
   def show
     @campaign = Campaign.find params['id']
-    if @campaign.active == false
+
+    unless @campaign.active?
       raise ActionController::RoutingError.new('The campaign you requested has been deactivated.')
-    end 
-    @templates = Template.where active: true
+    end
+    @templates = ActiveQuery.new(Template)
   end
 
   def edit
-    @campaign = Campaign.find params['id']
+    find_campaign
   end
 
   def update
-    @campaign = Campaign.find params['id']
-    permitted_params = CampaignParameters.new(params).permit
+    find_campaign
     @campaign.update permitted_params
     redirect_to :campaigns, notice: 'Campaign Updated'
   end
 
   def destroy
-    @campaign = Campaign.find params['id']
+    find_campaign
+
     # deactivates campaign pages associated with that campaign
     @campaign.campaign_page.update_all(:active =>false)
     # deactivates the campaign itself
     @campaign.update(:active => false)
     redirect_to :campaigns, notice: "The campaign '" + @campaign.campaign_name + "' was deactivated. Contact an admin if you'd like to reactivate it."
+  end
+
+  private
+
+  def permitted_params
+    CampaignParameters.new(params).permit
+  end
+
+  def find_campaign
+    @campaign = Campaign.find params['id']
   end
 end
