4d00be72d432e2bbe0a14fd55b75400d161eac21
Neal Donnelly <NealJMD@gmail.com>
2015-07-08 17:58:37 -0600

    move CampaignsPageController spec to unit tests over integration tests

--- spec/controllers/campaign_pages_controller_spec.rb
+++ spec/controllers/campaign_pages_controller_spec.rb
@@ -1,72 +1,68 @@
 describe CampaignPagesController do
 
-  let(:english) { create :language }
-  let( :admin ) { create :admin }
-  let(:petition_widget_params) { attributes_for :petition_widget }
-  let(:text_widget_params_1) { attributes_for :text_widget }
-  let(:text_widget_params_2) { attributes_for :text_widget }
-  let(:widget_params) { [petition_widget_params, text_widget_params_1, text_widget_params_2] }
-  let(:page_params) { attributes_for :widgetless_page, language_id: english.id }
-  let(:page_widget_params) { page_params.merge({widgets_attributes: widget_params}) }
+  let(:user) { double(:user) }
+  let(:campaign_page) { instance_double('CampaignPage', active?: true, featured?: true) }
 
-  # for error cases
-  let(:bad_widget_params) { attributes_for :text_widget, content: { body_html: nil } }
-  let(:bad_page_params) { attributes_for :widgetless_page, language_id: nil }
-  let(:bad_widget_page_params) { page_params.merge({widgets_attributes: bad_widget_params}) }
-  let(:widget_bad_page_params) { bad_page_params.merge({widgets_attributes: widget_params}) }
+  context 'logged in' do
 
-  # let(:existing_page) { p = CampaignPage.new(page_widget_params); p.save!; p }
-
-  describe "logged in as admin" do
-
-    before :each do
-      expect(admin).to be_persisted
-      allow(controller).to receive(:current_user) { admin }
+    before do
+      pending("making collections and redirect work with doubles")
+      allow(request.env['warden']).to receive(:authenticate!) { user }
+      allow(controller).to receive(:current_user) { user }
     end
 
-    describe 'create' do
+    describe 'GET index' do
 
-      describe 'success' do
-
-        after :each do
-          expect(response).to redirect_to CampaignPage.last
-        end
-
-        it 'should be able to create a page without widgets' do
-          expect{ post :create, campaign_page: page_params }.to change{ CampaignPage.count }.by 1
-        end
-
-        it 'should be able to create a page with widgets' do
-          expect{ post :create, campaign_page: page_widget_params }.to change{ CampaignPage.count }.by 1
-        end
-
-        it 'should be able to create widgets with a page' do
-          expect{ post :create, campaign_page: page_widget_params }.to change{ Widget.count }.by 3
-        end
+      before :each do
+        allow(CampaignPage).to receive(:where).and_return([campaign_page])
       end
 
-      describe 'failure' do
+      it 'gets active campaigns' do
+        expect(CampaignPage).to receive(:where).with({active: true})
+        get :index
+      end
 
-        after :each do
-          expect(response).to render_template :new
+      it 'renders index' do
+        get :index
+        expect(response).to render_template('index')
+      end
+
+      it 'assigns @campaign_pages' do
+        get :index
+        expect(assigns(:campaign_pages)).to eq([campaign_page])
+      end
+    end
+
+    describe "POST create" do
+      let(:fake_params) { { 'name' => 'Foo'} }
+
+      before do
+        allow(CampaignPageParameters).to receive_message_chain('new.permit'){ fake_params }
+        allow(CampaignPage).to receive(:new) { campaign_page }
+        allow(campaign_page).to receive(:save) { true }
+        post :create, campaign_page: fake_params
+      end
+
+      it 'filters params' do
+        expect(CampaignPageParameters).to have_received(:new).with(hash_including({ 'campaign_page' => fake_params}))
+      end
+
+      it 'creates new campaign' do
+        expect(CampaignPage).to have_received(:new).with(fake_params)
+      end
+
+      it 'assigns @campaign' do
+        expect(assigns(:campaign_page)).to eq(campaign)
+      end
+
+      describe 'with errors' do
+
+        before do
+          allow(campaign_page).to receive(:save).and_return(false)
         end
 
-        it 'should not create a page if page errors' do
-          expect{ post :create, campaign_page: widget_bad_page_params }.to change{ CampaignPage.count }.by 0
-        end
-
-        it 'should not create a page if widget errors' do
-          expect{ post :create, campaign_page: widget_bad_page_params }.to change{ CampaignPage.count }.by 0
-        end
-
-        it 'should not create widgets if page errors' do
-          pending('moving forward')
-          expect{ post :create, campaign_page: bad_widget_page_params }.to change{ Widget.count }.by 0
-        end
-
-        it 'should not create widgets if widget errors' do
-          pending('moving forward')
-          expect{ post :create, campaign_page: bad_widget_page_params }.to change{ Widget.count }.by 0
+        it 'renders new' do
+          expect(response).to render_template(:new)
         end
       end
     end
