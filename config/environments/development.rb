486dea2c2e48558054457fe45d998dbe5f1e3b86
tuuli <tuuli.pollanen@gmail.com>
2015-07-10 12:41:53 -0700

    Added nested model directory for widget models.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # sets location of ImageMagick for Paperclip. Get it by the terminal command 'which convert'.
+  Paperclip.options[:command_path] = '/usr/bin/'
 end
