648437ebbd2f88ac6e2c3c4ee7d2f2d67871be34
Omar Sahyoun <osahyoun@gmail.com>
2015-07-10 19:52:14 +0100

    Handle whitelisting with oauth

--- app/service_objects/connect_with_oauth_provider.rb
+++ app/service_objects/connect_with_oauth_provider.rb
@@ -1,5 +1,7 @@
-class ConnectWithOauthProvider
+# Raise if user's email domain is not whitelisted
+class Champaign::NotWhitelisted < StandardError; end;
 
+class ConnectWithOauthProvider
   def self.connect(data)
     new(data).connect
   end
@@ -9,6 +11,8 @@ class ConnectWithOauthProvider
   end
 
   def connect
+    raise Champaign::NotWhitelisted unless whitelisted
+
     return connected_user if user_already_connected
 
     return updated_disconnected_user if user_exists_but_disconnected
@@ -17,6 +21,14 @@ class ConnectWithOauthProvider
   end
 
   private
+
+  def whitelisted
+    return true if whitelist.empty?
+
+    whitelist.select do |list_domain|
+      email_domain == list_domain
+    end.any?
+  end
 
   def connected_user
     @connected_user ||= User.find_by(provider: @resp.provider, uid: @resp.uid)
@@ -38,6 +50,14 @@ class ConnectWithOauthProvider
       uid: @resp.uid,
       password: Devise.friendly_token[0, 20]
     )
+  end
+
+  def email_domain
+    @resp.info.email.split('@').last
+  end
+
+  def whitelist
+    ChampaignConfig.oauth_domain_whitelist
   end
 
   alias_method :user_exists_but_disconnected, :disconnected_user
