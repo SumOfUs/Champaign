e2aa7d1ec3179a824bf2e863265f7b5274f86e5b
Neal Donnelly <NealJMD@gmail.com>
2015-07-13 18:22:17 -0600

    delete deprecated code

--- app/parameters/campaign_page_parameters.rb
+++ app/parameters/campaign_page_parameters.rb
@@ -44,32 +44,4 @@ class CampaignPageParameters < ActionParameter::Base
     return permitted
   end
 
-  ##### DEPRECATED, to be moved to Widget class to do validation
-
-  def widget_params
-    allowed_keys = []
-    schemas.each do |schema|
-      schema['properties'].each_pair do |field_name, field_properties|
-        allowed_keys << strong_params_representation(field_name, field_properties)
-      end
-    end
-    return [{:content => allowed_keys}, :id, :type, :page_display_order]
-  end
-
-  def strong_params_representation(field_name, properties)
-    if not properties.has_key? 'type'
-      field_name.to_sym
-    elsif properties['type'] == "dictionary"
-      {field_name.to_sym => {}}
-    elsif properties['type'] == "array"
-      {field_name.to_sym => []}
-    else
-      field_name.to_sym
-    end
-  end
-
-  def schemas
-    Dir[Rails.root.join('db','json','*.json_schema')].map{ |f| JSON.parse File.read(f) }
-  end
-
 end
