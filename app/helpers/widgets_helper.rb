e8c78fce29cf3739bc16c12ad263786c0f1d2e00
Neal Donnelly <NealJMD@gmail.com>
2015-07-15 17:54:34 -0600

    added fields for thermometer widget

--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -1,7 +1,10 @@
 module WidgetsHelper
 
-  def try_content(widget, field)
+  def try_content(widget, field, is_boolean=false)
     if widget.present? and widget.content.present? and widget.content.has_key? field.to_s
+      if is_boolean
+        if widget.content[field.to_s] == "0" then return false else return true end
+      end
       return widget.content[field.to_s]
     else
       return ''
