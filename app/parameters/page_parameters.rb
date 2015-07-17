c71843b744dd4e6d84441162c523e2a847d237e7
Neal Donnelly <NealJMD@gmail.com>
2015-07-17 11:02:55 -0600

    change strong params to pass fields for widget setters

--- app/parameters/page_parameters.rb
+++ app/parameters/page_parameters.rb
@@ -2,10 +2,19 @@ class PageParameters < ActionParameter::Base
 
   private
 
+  def permit(fields, root_symbol)
+    format_widget_attributes(params, root_symbol)
+    nested = save_nested(params, root_symbol)
+    permitted = strip_nested(params, root_symbol).require(root_symbol).permit(
+      fields,
+      widgets_attributes: widget_shallow_fields
+    )
+    return restore_nested(permitted, nested)
+  end
+
   # In order to pass strong params inspection with nested hashes with
   # unknown keys, we strip that part out and replace it after inspection
-
-  def format_widget_attributes params, root_symbol
+  def format_widget_attributes(params, root_symbol)
     if params[root_symbol][:widgets_attributes].present?
       if params[root_symbol][:widgets_attributes].respond_to? :values
         params[root_symbol][:widgets_attributes] = params[root_symbol][:widgets_attributes].values
@@ -13,24 +22,37 @@ class PageParameters < ActionParameter::Base
     end
   end
 
-  def strip_json params, root_symbol
+  def widget_shallow_fields
+    [:id, :type, :_destroy, :page_display_order, :goal, :count, :autoincrement]
+  end
+
+  def widget_nesting_fields
+    [:checkboxes, :select_box, :content]
+  end
+
+  def strip_nested(params, root_symbol)
     return params unless params[root_symbol][:widgets_attributes].present?
-    params[root_symbol][:widgets_attributes].each{ |a| a.delete(:content) }
+    params[root_symbol][:widgets_attributes].each do |a|
+      widget_nesting_fields.each {|f| a.delete(f) }
+    end
     return params
   end
 
-  def save_json params, root_symbol
+  def save_nested(params, root_symbol)
     return nil unless params[root_symbol][:widgets_attributes].present?
-    ret = params[root_symbol][:widgets_attributes].map do |a|
-      if a.has_key? :content then a[:content] else nil end
+    return params[root_symbol][:widgets_attributes].map do |a|
+      backup = {}
+      widget_nesting_fields.each do |key|
+        if a.has_key? key then backup[key] = a[key] end
+      end
+      backup
     end
-    return ret
   end
 
-  def restore_json permitted, saved
+  def restore_nested(permitted, saved)
     return permitted unless saved.present?
     permitted[:widgets_attributes].each_with_index do |a, ii|
-      a[:content] = saved[ii] unless saved[ii].nil?
+      saved[ii].each_pair{ |key, backup| a[key] = backup }
     end
     return permitted
   end
