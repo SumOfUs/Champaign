e11faf96ac8c420b61b8145ce38b063f38a06aa0
Neal Donnelly <NealJMD@gmail.com>
2015-07-17 14:32:43 -0600

    make PageParameters support all parameters actually used my widgets

--- app/parameters/page_parameters.rb
+++ app/parameters/page_parameters.rb
@@ -23,11 +23,28 @@ class PageParameters < ActionParameter::Base
   end
 
   def widget_shallow_fields
-    [:id, :type, :_destroy, :page_display_order, :goal, :count, :autoincrement]
+    [
+      :goal, :count, :autoincrement,              # thermometer widget
+      :html,                                      # raw html widget
+      :text_body_html,                            # text body widget
+      :petition_text, :form_button_text, :require_full_name,                 # petition widget
+      :require_email_address, :require_country, :require_state,              # petition widget
+      :require_postal_code, :require_address, :require_city, :require_phone, # petition widget
+
+      :id, :type, :_destroy, :page_display_order # base widget fields
+    ]
   end
 
   def widget_nesting_fields
-    [:checkboxes, :select_box, :content]
+    [
+      :content,
+      :checkboxes,
+      :select_box,
+      :comment_textarea,
+      :call_in_form,
+      :letter_sent_form,
+      :image_attributes
+    ]
   end
 
   def strip_nested(params, root_symbol)
