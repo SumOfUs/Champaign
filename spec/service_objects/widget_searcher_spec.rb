8d6869b7dbbd0fbbeee113d21e02880c558130e0
tuuli <tuuli.pollanen@gmail.com>
2015-07-23 12:01:38 -0700

    Modified search spec per Omar's suggestions.

--- spec/service_objects/widget_searcher_spec.rb
+++ spec/service_objects/widget_searcher_spec.rb
@@ -12,27 +12,27 @@ describe 'Page search' do
   let(:widget_searcher) { WidgetSearcher.new(params) }
   let(:test_text) { 'test' }
 
-  context 'Full Text Search' do
-    it 'should search with params and return the found page' do
+  context 'search by text content' do
+    it 'searches with params and returns the matching page' do
       expect(widget_searcher.search).to eq([page])
     end
-    it 'should search the text of a widget and return the found page' do
+    it 'searches the text of a widget and returns the found page' do
       expect(widget_searcher.get_matches_by_text_widget(test_text)).to eq([page])
     end
   end
 
-  context 'Tag search' do
+  context 'search by tag' do
     let(:tag_name) { 'A testing tag' }
     let(:tag) { build(:tag, tag_name: 'A testing tag', actionkit_uri: '/foo/bar') }
 
-    it 'should be able to search for a page based on the tags on that page' do
+    it 'searches for a page based on the tags on that page' do
       page.tags = [tag]
       expect(widget_searcher.tag_search(tag_name: tag_name)).to eq([page])
     end
-    it 'should return an empty array when searching for a page without the expected tags' do
+    it 'returns an empty collection when no page with the existing tags exists' do
       page.tags = [tag]
       expect(widget_searcher.tag_search(tag_name: 'a bad tag name')).to eq([])
     end
   end
-  
+
 end
