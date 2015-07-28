2a20c3f15d7379d573191abddab96393d149dd5b
Neal Donnelly <NealJMD@gmail.com>
2015-07-28 16:34:22 -0600

    unify widget json controllers and allow update

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -8,7 +8,25 @@ class WidgetsController < ApplicationController
     render json: { status: 'success' }
   end
 
+  def create
+    widget = Widget.new(widget_params)
+    widget.page = page
+    widget.page_display_order = page.widgets.count + 1
+    widget.save
+    render json: widget.content.merge({id: widget.id, type: widget.type})
+  end
+
+  def update
+    widget.update_attributes(widget_params)
+    render json: widgets
+  end
+
+
   private
+
+  def widget_params
+    WidgetParameters.new(params).permit
+  end
 
   def widgets
     page.widgets.map do |widget|
