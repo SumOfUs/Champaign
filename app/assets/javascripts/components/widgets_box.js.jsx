a8f3af8f286c2a1945fb95b90406fed4f9dcb3ed
Neal Donnelly <NealJMD@gmail.com>
2015-07-28 17:57:45 -0600

    add raw html widget react

--- app/assets/javascripts/components/widgets_box.js.jsx
+++ app/assets/javascripts/components/widgets_box.js.jsx
@@ -41,6 +41,8 @@ var Widgets = React.createClass({
       switch (widget.type) {
         case "TextBodyWidget":
           return (<TextBodyWidget {...widget} onWidgetSubmit={this.props.onWidgetSubmit}></TextBodyWidget>)
+        case "RawHtmlWidget":
+          return (<RawHtmlWidget {...widget} onWidgetSubmit={this.props.onWidgetSubmit}></RawHtmlWidget>)
         default:
           break;
       }
@@ -193,6 +195,73 @@ var TextBodyWidgetForm = React.createClass({
           <div className="form-group">
             <label htmlFor="">Text</label>
             <textarea className='form-control' ref='body' defaultValue={this.props.text_body_html}></textarea>
+          </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+        </form>
+      </div>
+    )
+  }
+})
+
+var RawHtmlWidget = React.createClass({
+
+  propTypes: {
+    html:             React.PropTypes.string.isRequired,
+    onWidgetSubmit:   React.PropTypes.func.isRequired,
+    campaign_page_id: React.PropTypes.number.isRequired,
+    id:               React.PropTypes.number.isRequired
+  },
+
+  form() {
+    return (
+      <div className='widget-edit'>
+        <RawHtmlWidgetForm {...this.props}>
+        </RawHtmlWidgetForm>
+      </div>
+    )
+  },
+
+  display() {
+    return (
+      <div className='widget-show'>
+        {this.props.html}
+      </div>
+    )
+  },
+
+  render() {
+    return (
+      <div className="text-body-widget">
+        <Widget {...this.props} form={this.form} display={this.display}>
+        </Widget>
+      </div>
+    )
+  }
+})
+
+var RawHtmlWidgetForm = React.createClass({
+
+  propTypes: {
+    html:             React.PropTypes.string.isRequired,
+    onWidgetSubmit:   React.PropTypes.func.isRequired,
+    campaign_page_id: React.PropTypes.number.isRequired,
+    id:               React.PropTypes.number.isRequired
+  },
+
+  handleSubmit(e) {
+    e.preventDefault()
+    var text = React.findDOMNode(this.refs.body).value
+    var data = { widget: {html: text, text: text, type: 'RawHtmlWidget', campaign_page_id: this.props.campaign_page_id, id: this.props.id } }
+    this.props.onWidgetSubmit(data);
+  },
+
+  render() {
+    return (
+      <div className='widget-html-form'>
+         <form onSubmit={ this.handleSubmit }>
+          <div className="form-group">
+            <label htmlFor="">Text</label>
+            <textarea className='form-control' ref='body' defaultValue={this.props.html}></textarea>
           </div>
           <button type="submit" className="btn btn-default">Submit</button>
         </form>
