0eca8b7cce8641077f5f3e2554a0884a58669e2c
Neal Donnelly <NealJMD@gmail.com>
2015-07-28 15:46:20 -0600

    create strong params for widgets

--- app/parameters/page_parameters.rb
+++ app/parameters/page_parameters.rb
@@ -1,4 +1,4 @@
-class PageParameters < ActionParameter::Base
+class PageParameters < WidgetParameters
 
   private
 
@@ -20,30 +20,6 @@ class PageParameters < ActionParameter::Base
         params[root_symbol][:widgets_attributes] = params[root_symbol][:widgets_attributes].values
       end
     end
-  end
-
-  def widget_shallow_fields
-    [
-      :goal, :count, :autoincrement,              # thermometer widget
-      :html,                                      # raw html widget
-      :text_body_html,                            # text body widget
-      :petition_text, :form_button_text, :require_full_name,                 # petition widget
-      :require_email_address, :require_country, :require_state,              # petition widget
-      :require_postal_code, :require_address, :require_city, :require_phone, # petition widget
-
-      :id, :type, :_destroy, :page_display_order # base widget fields
-    ]
-  end
-
-  def widget_nesting_fields
-    [
-      :checkboxes,
-      :select_box,
-      :comment_textarea,
-      :call_in_form,
-      :letter_sent_form,
-      :image_attributes
-    ]
   end
 
   def strip_nested(params, root_symbol)
