1a900786d4e3ed36b185ec3a0ea4c7d714b1e4d4
tuuli <tuuli.pollanen@gmail.com>
2015-07-28 16:24:04 -0700

    Make pretty json with as_json instead of to_json

--- app/controllers/campaign_pages_controller.rb
+++ app/controllers/campaign_pages_controller.rb
@@ -22,7 +22,7 @@ class CampaignPagesController < ApplicationController
     @campaign_page = CampaignPage.new(@page_params)
     @campaign_page.compile_html
     if @campaign_page.save
-      SqsPusher.push(@campaign_page.to_json)
+      SqsPusher.push(@campaign_page.as_json)
       redirect_to @campaign_page, notice: 'Campaign page created!'
 
     else
