3b03e97ee59ffb949be38a0af15461f1fdc4f5e8
Neal Donnelly <NealJMD@gmail.com>
2015-07-28 18:11:31 -0600

    break up monolithic widgets .jsx file

---
+++ app/assets/javascripts/components/widgets/text_body_widget.js.jsx
@@ -0,0 +1,66 @@
+var TextBodyWidget = React.createClass({
+
+  propTypes: {
+    text_body_html:   React.PropTypes.string.isRequired,
+    onWidgetSubmit:   React.PropTypes.func.isRequired,
+    campaign_page_id: React.PropTypes.number.isRequired,
+    id:               React.PropTypes.number.isRequired
+  },
+
+  form() {
+    return (
+      <div className='widget-edit'>
+        <TextBodyWidgetForm {...this.props}>
+        </TextBodyWidgetForm>
+      </div>
+    )
+  },
+
+  display() {
+    return (
+      <div className='widget-show'>
+        {this.props.text_body_html}
+      </div>
+    )
+  },
+
+  render() {
+    return (
+      <div className="text-body-widget">
+        <Widget {...this.props} form={this.form} display={this.display}>
+        </Widget>
+      </div>
+    )
+  }
+})
+
+var TextBodyWidgetForm = React.createClass({
+
+  propTypes: {
+    text_body_html:   React.PropTypes.string.isRequired,
+    onWidgetSubmit:   React.PropTypes.func.isRequired,
+    campaign_page_id: React.PropTypes.number.isRequired,
+    id:               React.PropTypes.number.isRequired
+  },
+
+  handleSubmit(e) {
+    e.preventDefault()
+    var text = React.findDOMNode(this.refs.body).value
+    var data = { widget: {text_body_html: text, text: text, type: 'TextBodyWidget', campaign_page_id: this.props.campaign_page_id, id: this.props.id } }
+    this.props.onWidgetSubmit(data);
+  },
+
+  render() {
+    return (
+      <div className='widget-html-form'>
+         <form onSubmit={ this.handleSubmit }>
+          <div className="form-group">
+            <label htmlFor="">Text</label>
+            <textarea className='form-control' ref='body' defaultValue={this.props.text_body_html}></textarea>
+          </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+        </form>
+      </div>
+    )
+  }
+})
