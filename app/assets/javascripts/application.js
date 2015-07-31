287acf1674aed5f97e8e336c5f80a39c17986b2a
Omar Sahyoun <osahyoun@gmail.com>
2015-07-31 09:50:20 +0100

    Ditch turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require react
 //= require react_ujs
 //= require fluxxor
