9d345331fc28ae03a093926fa7181f09b7d88281
Omar Sahyoun <osahyoun@gmail.com>
2015-07-31 16:12:20 +0100

    First Jest test

---
+++ app/assets/javascripts/components/hello.js.jsx
@@ -0,0 +1,12 @@
+React = require('react');
+
+var Hello = React.createClass({
+  render: function(){
+    return(
+      <div>Hello, {this.props.name}!</div>
+    )
+  }
+});
+
+
+module.exports = Hello;
