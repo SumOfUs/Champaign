381293ea6bfaf8d9c6dc9ff81d31668ba34aa49d
Neal Donnelly <NealJMD@gmail.com>
2015-07-31 18:38:48 -0600

    build super basic store infrastructure for campaign page

--- app/assets/javascripts/flux/main.js
+++ app/assets/javascripts/flux/main.js
@@ -1,6 +1,8 @@
 var Fluxxor      = require('fluxxor');
 var WidgetStore  = require('flux/widget_store');
 var WidgetClient = require('flux/widget_client');
+var CampaignPageStore  = require('flux/campaign_page_store');
+var CampaignPageClient = require('flux/campaign_page_client');
 
 var actions = {
   loadWidgets: function(){
@@ -26,11 +28,18 @@ var actions = {
     WidgetClient.destroy(id, function(resp) {
       this.dispatch(WidgetStore.events.DESTROY_WIDGET, id);
     }.bind(this));
+  },
+
+  updateCampaignPage: function(data) {
+    CampaignPageClient.update(data, function(resp) {
+      this.dispatch(CampaignPageStore.events.UPDATE_CAMPAIGN_PAGE, data);
+    }.bind(this));
   }
 };
 
 var stores = {
-  WidgetStore: new WidgetStore.store()
+  WidgetStore: new WidgetStore.store(),
+  CampaignPageStore: new CampaignPageStore.store()
 };
 
 var flux = new Fluxxor.Flux(stores, actions);
