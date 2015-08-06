09403d9c84cddaf9cc9523882cbcdd3dd81f5db1
tuuli <tuuli.pollanen@gmail.com>
2015-08-06 12:03:27 -0700

    Add basic class for building objects to pass for the message queue for storing user actions.

--- app/services/ak_user_params.rb
+++ app/services/ak_user_params.rb
@@ -1,12 +1,15 @@
 require 'browser'
 
 class AkUserParams
-  def self.create(params)
-  # I'm assuming that the form params will come in with names that correspond to the AK API:
+  def self.create(params, browser)
+  # I'm assuming that the form params will come with a field called signature
+  # that will contain the petition form data with names that correspond to the AK API:
+    # USER:
     # akid
     # email
+    # prefix
+    # suffix
     # name
-      # name gets split to prefix, first name, middle name, last name and suffix automagically by AK ...
     # address1
     # address2
     # city
@@ -21,13 +24,36 @@ class AkUserParams
     # plus4
     # lang - get from current page session
     # source - fwd, fb, tw, pr, mtl, taf
-    @user_params = params[:signature]
-    @user_params[:user_agent] = browser.user_agent
-    @user_params[:browser_detected] = browser.known?
-    @user_params[:mobile] = browser.mobile?
-    @user_params[:tablet] = browser.tablet?
-    @user_params[:platform] = browser.platform
 
+  # PETITION ACTION:
+    # action_ptr - Action
+    # created_at - DateTimeField
+    # created_user - BooleanField
+    # id - AutoField
+    # ip_address - CharField
+    # is_forwarded - BooleanField
+    # link - IntegerField
+    # mailing - Mailing
+    # opq_id - CharField
+    # page - Page
+    # referring_mailing - Mailing
+    # referring_user - User
+    # source - CharField
+    # status - CharField
+    # subscribed_user - BooleanField
+    # taf_emails_sent - IntegerField
+    # targeted - Target (ManyToManyField)
+    # updated_at - DateTimeField
+    # user - User
+    @user_params = params[:signature]
+    @user_params.merge({
+                           user_agent: browser.user_agent,
+                           browser_detected: browser.known?,
+                           mobile: browser.mobile?,
+                           tablet: browser.tablet?,
+                           platform: browser.platform
+
+                       })
     @user_params
   end
 end
