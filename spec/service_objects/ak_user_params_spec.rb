80e895d845ce68d3b59354b83a3f0c229e43db83
tuuli <tuuli.pollanen@gmail.com>
2015-08-06 15:17:50 -0700

    Clean up test for petition signature object and add a test for pushing user action data onto SQS into campaign pages controller spec.

--- spec/service_objects/ak_user_params_spec.rb
+++ spec/service_objects/ak_user_params_spec.rb
@@ -1,33 +1,19 @@
 describe AkUserParams do
 
-  let(:params) {{
-    signature: {
-      name: Faker::Name.name,
-      email: Faker::Internet.email,
-      state: Faker::Address.state,
-      country: Faker::Address.country,
-      postal: Faker::Address.postcode,
-      address: Faker::Address.street_address,
-      state: Faker::Address.state,
-      city: Faker::Address.city,
-      phone: Faker::PhoneNumber.phone_number,
-      zip: Faker::Address.zip,
-      region: Faker::Config.locale,
-      lang: 'En'
-    }
-  }}
+  let(:params) { build(:petition_signature_params ) }
   let(:browser) { Browser.new }
   let(:expected_object) {
     (params[:signature].clone).merge({
-                                         user_agent: "",
-                                         browser_detected: false,
-                                         mobile: false,
-                                         tablet: false,
-                                         platform: :other
+                                         user_agent: browser.user_agent,
+                                         browser_detected: browser.known?,
+                                         mobile: browser.mobile?,
+                                         tablet: browser.tablet?,
+                                         platform: browser.platform
                                      })
   }
 
   it 'Builds an object containing data about the user and the action' do
-    expect(AkUserParams.create(params,browser)).to eq(expected_object)
+    expect(AkUserParams.create(params, browser)).to eq(expected_object)
   end
+
 end
