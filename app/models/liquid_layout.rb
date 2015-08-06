aa3f82ea50a686d9518e198950ae9695d5616530
Neal Donnelly <NealJMD@gmail.com>
2015-08-06 16:20:40 -0600

    make liquid layouts count and validate slots (needs spec)

--- app/models/liquid_layout.rb
+++ app/models/liquid_layout.rb
@@ -1,4 +1,27 @@
 class LiquidLayout < ActiveRecord::Base
 
   has_many :campaign_pages
+
+  validate :ordered_slots
+  before_validation :count_slots
+
+  private
+
+  # needs a spec
+  def slot_ids
+    content.scan(/\{\{ *slot([0-9]+) *\}\}/).map(&:first).map(&:to_i)
+  end
+
+  def count_slots
+    self.slot_count = slot_ids.size
+  end
+
+  # needs a spec
+  def ordered_slots
+    slots = self.slot_count
+    if slot_ids.sort != (1..slots).to_a
+      errors.add(:content, "There are #{slots} slot tags, but they do not match the numbers 1-#{slots}")
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806211203) do
+ActiveRecord::Schema.define(version: 20150806221035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150806211203) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "slot_count"
   end
 
   create_table "members", force: :cascade do |t|
