25b288bcf5b341574437ac3ac3da175b899c9c53
Neal Donnelly <NealJMD@gmail.com>
2015-08-09 16:34:08 -0600

    finish numbering and labeling for tags, with nice spec

--- app/models/liquid_layout.rb
+++ app/models/liquid_layout.rb
@@ -2,6 +2,9 @@ class LiquidLayout < ActiveRecord::Base
 
   has_many :campaign_pages
 
+  validates :content, presence: true, allow_blank: false
+  validates :title, presence: true, allow_blank: false
+
   validate :ordered_slots
   before_validation :count_slots
 
@@ -9,12 +12,21 @@ class LiquidLayout < ActiveRecord::Base
     "#{title} - #{slot_count} slots"
   end
 
-  private
-
-  # needs a spec
-  def slot_ids
-    content.scan(/\{\{ *slot([0-9]+) *\}\}/).map(&:first).map(&:to_i)
+  def slot_regex
+    # see the spec to better understand what this matches
+    /\{\{ *slot *([0-9]+) *\}\}\s*(<!-- *(.*?) *-->){,1}/
   end
+
+  def slot_ids
+    return [] unless content.present?
+    return content.scan(slot_regex).map{ |captured| captured.first.to_i }
+  end
+
+  def slot_labels
+    return [] unless content.present?
+    return content.scan(slot_regex).map{ |captured| captured[2] || "" }
+  end
+
 
   def count_slots
     self.slot_count = slot_ids.size
--- spec/models/liquid_layout_spec.rb
+++ spec/models/liquid_layout_spec.rb
@@ -1,5 +1,164 @@
 require 'rails_helper'
 
-RSpec.describe LiquidLayout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe LiquidLayout do
+
+  let(:template) { LiquidLayout.new(title: "My template", content: "placeholder") }
+  let(:example_html_top) { 
+    %Q{<div class="content">
+        <div id="foo"></div>
+        <div class="header slot3">}
+  }
+  let(:example_html_bottom) {
+    %Q{</div>
+      </div>}
+  }
+
+  it "is valid with just template and content" do
+    expect(template).to be_valid
+  end
+
+  describe "is invalid" do
+
+    after :each do
+      expect(template).to be_invalid
+    end
+
+    it "with empty string title" do
+      template.title = ""
+    end
+
+    it "with nil title" do
+      template.title = nil
+    end
+
+    it "with empty string content" do
+      template.content = ""
+    end
+
+    it "with nil content" do
+      template.content = nil
+    end
+  end
+  
+  describe "slots" do
+
+    describe "finds the slot if it" do
+
+      after :each do
+        template.content = "#{example_html_top}#{@example}#{example_html_bottom}"
+        expect(template.slot_ids).to eq [1]
+      end
+
+      it "has spaces in brackets" do
+        @example = '{{ slot1 }} '
+      end
+
+      it "does not have spaces in brackets" do
+        @example = ' {{slot1}}'
+      end
+
+      it "has whitespace around brackets" do
+        @example = ' {{slot1 }} '
+      end
+
+      it "does not have whitespace around brackets" do
+        @example = '{{ slot1}}'
+      end
+
+      it "has spaces between the word slot and the number" do
+        @example = '{{ slot 1 }}'
+      end
+
+    end
+
+    describe "does not find slot if it" do
+
+      after :each do
+        template.content = "#{example_html_top}#{@example}#{example_html_bottom}"
+        expect(template.slot_ids).to eq []
+      end
+
+      it "does not use the word 'slot'" do
+        @example = '{{ 1 }}'
+      end
+
+      it "does not have a number" do
+        @example = '{{ slot }}'
+      end
+
+      it "has an empty tag" do
+        @example = '{{ }}'
+      end
+    end
+
+  end
+  
+  describe "slot labels" do
+
+    describe "finds the slot label if it" do
+
+      after :each do
+        @expected ||= "sidebar"
+        template.content = "#{example_html_top}#{@example}#{example_html_bottom}"
+        expect(template.slot_labels).to eq [@expected]
+      end
+
+      it "is on a newline after the slot tag" do
+        @example  = %Q{
+          {{ slot1 }}
+          <!-- sidebar -->
+        }
+      end
+
+      it "is on the same line after the slot tag" do
+        @example  = "{{ slot1 }}<!-- sidebar -->"
+      end
+
+      it "is on the same line with spaces after the slot tag" do
+        @example  = "{{ slot1 }}   <!-- sidebar -->"
+      end
+
+      it "does not have spaces in the html comment" do
+        @example  = "{{ slot1 }} <!--sidebar-->"
+      end
+
+      it "has numbers, letters, and spaces" do
+        @example = "{{ slot1 }} <!-- my sw33t sidebar -->"
+        @expected = "my sw33t sidebar"
+      end
+
+      it "has dashes and carats" do
+        @example = "{{ slot1 }} <!-- <sidebar-slot-3> -->"
+        @expected = "<sidebar-slot-3>"
+      end
+    end
+
+    describe "does not find slot label if it" do
+
+      after :each do
+        @expected ||= [""]
+        template.content = "#{example_html_top}#{@example}#{example_html_bottom}"
+        expect(template.slot_labels).to eq @expected
+      end
+
+      it "is before the slot tag" do
+        @example = "<!--sidebar--> {{ slot1 }}"
+      end
+
+      it "does not have a nearby slot tag" do
+        @example = "<!--sidebar-->"
+        @expected = []
+      end
+
+      it "is a multi-line comment" do
+        @example  = %Q{
+          {{ slot1 }} <!--
+            sidebar 
+          -->
+        }
+      end
+    end
+
+  end
+
 end
