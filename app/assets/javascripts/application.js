59774e0e1dd9b386c04a40c221b33299a628cbec
Omar Sahyoun <osahyoun@gmail.com>
2015-08-12 13:59:33 +0100

    Refactor campaign_pages/edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require pub_sub
+
 //= require components
 //= require bootstrap-sprockets
 //= require select2
 //= require dropzone
-//= require dropzone_config
 //= require form_builder
 //= require_tree ./plugins
+
 //= require forms
+//= require campaign_page
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811174841) do
+ActiveRecord::Schema.define(version: 20150812105455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20150811174841) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "compiled_html"
-    t.text     "content"
+    t.text     "content",          default: ""
     t.boolean  "thermometer",      default: false
     t.boolean  "featured",         default: false
     t.boolean  "active",           default: false
