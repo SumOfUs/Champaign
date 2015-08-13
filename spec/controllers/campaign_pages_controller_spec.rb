dda4eb994d07b2c566ab7b46692705004b81970f
Neal Donnelly <NealJMD@gmail.com>
2015-08-13 15:12:11 -0600

    remove pending tests punted since week 1

--- spec/controllers/campaign_pages_controller_spec.rb
+++ spec/controllers/campaign_pages_controller_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 describe CampaignPagesController do
 
   let(:user) { instance_double('User') }
@@ -12,46 +14,6 @@ describe CampaignPagesController do
     it 'renders index' do
       get :index
       expect(response).to render_template('index')
-    end
-  end
-
-  context 'logged in' do
-    before do
-      pending("making collections and redirect work with doubles")
-    end
-
-    describe "POST create" do
-      let(:fake_params) { { 'name' => 'Foo'} }
-
-      before do
-        allow(CampaignPageParameters).to receive_message_chain('new.permit'){ fake_params }
-        allow(CampaignPage).to receive(:new) { campaign_page }
-        allow(campaign_page).to receive(:save) { true }
-        post :create, campaign_page: fake_params
-      end
-
-      it 'filters params' do
-        expect(CampaignPageParameters).to have_received(:new).with(hash_including({ 'campaign_page' => fake_params}))
-      end
-
-      it 'creates new campaign' do
-        expect(CampaignPage).to have_received(:new).with(fake_params)
-      end
-
-      it 'assigns @campaign' do
-        expect(assigns(:campaign_page)).to eq(campaign)
-      end
-
-      describe 'with errors' do
-
-        before do
-          allow(campaign_page).to receive(:save).and_return(false)
-        end
-
-        it 'renders new' do
-          expect(response).to render_template(:new)
-        end
-      end
     end
   end
 
