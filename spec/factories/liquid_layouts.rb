fc417e82b219de5ea5b5c6af20b1f52d4874b66f
Neal Donnelly <NealJMD@gmail.com>
2015-08-13 13:56:52 -0600

    add basic validation and spec to liquid_partial

--- spec/factories/liquid_layouts.rb
+++ spec/factories/liquid_layouts.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :liquid_layout do
     title { Faker::Company.bs }
-    content "<div class='fun'>{{ title }}</div>"
+    content "<div class='fun'>{ include 'thermometer' }</div>"
   end
 
 end
--- spec/models/liquid_partial_spec.rb
+++ spec/models/liquid_partial_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe LiquidPartial, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe LiquidPartial do
+  
+  let(:partial) { create(:liquid_partial) }
+
+  it "is valid" do
+    expect(partial).to be_valid
+  end
+
+  describe "is invalid" do
+
+    after :each do
+      expect(partial).to be_invalid
+    end
+
+    it "with a blank title" do
+      partial.title = " "
+    end
+
+    it "with a blank content" do
+      partial.content = " "
+    end
+  end
+
 end
