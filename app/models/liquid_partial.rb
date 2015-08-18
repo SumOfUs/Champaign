dbdf811c99a239a1831ef9e8556cf404e54ce85d
Neal Donnelly <NealJMD@gmail.com>
2015-08-18 17:10:41 -0400

    limit partials to referencing only one plugin

--- app/models/liquid_partial.rb
+++ app/models/liquid_partial.rb
@@ -1,12 +1,17 @@
 class LiquidPartial < ActiveRecord::Base
   validates :title, presence: true, allow_blank: false
   validates :content, presence: true, allow_blank: false
+  validate :one_plugin
 
-  # here we assume there's only one plugin per partial,
-  # but that needs a validation still
   def plugin_name
     return LiquidTagFinder.new(content).plugin_names[0]
   end
 
+  def one_plugin
+    plugin_names = LiquidTagFinder.new(content).plugin_names
+    if plugin_names.size > 1
+      errors.add(:content, "can only reference one partial, but found #{plugin_names.join(',')}")
+    end
+  end
 
 end
