bf1386db07c452dab1a9ba2693d59bcaaa3d4bd4
Omar Sahyoun <osahyoun@gmail.com>
2015-08-18 13:54:34 +0100

    Make form element deletable

--- app/controllers/form_elements_controller.rb
+++ app/controllers/form_elements_controller.rb
@@ -1,11 +1,23 @@
 class FormElementsController < ApplicationController
+  before_filter :find_form, only: [:create, :destroy]
+
   def create
-    @form = Form.find params[:form_id]
     @element = @form.form_elements.create(permitted_params)
 
     respond_to do |format|
       format.html do
-        render partial: 'element', locals: { element: @element }, status: :ok
+        render partial: 'element', locals: { form: @form, element: @element }, status: :ok
+      end
+    end
+  end
+
+  def destroy
+    element = @form.form_elements.find(params[:id])
+    element.destroy
+
+    respond_to do |format|
+      format.json do
+        render json: {status: :ok}, status: :ok
       end
     end
   end
@@ -15,5 +27,9 @@ class FormElementsController < ApplicationController
 
   def permitted_params
     params.require(:form_element).permit(:label, :name, :data_type, :required)
+  end
+
+  def find_form
+    @form = Form.find params[:form_id]
   end
 end
