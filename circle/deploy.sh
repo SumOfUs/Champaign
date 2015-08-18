fdb583882750ea5c4810dbc38e87d9c49bf452aa
tuuli <tuuli.pollanen@gmail.com>
2015-08-18 16:58:41 -0700

    Make changes to deploy script

--- circle/deploy.sh
+++ circle/deploy.sh
@@ -7,13 +7,9 @@ SHA1=$1
 docker push soutech/champaign_web:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=hello-bucket
-DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name hello \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name 'Champaign core application' \
+  --version-label $SHA1
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name hello-env \
+aws elasticbeanstalk update-environment --environment-name 'champaign' \
     --version-label $SHA1
