7d40ac55da432d7fd5edbf47ac81f2b7a1a19bfc
Neal Donnelly <NealJMD@gmail.com>
2015-08-19 08:55:51 -0400

    add spec for LiquidTagFinder.partial_refs

--- spec/services/liquid_tag_finder_spec.rb
+++ spec/services/liquid_tag_finder_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+# are these tests failing? it could be a breaking change in the liquid API.
+# change to version 3.0.x and try again.
 describe LiquidTagFinder do
 
   describe "plugin_names" do
@@ -61,8 +63,6 @@ describe LiquidTagFinder do
     end
   end
 
-
-
   describe "partial_names" do
 
     after :each do
@@ -70,22 +70,22 @@ describe LiquidTagFinder do
       expect(actual).to eq @expected
     end
 
-    it 'identifies a single include tag that uses single quotes' do
+    it 'finds a single include tag that uses single quotes' do
       @content  = "<div class='foo'>{% include 'example' %}</div>"
       @expected = ["example"]
     end
 
-    it 'identifies a single include tag that uses double quotes' do
+    it 'finds a single include tag that uses double quotes' do
       @content  = '<div class="foo">{% include "example" %}</div>'
       @expected = ["example"]
     end
 
-    it 'identifies a single include tag that passes a parameter' do
+    it 'finds a single include tag that passes a parameter' do
       @content  = '<div class="foo">{% include "my-template", color: "#f3a900" %}</div>'
       @expected = ["my-template"]
     end
 
-    it 'identifies two tags without matching a variable' do
+    it 'finds two tags without matching a variable' do
       @content = %Q{<section class="wrapper">
                       <div class="foo">
                         {% include "example" %}
@@ -96,6 +96,82 @@ describe LiquidTagFinder do
                       </div>
                     </section>}
       @expected = ["example", "swell"]
+    end
+  end
+
+  describe "partial_refs" do
+
+    let(:nested_top) {
+      %Q{ {% for field in plugins.Nd_0.fields %}
+            {% if plugins.Nd_0.is_chill %}
+              <h2>{{ plugins.chill.title }}</h2>
+            {% else %}
+              {% unless field == 'derp' %}
+                {{ plugins.Nd_1 }} }
+    }
+    let(:nested_bottom) {
+      %Q{     {% endunless %}
+            {% endif %}
+          {% endfor %} }
+    }
+    let(:surrounding) { {simple: ['',''], nested: [nested_top, nested_bottom] } }
+
+    [:simple, :nested].each do |nesting|
+
+      describe "with a #{nesting} partial" do
+
+        after :each do
+          liquid_markup = "#{surrounding[nesting][0]}#{@content}#{surrounding[nesting][1]}"
+          actual = LiquidTagFinder.new(liquid_markup).partial_refs
+          expect(actual).to match_array @expected
+        end
+
+        it "finds a single tag with no ref" do
+          @content  = "<div class='foo'>{% include 'example' %}</div>"
+          @expected = [['example', nil]]
+        end
+
+        it "finds a single tag with a ref" do
+          @content  = "<div class='foo'>{% include 'example', ref: 'juiz' %}</div>"
+          @expected = [['example', 'juiz']]
+        end
+
+        it "finds two of the same includes with different refs" do
+          @content  = "{% include 'example', ref: 'zebra' %}<div>{% include 'example', ref: 'juiz' %}</div>"
+          @expected = [['example', 'zebra'], ['example', 'juiz']]
+        end
+
+        it "finds two of the same includes with one ref" do
+          @content  = "{% include 'example', ref: 'zebra' %}<div>{% include 'example' %}</div>"
+          @expected = [['example', 'zebra'], ['example', nil]]
+        end
+
+        it "condenses two same includes with same ref" do
+          @content  = "{% include 'example', ref: 'zebra' %}<div>{% include 'example', ref: 'zebra' %}</div>"
+          @expected = [['example', 'zebra']]
+        end
+
+        it "condenses two same includes with no refs" do
+          @content  = "{% include 'example' %}<div>{% include 'example' %}</div>"
+          @expected = [['example', nil]]
+        end
+
+        it "finds two different includes with no refs" do
+          @content  = "{% include 'example' %}<div>{% include 'la paz' %}</div>"
+          @expected = [['example', nil], ['la paz', nil]]
+        end
+
+        it "finds two different includes with different refs" do
+          @content  = "{% include 'example', ref: 'zebra' %}<div>{% include 'la paz', ref: 'juiz' %}</div>"
+          @expected = [['example', 'zebra'], ['la paz', 'juiz']]
+        end
+
+        it "finds two different includes with same refs" do
+          @content  = "{% include 'example', ref: 'zebra' %}<div>{% include 'la paz', ref: 'juiz' %}</div>"
+          @expected = [['example', 'zebra'], ['la paz', 'juiz']]
+        end
+
+      end
     end
   end
 
