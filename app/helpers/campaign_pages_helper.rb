2dcae63339d2778115b7304c5efeb4510c56cd3e
Omar Sahyoun <osahyoun@gmail.com>
2015-08-19 11:43:12 +0100

    View helper method for displaying links on campaign page/plugins

--- app/helpers/campaign_pages_helper.rb
+++ app/helpers/campaign_pages_helper.rb
@@ -1,2 +1,9 @@
 module CampaignPagesHelper
+  def campaign_page_nav_item(text, path)
+    klass = current_page?(path) ? 'active' : nil
+
+    content_tag :li, class: klass do
+      link_to text, path
+    end
+  end
 end
