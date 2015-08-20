9387461ecf1dd0cc4cd529e51e25024e188143b7
tuuli <tuuli.pollanen@gmail.com>
2015-08-19 17:30:00 -0700

    Add S3 bucket for Dockerrun files

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,15 @@ SHA1=$1
 docker push soutech/champaign_web:$SHA1
 
 # Create new Elastic Beanstalk version
+
+EB_BUCKET=champaign.dockerrun.files
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+
+aws configure set default.region $AWS_REGION
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name 'Champaign core application' \
-  --version-label $SHA1
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name 'champaign' \
