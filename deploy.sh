8a1fe3cbbc9ed1b24b6b615597725763623194ec
tuuli <tuuli.pollanen@gmail.com>
2015-08-21 10:34:39 -0700

    The deploy script now includes a container command that adds pending migrations on Elastic Beanstalk.

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,18 @@ SHA1=$1
 # Deploy image to Docker Hub
 docker push soutech/champaign_web:$SHA1
 
-# Create new Elastic Beanstalk version
-
+# Update Elastic Beanstalk
 EB_BUCKET=champaign.dockerrun.files
-DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+echo 'Shipping source bundle to S3...'
+zip -r9 $SHA1-config.zip Dockerrun.aws.json ./.ebextensions/
+SOURCE_BUNDLE=$SHA1-config.zip
 
 aws configure set default.region $AWS_REGION
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name 'Champaign core application' \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws s3 cp $SOURCE_BUNDLE s3://$EB_BUCKET/$SOURCE_BUNDLE
 
-# Update Elastic Beanstalk environment to new version
+echo 'Creating new application version...'
+aws elasticbeanstalk create-application-version --application-name 'Champaign core application' \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$SOURCE_BUNDLE
+echo 'Updating environment...'
 aws elasticbeanstalk update-environment --environment-name 'champaign' \
     --version-label $SHA1
