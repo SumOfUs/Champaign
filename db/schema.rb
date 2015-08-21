b387b731f75076e78afa71521590ccf901c84e5b
Omar Sahyoun <osahyoun@gmail.com>
2015-08-21 17:13:30 +0100

    Necessary migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817195310) do
+ActiveRecord::Schema.define(version: 20150821125817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,10 +100,11 @@ ActiveRecord::Schema.define(version: 20150817195310) do
   add_index "form_elements", ["form_id"], name: "index_form_elements_on_form_id", using: :btree
 
   create_table "forms", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "visible",     default: false
   end
 
   create_table "images", force: :cascade do |t|
