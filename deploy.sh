21d2b983a5b67834347724b772a0130b6df1c1d4
tuuli <tuuli.pollanen@gmail.com>
2015-08-26 13:26:18 -0700

    Rename db on circle

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ aws configure set default.region $AWS_REGION
 aws s3 cp $SOURCE_BUNDLE s3://$EB_BUCKET/$SOURCE_BUNDLE
 
 echo 'Creating new application version...'
-aws elasticbeanstalk create-application-version --application-name $AWS_APPLICATION_NAME \
+aws elasticbeanstalk create-application-version --application-name "$AWS_APPLICATION_NAME" \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$SOURCE_BUNDLE
 echo 'Updating environment...'
 aws elasticbeanstalk update-environment --environment-name $AWS_ENVIRONMENT_NAME \
