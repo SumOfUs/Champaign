36174bef00bf8c5908486538ad0cbe494b24a2ea
Omar Sahyoun <osahyoun@gmail.com>
2015-08-28 15:16:43 +0100

    Ditch unused action

--- app/controllers/plugins/actions/forms_controller.rb
+++ app/controllers/plugins/actions/forms_controller.rb
@@ -12,30 +12,5 @@ class Plugins::Actions::FormsController < ApplicationController
       end
     end
   end
-
-
-  def update
-    @plugin = Plugins::Action.find(params[:id])
-    @plugin.update_attributes(permitted_params)
-    @campaign_page = @plugin.campaign_page
-
-    respond_to do |format|
-      format.html { render 'plugins/show' }
-      format.json { render json: action, status: :ok }
-    end
-  end
-
-  private
-
-  def find_form
-    if params[:plugins_action][:form_id]
-      Form.find params[:plugins_action][:form_id]
-    end
-  end
-
-  def permitted_params
-    params.require(:form).
-      permit(:form_id, :active)
-  end
 end
 
