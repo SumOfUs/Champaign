437ae711dac7f47cfedf437cc49087e5591644e0
Omar Sahyoun <osahyoun@gmail.com>
2015-09-01 12:46:06 +0100

    Adds master trait

--- spec/factories/liquid_layouts.rb
+++ spec/factories/liquid_layouts.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
   factory :liquid_layout do
     title { Faker::Company.bs }
     content "<div class='fun'></div>"
+
+    trait :master do
+      title 'master'
+    end
   end
 
 end
