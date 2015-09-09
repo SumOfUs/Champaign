98fe1b0f7838c42bbde78d326300169a0ba53057
Neal Donnelly <NealJMD@gmail.com>
2015-09-09 10:20:49 -0600

    move manage action service class to its own file

---
+++ app/services/manage_action.rb
@@ -0,0 +1,34 @@
+class ManageAction
+
+  def initialize(params)
+    @params = params
+  end
+
+  def create
+    return if user_has_already_taken_action
+
+    Action.create({
+      action_user: action_user,
+      campaign_page: page,
+      form_data: @params
+    })
+  end
+
+  private
+
+  def user_has_already_taken_action
+    Action.exists? action_user: action_user
+  end
+
+  def previous_action
+    Action.where(action_user: action_user, campaign_page_id: page).first
+  end
+
+  def action_user
+    @user ||= ActionUser.find_by(email: @params[:email]) || ActionUser.create(email: @params[:email])
+  end
+
+  def page
+    @campaign_page ||= CampaignPage.find(@params[:campaign_page_id])
+  end
+end
