ee3b697915913a8899c2ad806d9b7feb11a77f15
tuuli <tuuli.pollanen@gmail.com>
2015-09-11 15:37:00 -0700

    Add pending specs for search by plugin

--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'Search ::' do
 
   let(:test_text) { 'a spectacular test string' }
-  let(:language) { build(:language) }
   let!(:tag) { create(:tag, name: test_text, actionkit_uri: '/foo/bar') }
   let!(:alternative_tag) { create(:tag, name: 'alternative tag', actionkit_uri: '/alternative_tag/') }
   let!(:the_best_tag) { create(:tag, name: 'best tag ever', actionkit_uri: '/secretly_insecure/') }
@@ -19,7 +18,16 @@ describe 'Search ::' do
   let!(:only_tag) { create(:tag, name: 'only tag on a page', actionkit_uri: '/entitled/tag') }
   let!(:campaign) { create(:campaign, name: test_text) }
   let!(:campaign2) { create(:campaign, name: 'Why not Zoidberg?') }
+  let!(:twin_campaign) { create(:campaign, name: 'Campaign that contains two pages') }
+  let!(:unimpactful_campaign) { create(:campaign, name: 'Campaign with just one page?') }
   let!(:layout) { create(:liquid_layout) }
+  let!(:unused_layout) { create(:liquid_layout, title: 'too bad for anyone to use, ever') }
+  let!(:messy_layout) { create(:liquid_layout, title: 'has kinda tacky UX')}
+  let!(:twin_layout) { create(:liquid_layout, title: 'Layout that has two pages associated with it')}
+  let!(:language) { create(:language) }
+  let!(:klingon) { create(:language, code: 'KLI', name: 'Klingon') }
+  let!(:pig_latin) { create(:language, code: 'PIG', name: 'Oink_oink') }
+  let!(:unused_language) { create(:language, code: 'NIL', name: 'Esperanto') }
 
   describe 'PageSearcher' do
 
@@ -45,50 +53,59 @@ describe 'Search ::' do
     let!(:has_many_tags) {
       create(:page,
              title: 'a very taggy page',
-             tags: [alternative_tag, the_best_tag]
+             tags: [alternative_tag, the_best_tag],
       )
     }
 
-    let!(:has_many_unique_tags) {
+    let!(:single_return_page) {
       create(:page,
              title: 'a special snowflake',
-             tags: [hipster_tag, unpopular_tag]
+             tags: [hipster_tag, unpopular_tag],
+             language: pig_latin,
+             campaign: unimpactful_campaign,
+             liquid_layout: messy_layout
       )
     }
 
 
     let!(:twin_page_1) {
       create(:page,
-             title: 'has same tag as twin page 2',
-             tags: [only_tag]
+             title: 'looks suspiciously like twin page 2',
+             tags: [only_tag],
+             language: klingon,
+             campaign: twin_campaign,
+             liquid_layout: twin_layout
       )
     }
 
     let!(:twin_page_2) {
       create(:page,
-             title: 'has same tag as twin page 1',
-             tags: [only_tag]
+             title: 'looks suspiciously like twin page 1',
+             tags: [only_tag],
+             language: klingon,
+             campaign: twin_campaign,
+             liquid_layout: twin_layout
       )
     }
 
     let!(:intersection_page_1) {
       create(:page,
              title: 'has one same tag as intersection page 2',
-             tags: [tag1, tag2, tag3, tag4]
+             tags: [tag1, tag2, tag3, tag4],
       )
     }
 
     let!(:intersection_page_2) {
       create(:page,
              title: 'has one same tag as intersection page 1',
-             tags: [tag3, tag4, tag5]
+             tags: [tag3, tag4, tag5],
       )
     }
 
     let!(:page_that_doesnt_match_anything) {
       create(:page,
          title: 'Not a good match',
-         language: build(:language, code: 'PIG', name: 'Pig latin'),
+         language: build(:language, code: 'FIN', name: 'Finnish'),
          tags: [
              create(:tag, name: 'tag not found', actionkit_uri: '/foo/404'),
              create(:tag, name: 'tag erroror', actionkit_uri: '/foo/500')
@@ -170,8 +187,8 @@ describe 'Search ::' do
             expect((Search::PageSearcher.new({search: {tags: [the_best_tag.id]}})).search).to match_array([has_many_tags])
           end
           it "with multiple of that page's tags in any order" do
-            expect((Search::PageSearcher.new({search: {tags: [hipster_tag.id, unpopular_tag.id]}})).search).to match_array([has_many_unique_tags])
-            expect((Search::PageSearcher.new({search: {tags: [unpopular_tag.id, hipster_tag.id]}})).search).to match_array([has_many_unique_tags])
+            expect((Search::PageSearcher.new({search: {tags: [hipster_tag.id, unpopular_tag.id]}})).search).to match_array([single_return_page])
+            expect((Search::PageSearcher.new({search: {tags: [unpopular_tag.id, hipster_tag.id]}})).search).to match_array([single_return_page])
           end
           it "with a tag that matches two pages and a tag that matches one page" do
             expect((Search::PageSearcher.new({search: {tags: [alternative_tag.id, the_best_tag.id]}})).search).to match_array([has_many_tags])
@@ -194,58 +211,108 @@ describe 'Search ::' do
 
       context 'search by campaign' do
         let(:search_by_campaign) { Search::PageSearcher.new({search: {campaign: [campaign.id]} }) }
+        let!(:unused_campaign) { create(:campaign, name:'unused campaign') }
         it 'searches for a page based on the campaign it belongs to' do
           expect(search_by_campaign.search).to match_array([title_language_campaign_match])
         end
 
-        describe 'returns nothing when searching' do
-          it 'with an empty array'
-          it 'with nil'
-          it 'with a non-existent campaign id'
-          it 'with an empty string'
-          it 'with an unused campaign id'
+        describe 'returns all pages when searching' do
+          it 'with an empty array' do
+            expect(Search::PageSearcher.new({search: {campaign: []} }).search).to match_array(CampaignPage.all)
+          end
+          it 'with nil' do
+            expect(Search::PageSearcher.new({search: {campaign: nil} }).search).to match_array(CampaignPage.all)
+          end
+
+          it 'with an empty string' do
+            expect(Search::PageSearcher.new({search: {campaign: ''} }).search).to match_array(CampaignPage.all)
+          end
+        end
+
+        describe 'returns no pages when searching' do
+          it 'with a non-existent campaign id' do
+            expect(Search::PageSearcher.new({search: {campaign: [CampaignPage.last.id+1]} }).search).to match_array([])
+          end
+          it 'with an unused campaign id' do
+            expect(Search::PageSearcher.new({search: {campaign: [unused_campaign.id]} }).search).to match_array([])
+          end
         end
 
         describe 'returns one page when searching' do
-          it 'with a campaign that only contains one page'
+          it 'with a campaign that only contains one page' do
+            expect(Search::PageSearcher.new({search: {campaign: [unimpactful_campaign.id]} }).search).to match_array([single_return_page])
+          end
         end
 
         describe 'returns some pages when searching' do
-          it 'with a used campaign id and an unused campaign id'
+          it 'with a used campaign id and an unused campaign id' do
+            expect(Search::PageSearcher.new({search: {campaign: [unimpactful_campaign.id, unused_campaign.id]} }).search).to match_array([single_return_page])
+          end
         end
 
         describe 'returns multiple pages when searching' do
-          it 'with mutiple campaign ids that different pages belong to'
-          it 'with a campaign id that several pages belong to'
+          it 'with mutiple campaign ids that different pages belong to' do
+            expect(Search::PageSearcher.new({search: {campaign: [unimpactful_campaign.id, twin_campaign.id]} }).search).to match_array([
+              twin_page_1, twin_page_2, single_return_page
+            ])
+          end
+          it 'with a campaign id that several pages belong to' do
+            expect(Search::PageSearcher.new({search: {campaign: [twin_campaign.id]} }).search).to match_array([twin_page_1, twin_page_2])
+          end
         end
 
       end
 
       context 'search by layout' do
         let(:layout_searcher) { Search::PageSearcher.new({search: {layout: [layout.id]} }) }
+
         it 'finds a page that contains the specified liquid layout' do
           expect(layout_searcher.search).to match_array([content_tag_plugin_layout_match])
         end
 
-        describe 'returns nothing when searching' do
-          it 'with an empty array'
-          it 'with nil'
-          it 'with an empty string'
-          it 'with a non-existent layout'
-          it 'with an unused layout'
+        describe 'returns all campaign pages unfiltered when searching' do
+          describe 'returns all pages when searching' do
+            it 'with an empty array' do
+              expect(Search::PageSearcher.new({search: {layout: []} }).search).to match_array(CampaignPage.all)
+            end
+            it 'with nil' do
+              expect(Search::PageSearcher.new({search: {layout: nil} }).search).to match_array(CampaignPage.all)
+            end
+
+            it 'with an empty string' do
+              expect(Search::PageSearcher.new({search: {layout: ''} }).search).to match_array(CampaignPage.all)
+            end
+          end
+        end
+
+        describe 'returns no pages when searching' do
+          it 'with a non-existent layout' do
+            expect(Search::PageSearcher.new({search: {layout: [LiquidLayout.last.id+1]} }).search).to match_array([])
+          end
+          it 'with an unused layout' do
+            expect(Search::PageSearcher.new({search: {layout: [unused_layout.id]} }).search).to match_array([])
+          end
         end
 
         describe 'returns one page when searching' do
-          it 'with a layout that only one page belongs to'
+          it 'with a layout that only one page belongs to' do
+            expect(Search::PageSearcher.new({search: {layout: [messy_layout.id]} }).search).to match_array([single_return_page])
+          end
         end
 
         describe 'returns some pages when searching' do
-          it 'with a used layout and an unused layout'
+          it 'with a used layout and an unused layout' do
+            expect(Search::PageSearcher.new({search: {layout: [messy_layout.id, unused_layout.id]}}).search).to match_array([single_return_page])
+          end
         end
 
         describe 'returns multiple pages when searching' do
-          it 'with mutiple layouts that different pages belong to'
-          it 'with a layout that several pages belong to'
+          it 'with mutiple layouts that different pages belong to' do
+            expect(Search::PageSearcher.new({search: {layout: [messy_layout.id, twin_layout.id]}}).search).to match_array([single_return_page, twin_page_1, twin_page_2])
+          end
+          it 'with a layout that several pages belong to' do
+            expect(Search::PageSearcher.new({search: {layout: [twin_layout.id]}}).search).to match_array([twin_page_1, twin_page_2])
+          end
         end
 
       end
@@ -255,12 +322,77 @@ describe 'Search ::' do
         it 'finds only the page that corresponds to the specified language' do
           expect(language_searcher.search).to match_array([title_language_campaign_match])
         end
+        describe 'returns all campaign pages unfiltered when searching' do
+          describe 'returns all pages when searching' do
+            it 'with an empty array' do
+              expect(Search::PageSearcher.new({search: {language: []} }).search).to match_array(CampaignPage.all)
+            end
+            it 'with nil' do
+              expect(Search::PageSearcher.new({search: {language: nil} }).search).to match_array(CampaignPage.all)
+            end
+
+            it 'with an empty string' do
+              expect(Search::PageSearcher.new({search: {language: ''} }).search).to match_array(CampaignPage.all)
+            end
+          end
+        end
+
+        describe 'returns no pages when searching' do
+          it 'with a non-existent language' do
+            expect(Search::PageSearcher.new({search: {language: [Language.last.id+999]} }).search).to match_array([])
+          end
+          it 'with an unused language' do
+            expect(Search::PageSearcher.new({search: {language: [unused_language.id]} }).search).to match_array([])
+          end
+        end
+
+        describe 'returns one page when searching' do
+          it 'with a language that only one page belongs to' do
+            expect(Search::PageSearcher.new({search: {language: [pig_latin.id]} }).search).to match_array([single_return_page])
+          end
+        end
+
+        describe 'returns some pages when searching' do
+          it 'with a used language and an unused language' do
+            expect(Search::PageSearcher.new({search: {language: [pig_latin.id, unused_language.id]}}).search).to match_array([single_return_page])
+          end
+        end
+
+        describe 'returns multiple pages when searching' do
+          it 'with mutiple languages that different pages belong to' do
+            expect(Search::PageSearcher.new({search: {language: [pig_latin.id, klingon.id]}}).search).to match_array([single_return_page, twin_page_1, twin_page_2])
+          end
+          it 'with a language that several pages belong to' do
+            expect(Search::PageSearcher.new({search: {language: [klingon.id]}}).search).to match_array([twin_page_1, twin_page_2])
+          end
+        end
       end
 
       context 'search by plugin' do
         let(:plugin_searcher) { Search::PageSearcher.new({search: {plugin_type: ['Plugins::Action']}})}
         it 'finds the page that is associated with that plugin type' do
           expect(plugin_searcher.search).to match_array([content_tag_plugin_layout_match])
+        end
+        describe 'returns all pages when searching' do
+          it 'with an empty array' do
+            expect(Search::PageSearcher.new({search: {plugin: []}}).search).to match_array(CampaignPage.all)
+          end
+          it 'with nil' do
+            expect(Search::PageSearcher.new({search: {plugin: nil}}).search).to match_array(CampaignPage.all)
+          end
+          it 'with empty string' do
+            expect(Search::PageSearcher.new({search: {plugin: ""}}).search).to match_array(CampaignPage.all)
+          end
+        end
+        describe 'returns no pages when searching' do
+          it 'if searching with a layout that does not contain the plugin' do
+          it 'if searching with a layout that does contain the plugin but if all of the matching pages have that plugin turned off'
+          it 'if searching with a plugin that does not exist'
+          it 'if searching with several plugins where a page matches one but not the rest of them'
+          it 'if searching with several plugins where a page matches by criteria but the requested plugins are deactivated'
+        end
+        describe 'returns some pages when searching' do
+          it 'finds a page that belongs to a layout that contains the plugin and the plugin is activated'
         end
       end
     end
