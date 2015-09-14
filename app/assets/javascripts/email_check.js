ae30e661c70e53ca38444fd70ff3608a729b7648
Neal Donnelly <NealJMD@gmail.com>
2015-09-14 12:30:36 -0600

    make mailcheck check class instead of name and add msg div

--- app/assets/javascripts/email_check.js
+++ app/assets/javascripts/email_check.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
-  $('[name="email_address"]').on('blur', function(){
-    var message_div = $('#mailcheck_message');
-    $(this).mailcheck({
+  $('.mailcheck').on('blur', function(){
+    var $target = $(this);
+    var $message_div = $target.parent().find('.mailcheck-message');
+    if( $message_div.length === 0) {
+      $target.after('<div class="mailcheck-message"></div>');
+      $message_div = $target.parent().find('.mailcheck-message');
+    }
+    $target.mailcheck({
       suggested: function(element, suggestion) {
-        message_div.html('<em>Did you mean ' + suggestion['full'] + '?</em> - <a href="javascript:;" id="change_email">Change</a>');
+        $message_div.html('<em>Did you mean ' + suggestion['full'] + '?</em> - <a href="javascript:;" id="change_email">Change</a>');
         $('#change_email').on('click', function(){
-          $('[name="email_address"]').val(suggestion['full'])
-          message_div.addClass('hidden');
+          $target.val(suggestion['full'])
+          $message_div.addClass('hidden');
         });
-        message_div.removeClass('hidden');
+        $message_div.removeClass('hidden');
       },
       empty: function() {
-        message_div.addClass('hidden');
+        $message_div.addClass('hidden');
       }
     });
   });
