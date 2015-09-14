0ea0835b407861aeae4abb68c13d698548612e0a
Eric Boersma <eric.boersma@gmail.com>
2015-09-14 10:17:42 -0400

    Offering change UX to mail check.

--- app/assets/javascripts/email_check.js
+++ app/assets/javascripts/email_check.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
     var message_div = $('#mailcheck_message');
     $(this).mailcheck({
       suggested: function(element, suggestion) {
-        message_div.html('<em>Did you mean ' + suggestion['full'] + '?</em>');
+        message_div.html('<em>Did you mean ' + suggestion['full'] + '?</em> - <a href="javascript:;" id="change_email">Change</a>');
+        $('#change_email').on('click', function(){
+          $('[name="email_address"]').val(suggestion['full'])
+          message_div.addClass('hidden');
+        });
         message_div.removeClass('hidden');
       },
       empty: function() {
