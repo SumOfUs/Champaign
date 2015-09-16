35bb654a19f9c759d11d76efadcc26339b3c3cce
Omar Sahyoun <osahyoun@gmail.com>
2015-09-16 11:52:33 +0100

    Liquid filesystem refactoring

--- app/liquid/liquid_file_system.rb
+++ app/liquid/liquid_file_system.rb
@@ -1,20 +1,31 @@
 class LiquidFileSystem
   class << self
     def partials(title)
-      @partials ||= Dir.glob(["#{Rails.root}/app/views/plugins/**/_#{title}.liquid", "#{Rails.root}/app/views_liquid/**/_#{title}.liquid"])
+      Dir.glob(["#{Rails.root}/app/views/plugins/**/_#{title}.liquid", "#{Rails.root}/app/views_liquid/**/_#{title}.liquid"])
     end
 
     def read_template_file(title)
-      view = LiquidPartial.find_by(title: title)
-      if view
-        return view.content
+      if ENV['LIQUID_TEMPLATING_SOURCE'] == 'file'
+        return read_from_file(title)
       else
-        if LiquidFileSystem.partials(title).any?
-          File.read LiquidFileSystem.partials(title).first
-        else
-          "Partial #{title} was not found"
-        end
+        return read(title)
       end
+    end
+
+    private
+
+    def read(title)
+      read_from_store(title) || read_from_file(title) || "Partial #{title} was not found"
+    end
+
+    def read_from_store(title)
+      LiquidPartial.find_by(title: title).try(:content)
+    end
+
+    def read_from_file(title)
+      return nil if LiquidFileSystem.partials(title).empty?
+      File.read(LiquidFileSystem.partials(title).try(:first))
     end
   end
 end
+
