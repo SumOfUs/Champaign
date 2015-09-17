517b45ab17f9992ba1fc7cddef5e571539a7b118
Neal Donnelly <NealJMD@gmail.com>
2015-09-17 02:43:27 -0600

    add sticky behavior for demo

--- app/assets/javascripts/sumofus.js
+++ app/assets/javascripts/sumofus.js
@@ -4,5 +4,6 @@
 
 //= require show_errors
 //= require select2
+//= require sticky
 //= require_tree ./sumofus
 //= require_directory ./plugins
--- app/assets/javascripts/sumofus/scroll.js
+++ app/assets/javascripts/sumofus/scroll.js
@@ -14,4 +14,7 @@ $(document).ready(function(){
     });
   });
 
+
+  $(".stickem").sticky({topSpacing:0});
+
 });
