7ca76613534e65b31ef380b16b29b194c0c39643
tuuli <tuuli.pollanen@gmail.com>
2015-09-21 15:58:34 -0700

    Segment page searcher specs into individual files

---
+++ spec/services/search/page_searcher/single_criterion_search/search_by_layout_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+require_relative '../../page_searcher_spec_data.rb'
+
+describe 'Search ::' do
+  describe 'PageSearcher' do
+    context 'searches by single criterion,' do
+      context 'by layout' do
+        include_context 'page_searcher_spec_data'
+        let(:layout_searcher) { Search::PageSearcher.new({search: {layout: [layout.id]} }) }
+
+        it 'finds a page that contains the specified liquid layout' do
+          expect(layout_searcher.search).to match_array([content_tag_plugin_layout_match])
+        end
+
+        describe 'returns all campaign pages unfiltered when searching' do
+          describe 'returns all pages when searching' do
+            it 'with an empty array' do
+              expect(Search::PageSearcher.new({search: {layout: []} }).search).to match_array(CampaignPage.all)
+            end
+            it 'with nil' do
+              expect(Search::PageSearcher.new({search: {layout: nil} }).search).to match_array(CampaignPage.all)
+            end
+
+            it 'with an empty string' do
+              expect(Search::PageSearcher.new({search: {layout: ''} }).search).to match_array(CampaignPage.all)
+            end
+          end
+        end
+
+        describe 'returns no pages when searching' do
+          it 'with a non-existent layout' do
+            expect(Search::PageSearcher.new({search: {layout: [LiquidLayout.last.id+1]} }).search).to match_array([])
+          end
+          it 'with an unused layout' do
+            expect(Search::PageSearcher.new({search: {layout: [unused_layout.id]} }).search).to match_array([])
+          end
+        end
+
+        describe 'returns one page when searching' do
+          it 'with a layout that only one page belongs to' do
+            expect(Search::PageSearcher.new({search: {layout: [messy_layout.id]} }).search).to match_array([single_return_page])
+          end
+        end
+
+        describe 'returns some pages when searching' do
+          it 'with a used layout and an unused layout' do
+            expect(Search::PageSearcher.new({search: {layout: [messy_layout.id, unused_layout.id]}}).search).to match_array([single_return_page])
+          end
+        end
+
+        describe 'returns multiple pages when searching' do
+          it 'with mutiple layouts that different pages belong to' do
+            expect(Search::PageSearcher.new({search: {layout: [messy_layout.id, twin_layout.id]}}).search).to match_array([single_return_page, twin_page_1, twin_page_2])
+          end
+          it 'with a layout that several pages belong to' do
+            expect(Search::PageSearcher.new({search: {layout: [twin_layout.id]}}).search).to match_array([twin_page_1, twin_page_2])
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/search/page_searcher/single_criterion_search/search_by_text_content_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require_relative '../../page_searcher_spec_data.rb'
+
+describe 'Search ::' do
+  describe 'PageSearcher' do
+    context 'searches by single criterion,' do
+      context 'by text content' do
+        include_context 'page_searcher_spec_data'
+        let(:text_searcher) { Search::PageSearcher.new({search: {content_search: test_text} }) }
+        it 'gets pages that match by title or by text body if the text search method is called' do
+          expect(text_searcher.search).to match_array([content_tag_plugin_layout_match, title_language_campaign_match])
+        end
+      end
+    end
+  end
+end
