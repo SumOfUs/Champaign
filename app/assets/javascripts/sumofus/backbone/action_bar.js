da2be4c4fda5656b715ce0b72c64a2be6829a6e1
Neal Donnelly <NealJMD@gmail.com>
2015-09-23 15:43:42 -0600

    spruce up action_bar code with es6

--- app/assets/javascripts/sumofus/backbone/action_bar.js
+++ app/assets/javascripts/sumofus/backbone/action_bar.js
@@ -6,7 +6,7 @@ const ActionBar = Backbone.View.extend({
     'click .action-bar__open-button': 'reveal',
     'click .action-bar__close-button': 'hide',
     'click .action-bar__expand-arrow': 'toggleBlurb',
-    'click .action-bar__top': 'toggleBlurb'
+    'click .action-bar__top': 'toggleBlurb',
   },
 
   initialize: function() {
@@ -29,19 +29,27 @@ const ActionBar = Backbone.View.extend({
 
   toggleBlurb: function() {
     if (this.$('.action-bar__expand-arrow').hasClass('action-bar__expand-arrow--expanded')) {
-      this.$('.action-bar__main').css('top', '');
-      this.$el.parent('.sticky-wrapper').css('top', '');
+      this.expandBlurb();
     } else {
-      var height = this.$('.action-bar__top').outerHeight();
-      this.$('.action-bar__main').css('top', `${height}px`);
-      this.$el.parent('.sticky-wrapper').css('top', `-${height}px`);
+      this.collapseBlurb();
     }
     this.$('.action-bar__expand-arrow').toggleClass('action-bar__expand-arrow--expanded');
   },
 
+  expandBlurb: function() {
+    this.$('.action-bar__main').css('top', '');
+    this.$el.parent('.sticky-wrapper').css('top', '');
+  },
+
+  collapseBlurb: function() {
+    const height = this.$('.action-bar__top').outerHeight();
+    this.$('.action-bar__main').css('top', `${height}px`);
+    this.$el.parent('.sticky-wrapper').css('top', `-${height}px`);
+  },
+
   makeSticky: function() {
     this.$el.sticky({topSpacing:0});
-  }
+  },
 
 });
 
