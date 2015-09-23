961ca87c9e436df067e086ed0d4ce9162cc8eed0
Neal Donnelly <NealJMD@gmail.com>
2015-09-23 16:19:36 -0600

    fix sticky sticky glitchiness on resize

--- app/assets/javascripts/sumofus/backbone/action_bar.js
+++ app/assets/javascripts/sumofus/backbone/action_bar.js
@@ -10,9 +10,12 @@ const ActionBar = Backbone.View.extend({
   },
 
   initialize: function() {
+    this.isSticky = false;
     if (!this.isMobile()) {
       this.makeSticky();
     }
+    // can't use events hash cause scoped to window
+    $(window).on('resize', () => this.questionSticky());
   },
 
   isMobile: function() {
@@ -48,7 +51,25 @@ const ActionBar = Backbone.View.extend({
   },
 
   makeSticky: function() {
-    this.$el.sticky({topSpacing:0});
+    if(!this.isSticky) {
+      this.$el.sticky({topSpacing:0});
+      this.isSticky = true;
+    }
+  },
+
+  unmakeSticky: function() {
+    if(this.isSticky) {
+      this.$el.unstick();
+      this.isSticky = false;
+    }
+  },
+
+  questionSticky: function() {
+    if(this.isMobile()) {
+      this.unmakeSticky();
+    } else {
+      this.makeSticky();
+    }
   },
 
 });
