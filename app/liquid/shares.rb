5f2a589f6bf4ce0510f2bbac73e3e0e8ea88b310
Neal Donnelly <NealJMD@gmail.com>
2015-09-25 17:37:42 -0600

    add reasonable draft of share page layout per agency design

--- app/liquid/shares.rb
+++ app/liquid/shares.rb
@@ -1,7 +1,7 @@
 module Shares
   def self.get_all(page)
     Share::Button.where(page_id: page.id).inject({}) do |shares, button|
-      shares[button.sp_type] = button.sp_button_html
+      shares[button.sp_type] = button.sp_button_html.html_safe unless button.sp_button_html.blank?
       shares
     end
   end
