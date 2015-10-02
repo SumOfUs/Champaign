aaa6567e9ae53e99370dc7188d2ecaaeb01e35c9
Eric Boersma <eric.boersma@gmail.com>
2015-10-02 15:14:10 -0400

    Add queue post to action creation.

--- app/services/manage_action.rb
+++ app/services/manage_action.rb
@@ -5,6 +5,8 @@ class ManageAction
 
   def create
     Action.find_or_create_by( action_user: action_user, page: page ) do |action|
+      queue_message = {type: 'action', params: {slug: page.slug, email: action_user.email}.merge(@params)}
+      ChampaignQueue.push(queue_message)
       action.form_data = @params
     end
   end
