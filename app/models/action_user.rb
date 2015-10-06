402e3fcc06d8e36720cba0d1d3d37eab7bc0d27b
Eric Boersma <eric.boersma@gmail.com>
2015-10-06 13:47:02 -0400

    Specs for ActionUser method for use in PageController

--- app/models/action_user.rb
+++ app/models/action_user.rb
@@ -1,2 +1,20 @@
 class ActionUser < ActiveRecord::Base
+
+  def self.find_action_user_from_request(akid, cookie_id)
+    action_user = self.find_by_actionkit_user_id(akid)
+    if action_user and akid
+      # We don't allow returns when there's no AKID because
+      # that field isn't required, so a nil AKID would return
+      # lots of users, which we don't want.
+      action_user
+    elsif cookie_id
+      begin
+        ActionUser.find(cookie_id)
+      rescue ActiveRecord::RecordNotFound
+        nil
+      end
+    else
+      nil
+    end
+  end
 end
--- spec/models/action_user_spec.rb
+++ spec/models/action_user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ActionUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_with_akid) {ActionUser.create! actionkit_user_id: 'test' }
+  let(:user_without_akid) {ActionUser.create! actionkit_user_id: 'fake_id'}
+
+  it 'finds users by akid' do
+    user = user_with_akid
+    expect(ActionUser.find_action_user_from_request('test', nil)).to eq(user)
+  end
+
+  it 'finds users by id' do
+    user = user_without_akid
+    expect(ActionUser.find_action_user_from_request(nil, user.id)).to eq(user)
+  end
+
+  it 'returns nil when it cannot find any users' do
+    _ = user_without_akid
+    expect(ActionUser.find_action_user_from_request(nil, nil)).to eq(nil)
+  end
 end
