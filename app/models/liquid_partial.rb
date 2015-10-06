c1270dd92ff5a3e9b1dae67aa21a14193adf9ca9
Neal Donnelly <NealJMD@gmail.com>
2015-10-06 16:07:18 -0600

    add spec, comments, and small fixes for LiquidPartial.plugin_refs

--- app/models/liquid_partial.rb
+++ app/models/liquid_partial.rb
@@ -10,8 +10,19 @@ class LiquidPartial < ActiveRecord::Base
     LiquidTagFinder.new(content).plugin_names[0]
   end
 
+  # This method is used to find all the plugins referenced in both
+  # this partial, and any partials included by this partial. It returns
+  # an array of [plugin_name, plugin_ref] pairs.
+  #
+  # The method operates recursively by instantiating other partials
+  # that are referenced in this partial and calling this method on them.
+  # To avoid circular references, the recursion is only allowed to a depth
+  # of 2 subpartials.
+  #
+  # For examples, see liquid_partial_spec.rb
+  #
   def plugin_refs(ref: nil, depth: 0)
-    return if depth > 2
+    return [] if depth > 2
     introspector = LiquidTagFinder.new(content)
     plugin_names = introspector.plugin_names
     collector = plugin_names.empty? ? [] : [[plugin_names[0], ref]]
@@ -20,7 +31,7 @@ class LiquidPartial < ActiveRecord::Base
       next if child_partial.blank?
       collector += child_partial.plugin_refs(ref: child_ref, depth: depth+1)
     end
-    collector
+    collector.uniq
   end
 
   # Filters array of partial names to those absent from the database. (returns new array)
