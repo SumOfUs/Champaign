7b6a0694969e946f456888181fd273d107a05747
Neal Donnelly <NealJMD@gmail.com>
2015-10-07 12:27:08 -0600

    finish spec for liquid layout switcher

--- spec/liquid/liquid_layout_switcher_spec.rb
+++ spec/liquid/liquid_layout_switcher_spec.rb
@@ -10,7 +10,10 @@ describe LiquidLayoutSwitcher do
 
   let(:blank_layout) { create :liquid_layout, title: 'blank', content: '<h1>yoooo</h1>'}
   let(:both_refless_layout) { create :liquid_layout, title: 'both_refless', content: '{% include "action" %} {% include "thermo" %}'}
-  let(:action_ref_layout) { create :liquid_layout, title: 'action_ref', content: '{% include "action", ref: "modal" %}'}
+  let(:action_ref_layout) { create :liquid_layout, title: 'action_ref', content: '{% include "action", ref: "modal" %} '}
+  let(:thermo_action_ref_layout) { create :liquid_layout, title: 'action_ref', content: '{% include "action", ref: "modal" %} {% include "thermo" %}'}
+  let(:many_action_layout) { create :liquid_layout, title: 'many_action', content: '{% include "action", ref: "a" %} {% include "action", ref: "b" %} {% include "action" %}'}
+  let(:nested_refless_layout) { create :liquid_layout, title: 'nested_refless', content: '{% include "action_nested_thermo" %}' }
   let(:page) { create :page, liquid_layout: both_refless_layout }
 
   let(:switcher) { LiquidLayoutSwitcher.new(page) }
@@ -19,25 +22,11 @@ describe LiquidLayoutSwitcher do
 
     describe 'creating' do
 
-      describe 'when using the same template' do
-
-        before :each do
-          expect(page.plugins).to be_empty
-          expect(page.liquid_layout).to eq both_refless_layout
-        end
-
-        after :each do
-          expect(page.plugins.size).to eq 2
-        end
-
-        it 'creates missing action' do
-          expect{ switcher.switch(both_refless_layout) }.to change{ Plugins::Action.count }.by 1
-        end
-
-        it 'creates missing thermometer' do
-          expect{ switcher.switch(both_refless_layout) }.to change{ Plugins::Thermometer.count }.by 1
-        end
-
+      it 'creates missing plugins when using the same template' do
+        expect(page.plugins).to be_empty
+        expect(page.liquid_layout).to eq both_refless_layout
+        expect{ switcher.switch(both_refless_layout) }.to change{ Plugins::Action.count }.by(1).and change{ Plugins::Thermometer.count }.by 1
+        expect(page.plugins.size).to eq 2
       end
 
       it 'creates new plugins when switching to a template with more plugins' do
@@ -61,18 +50,61 @@ describe LiquidLayoutSwitcher do
         expect( created.ref ).to eq nil
       end
 
+      it 'creates multiple of the same type' do
+        expect{ switcher.switch( many_action_layout ) }.to change{ Plugins::Action.count }.by 3
+      end
+
     end
 
-    describe 'switching' do
+    describe 'replacing' do
 
-      it 'does not replace instances if new template has same plugins'
-      it 'does not replace instances if new template is old template'
-      it 'does replace instances if refs are different'
-      it 'does can replace one instance but not the other'
+      before :each do
+        switcher.switch(both_refless_layout)
+      end
+
+      it 'does not replace instances if new template has same plugins' do
+        plugins = page.plugins
+        expect{ switcher.switch(nested_refless_layout) }.to change{ Plugins::Action.count }.by 0
+        expect(plugins).to all be_persisted
+      end
+
+      it 'does not replace instances if new template is old template' do
+        plugins = page.plugins
+        expect{ switcher.switch(both_refless_layout) }.to change{ Plugins::Action.count }.by 0
+        expect(plugins).to all be_persisted
+      end
+
+      it 'does replace instances if refs are different'do
+        action = page.plugins.select{|p| p.name == "Action" }.first
+        expect(action).to be_persisted
+        expect{ switcher.switch(thermo_action_ref_layout) }.to change{ Plugins::Action.count }.by 0
+        expect{ action.reload }.to raise_error ActiveRecord::RecordNotFound
+        new_action = page.plugins.select{|p| p.name == "Action" }.first
+        expect(new_action.id).not_to eq action.id
+      end
+
+      it 'does replace one instance but not the other' do
+        thermo = page.plugins.select{|p| p.name == "Thermometer" }.first
+        expect{ switcher.switch(thermo_action_ref_layout) }.to change{ Plugins::Thermometer.count }.by 0
+        expect{ thermo.reload }.not_to raise_error
+        new_thermo = page.plugins.select{|p| p.name == "Thermometer" }.first
+        expect(new_thermo.id).to eq thermo.id
+      end
     end
 
     describe 'destroying' do
-      it 'destroys all plugins when switching to a template without plugins'
+
+      before :each do
+        switcher.switch(both_refless_layout)
+      end
+
+      it 'destroys all plugins when switching to a template without plugins' do
+        plugins = page.plugins
+        expect{ switcher.switch(blank_layout) }.to change{ Plugins::Thermometer.count }.by(-1).and change{ Plugins::Action.count }.by(-1)
+        plugins.each do |plugin|
+          expect{ plugin.reload }.to raise_error ActiveRecord::RecordNotFound
+        end
+      end
     end
 
   end
