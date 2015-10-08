d1c706ef788a529587f988ead258e54ff17ca6fc
Neal Donnelly <NealJMD@gmail.com>
2015-10-08 15:45:41 -0600

    add version of thermometer matching agency design to action sidebar

--- app/models/plugins/thermometer.rb
+++ app/models/plugins/thermometer.rb
@@ -1,3 +1,5 @@
+include ActionView::Helpers::NumberHelper
+
 class Plugins::Thermometer < ActiveRecord::Base
   belongs_to :page
 
@@ -6,13 +8,21 @@ class Plugins::Thermometer < ActiveRecord::Base
   validates :goal, :offset, presence: true
   validates :goal, :offset, numericality: { greater_than_or_equal_to: 0 }
 
-  def get_current
-    count = page.actions.count
-    ( offset + count ) / goal * 100
+  def current_total
+    offset + page.actions.count
+  end
+
+  def current_progress
+    current_total / goal.to_f * 100
   end
 
   def liquid_data
-    attributes.merge('current' => get_current)
+    attributes.merge(
+      percentage: current_progress,
+      remaining: number_with_delimiter(goal - current_total),
+      signatures: number_with_delimiter(current_total),
+      goal_k: "#{(goal / 1000).to_i}k"
+    )
   end
 
   def name
