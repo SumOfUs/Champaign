8f373b3596a51fef849cd68f5cb5714cbe929395
Eric Boersma <eric.boersma@gmail.com>
2015-10-16 15:18:17 -0400

    Update action count on page after action creation.

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,6 +1,9 @@
 class Action < ActiveRecord::Base
   belongs_to :page
   belongs_to :action_user
+  after_create :update_page_action_count
 
+  def update_page_action_count
+    self.page.action_count = self.page.actions.count
+  end
 end
-
