01fa8eb44afb36fc4a112f6f6c34e243c84d3ebf
tuuli <tuuli.pollanen@gmail.com>
2015-10-16 16:34:22 -0700

    Add more config into asset sync initializer since it wasn't accessing ENV vars on AWS

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,1 +1,8 @@
-AssetSync.config.run_on_precompile = false
+AssetSync.configure do |config|
+  config.fog_provider = ENV['FOG_PROVIDER']
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  config.fog_directory = ENV['FOG_DIRECTORY']
+  config.fog_region = ENV['FOG_REGION']
+  config.run_on_precompile = false
+end
