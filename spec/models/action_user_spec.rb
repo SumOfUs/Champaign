cf5955935fe78bd8f205333913991a329644000f
Neal Donnelly <NealJMD@gmail.com>
2015-11-04 16:30:57 -0600

    add super basic spec for long-standing pesky pending specs

--- spec/models/action_user_spec.rb
+++ spec/models/action_user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe ActionUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ActionUser do
+
+  let(:action_user) { create :action_user }
+  subject { action_user }
+
+  it { is_expected.to be_valid }
+  it { is_expected.to respond_to :title }
+  it { is_expected.to respond_to :email }
+  it { is_expected.to respond_to :country }
+  it { is_expected.to respond_to :first_name }
+  it { is_expected.to respond_to :last_name }
+  it { is_expected.to respond_to :city }
+  it { is_expected.to respond_to :postal_code }
+  it { is_expected.to respond_to :title }
+  it { is_expected.to respond_to :address1 }
+  it { is_expected.to respond_to :address2 }
+
 end
