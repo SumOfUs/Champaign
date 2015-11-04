476f571bc31b01efc3ebd4309daa7f27c4407fd3
Omar Sahyoun <osahyoun@gmail.com>
2015-11-04 17:25:58 +0000

    Unify buckets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     storage: :s3,
     s3_host_name: ENV['S3_HOST_NAME'],
     s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
+      bucket: ENV['FOG_DIRECTORY'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
