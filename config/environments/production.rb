30020091bf56b16a6da44d2776e00e67fe12e136
tuuli <tuuli.pollanen@gmail.com>
2015-11-05 15:24:04 -0800

    Configure behavior of static assets by env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,14 +25,15 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files =  ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files =  ENV['RAILS_SERVE_STATIC_FILES'] || false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # AssetSync with Rails 4.0.3+ doesn't work without this enabled!
+  config.assets.compile = ENV['COMPILE_STATIC'] || false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
