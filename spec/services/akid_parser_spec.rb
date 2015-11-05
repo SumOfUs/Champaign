61875cb19c025c8dd471c187cd74910f1576cb87
Neal Donnelly <NealJMD@gmail.com>
2015-11-05 17:19:56 -0600

    slightly beef up AkidParser spec

--- spec/services/akid_parser_spec.rb
+++ spec/services/akid_parser_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 describe AkidParser do
-  let(:valid_string) {'14203.7145943.Si3iNO'}
   let(:valid_mailing_id) {'14203'}
-  let(:valid_akid) {'7145943'}
-  let(:invalid_string) {'Not a valid akid'}
-  let(:valid_return) { {mailing_id: valid_mailing_id, actionkit_user_id: valid_akid}}
+  let(:valid_actionkit_user_id) {'7145943'}
+  let(:valid_akid) {"#{valid_mailing_id}.#{valid_actionkit_user_id}.Si3iNO"}
+  let(:valid_return) { {mailing_id: valid_mailing_id, actionkit_user_id: valid_actionkit_user_id}}
   let(:invalid_return) { {mailing_id: nil, actionkit_user_id: nil} }
 
-  it 'validates the provided akid before working on it' do
-    expect(AkidParser.new(invalid_string).invalid?).to eq(true)
-    expect(AkidParser.parse(invalid_string)).to eq(invalid_return)
+  it 'correctly parses a valid akid' do
+    expect(AkidParser.new(valid_akid).invalid?).to eq false
+    expect(AkidParser.parse(valid_akid)).to eq(valid_return)
   end
 
-  it 'correctly parses a valid akid' do
-    expect(AkidParser.parse(valid_string)).to eq(valid_return)
+  it 'is invalid if akid is an actionkit_user_id' do
+    invalid_akid = valid_actionkit_user_id
+    expect(AkidParser.new(invalid_akid).invalid?).to eq true
+    expect(AkidParser.parse(invalid_akid)).to eq invalid_return
   end
+
+  it "is invalid if akid has only one period" do
+    invalid_akid = "#{valid_mailing_id}1#{valid_actionkit_user_id}.Si3iNO"
+    expect(AkidParser.new(invalid_akid).invalid?).to eq true
+    expect(AkidParser.parse(invalid_akid)).to eq invalid_return
+  end
+
+  it 'is invalid if it has 3 periods' do
+    invalid_akid = "#{valid_mailing_id}.#{valid_actionkit_user_id}.Si3.NO"
+    expect(AkidParser.new(invalid_akid).invalid?).to eq true
+    expect(AkidParser.parse(invalid_akid)).to eq invalid_return
+  end
+
 
 end
