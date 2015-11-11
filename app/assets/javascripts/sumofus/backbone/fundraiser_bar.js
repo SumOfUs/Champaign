7370f5f698d734a0971c6ed432f2076a4c640bdf
Neal Donnelly <NealJMD@gmail.com>
2015-11-11 16:54:49 -0600

    created html, css, and js for 1-2-3 donation circles

--- app/assets/javascripts/sumofus/backbone/fundraiser_bar.js
+++ app/assets/javascripts/sumofus/backbone/fundraiser_bar.js
@@ -5,19 +5,43 @@ const FundraiserBar = Backbone.View.extend(_.extend(StickyMethods, {
   el: '.fundraiser-bar',
 
   events: {
-
+    'click .fundraiser-bar__step-number': 'triggerStepChange'
   },
 
   initialize: function() {
-    console.log(this.$el);
-    console.log('efff')
     this.initializeSticky();
+    this.changeStep(1);
   },
 
   isMobile: function() {
     return $('.mobile-indicator').is(':visible');
   },
 
+  triggerStepChange: function(e) {
+    this.changeStep(this.$(e.target).data('step'));
+  },
+
+  changeStep: function(targetStep) {
+    // if (targetStep - this.currentStep > 1) {
+    //   targetStep = this.currentStep + 1; // max advance of 1
+    // }
+    this.$('.fundraiser-bar__step-number').
+      removeClass('fundraiser-bar__step-number--past').
+      removeClass('fundraiser-bar__step-number--current').
+      removeClass('fundraiser-bar__step-number--upcoming')
+
+    this.currentStep = targetStep;
+    this.$('.fundraiser-bar__step-number').each((ii, el) => {
+      const step = this.$(el).data('step');
+      if ( step < targetStep ) {
+        $(el).addClass('fundraiser-bar__step-number--past');
+      } else if ( step == targetStep) {
+        $(el).addClass('fundraiser-bar__step-number--current');
+      } else {
+        $(el).addClass('fundraiser-bar__step-number--upcoming');
+      }
+    });
+  }
 
 }));
 
