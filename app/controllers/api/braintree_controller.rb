5cf084e86c176e19d8fa7b76e5c86ba37d024bd3
tuuli <tuuli.pollanen@gmail.com>
2015-11-11 16:14:41 -0800

    Add basics for the transaction method, including namespacing (looks like it's inspired by java...)

--- app/controllers/api/braintree_controller.rb
+++ app/controllers/api/braintree_controller.rb
@@ -1,9 +1,10 @@
 class Api::BraintreeController < ApplicationController
   def token
-    render json: {token: Braintree::ClientToken.generate}
+    render json: {token: ::Braintree::ClientToken.generate}
   end
 
   def transaction
-
+    # we could use permitted params instead of params.except(:action, :controller)
+    render json: PaymentProcessor::Clients::Braintree::Transaction.make_transaction(params.except(:action, :controller))
   end
 end
