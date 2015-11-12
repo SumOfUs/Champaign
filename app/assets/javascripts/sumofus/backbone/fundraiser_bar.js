9d7746ef832fb955e287ef68bca0f5f9073e27b8
Neal Donnelly <NealJMD@gmail.com>
2015-11-12 17:03:59 -0600

    advance form when donation amount selected

--- app/assets/javascripts/sumofus/backbone/fundraiser_bar.js
+++ app/assets/javascripts/sumofus/backbone/fundraiser_bar.js
@@ -7,12 +7,15 @@ const FundraiserBar = Backbone.View.extend(_.extend(StickyMethods, {
   events: {
     'click .fundraiser-bar__step-number': 'triggerStepChange',
     'focus .fundraiser-bar__custom-field': 'primeCustom',
-    'blur .fundraiser-bar__custom-field': 'resetCustom',
+    'blur  .fundraiser-bar__custom-field': 'resetCustom',
+    'click .fundraiser-bar__amount-button': 'advanceToDetails',
+    'click .fundraiser-bar__first-continue': 'advanceToDetails',
   },
 
   initialize: function() {
     this.initializeSticky();
     this.changeStep(1);
+    this.donationAmount = 0;
   },
 
   isMobile: function() {
@@ -32,6 +35,17 @@ const FundraiserBar = Backbone.View.extend(_.extend(StickyMethods, {
     if ($field.val() == '$' || $field.val() == '') {
       $field[0].value = '';
       this.$('.fundraiser-bar__first-continue').slideUp(200);
+    }
+  },
+
+  advanceToDetails: function(e) {
+    let amount = this.$(e.target).data('amount') || this.$('.fundraiser-bar__custom-field').val();
+    if (typeof amount == 'string' && amount.indexOf('$') > -1) {
+      amount = amount.replace('$', '');
+    }
+    this.donationAmount = parseFloat(amount);
+    if (this.donationAmount > 0) {
+      this.changeStep(2)
     }
   },
 
