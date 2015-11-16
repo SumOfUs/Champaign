69f317a5ac1043b01e6ba29e766179546d2d8ad1
Omar Sahyoun <osahyoun@gmail.com>
2015-11-16 14:13:27 +0100

    Filter braintree credentials. Set braintree dummy credentials in test.rb

--- spec/requests/api/braintree_spec.rb
+++ spec/requests/api/braintree_spec.rb
@@ -4,15 +4,22 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+
+  # The filter_sensitive_data configuration option prevents
+  # sensitive data from being written to your cassette files.
+  #
+  %w{BRAINTREE_MERCHANT_ID BRAINTREE_PUBLIC_KEY BRAINTREE_PRIVATE_KEY}.each do |env|
+    config.filter_sensitive_data("<#{env}>") { ENV[env] }
+  end
 end
 
 describe "Braintree API" do
   describe "making a transaction" do
     it 'returns transaction code' do
       VCR.use_cassette("transaction_success") do
-        post '/api/braintree/transaction', payment_method_nonce: 'fake-valid-nonce', amount: 100.00, user: {email: 'bob@example.com', id: '123' }
+        post '/api/braintree/transaction', payment_method_nonce: 'fake-valid-nonce', amount: 100.00, user: {email: 'foo@example.com', id: '567' }
 
-        expect(response.body).to eq({success: true, transaction_id: '82n7vy'}.to_json)
+        expect(response.body).to eq({success: true, transaction_id: 'bvzfhp'}.to_json)
       end
     end
   end
