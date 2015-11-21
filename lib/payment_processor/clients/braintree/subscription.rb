e724572e4790b7e7b258f0eda7fa84a178690769
tuuli <tuuli.pollanen@gmail.com>
2015-11-20 17:11:28 -0800

    Add service class for managing subscriptions

---
+++ lib/payment_processor/clients/braintree/subscription.rb
@@ -0,0 +1,35 @@
+module PaymentProcessor
+  module Clients
+    module Braintree
+      class Subscription
+        def self.make_subscription(amount:, plan_id:, payment_method_token:)
+          new(amount, plan_id, payment_method_token).subscribe
+        end
+
+        def initialize(amount, plan_id, payment_method_token)
+          @amount = amount
+          @plan_id = plan_id
+          @payment_method_token = payment_method_token
+        end
+
+        def subscribe
+          ::Braintree::Subscription.create(options)
+        end
+
+        private
+
+        def find_user
+
+        end
+
+        def options
+          {
+              payment_method_token: @token,
+              plan_id: @plan_id
+          }
+        end
+
+      end
+    end
+  end
+end
