a37ca49c1ac1be0dd2939135dc850b33498d2e7f
tuuli <tuuli.pollanen@gmail.com>
2015-11-23 16:56:20 -0800

    Add option to subscribe through a transaction per Omar's descriptions

--- lib/payment_processor/clients/braintree/subscription.rb
+++ lib/payment_processor/clients/braintree/subscription.rb
@@ -6,6 +6,13 @@ module PaymentProcessor
           new(price, plan_id, payment_method_token).subscribe
         end
 
+        def self.make_subscription_from_transaction(result)
+          price = result.transaction.amount
+          plan_id = ENV['BRAINTREE_SUBSCRIPTION_PLAN_ID']
+          payment_method_token = result.transaction.credit_card_details.token
+          new(price, plan_id, payment_method_token).subscribe
+        end
+
         def initialize(price, plan_id, payment_method_token)
           @price = price
           @plan_id = plan_id
@@ -20,9 +27,9 @@ module PaymentProcessor
 
         def options
           {
-              payment_method_token: @payment_method_token,
-              plan_id: @plan_id,
-              price: @price
+            payment_method_token: @payment_method_token,
+            plan_id: @plan_id,
+            price: @price
           }
         end
 
