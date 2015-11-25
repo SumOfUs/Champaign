1204261573399bb79c86e2e90e123a666bbce5f3
Omar Sahyoun <osahyoun@gmail.com>
2015-11-25 16:18:34 +0100

    API for mass converting currencies

---
+++ spec/lib/donations/currencies_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Donations::Currencies do
+  describe '#for' do
+    it 'converts and preents listed currencies as JSON' do
+      VCR.use_cassette('donation_currencies') do
+        expect(
+          Donations::Currencies.for([102,204,302,401]).to_json
+        ).to eq( {
+          USD: [1.02, 2.04, 3.02, 4.01],
+          GBP: [0.67, 1.35, 2.0, 2.65],
+          EUR: [0.96, 1.92, 2.85, 3.79]
+        }.to_json )
+      end
+    end
+  end
+end
+
