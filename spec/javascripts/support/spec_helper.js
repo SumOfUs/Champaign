d0ae7f2cd0988a235857020b13f6f6f2a0d93d84
Neal Donnelly <NealJMD@gmail.com>
2015-11-25 18:28:50 -0500

    restore teaspoon spec_helper comments, ditch spy helpers

--- spec/javascripts/support/spec_helper.js
+++ spec/javascripts/support/spec_helper.js
@@ -4,42 +4,28 @@
 //= require chai-jquery
 //= require magic_lamp
 //= require support/sinon-chai
-
 //= require support/helper_functions
 
-// require_self
+// PhantomJS (Teaspoons default driver) doesn't have support for Function.prototype.bind, which has caused confusion.
+// Use this polyfill to avoid the confusion.
+//= require support/phantomjs-shims
+
+// You can require your own javascript files here. By default this will include everything in application, however you
+// may get better load performance if you require the specific files that are being used in the spec that tests them.
+// require application
+//
+// Deferring execution
+// If you're using CommonJS, RequireJS or some other asynchronous library you can defer execution. Call
+// Teaspoon.execute() after everything has been loaded. Simple example of a timeout:
+//
+// Teaspoon.defer = true
+// setTimeout(Teaspoon.execute, 1000)
+//
+// Matching files
+// By default Teaspoon will look for files that match _spec.{js,js.coffee,.coffee}. Add a filename_spec.js file in your
+// spec path and it'll be included in the default suite automatically. If you want to customize suites, check out the
+// configuration in teaspoon_env.rb
 
 jQuery.fx.off = true;
 window.expect = chai.expect;
-
-// var spies;
-// var stubs;
-// window.spyOn = function(object, method) {
-//   var spy = sinon.spy(object, method);
-//   spies.push(spy);
-//   return spy;
-// };
-
-// window.stub = function(object, method, retVal) {
-//   var stub = sinon.stub(object, method).returns(retVal);
-//   stubs.push(stub);
-//   return stub;
-// };
-
-// beforeEach(function() {
-//   spies = [];
-//   stubs = [];
-// });
-
-// afterEach(function() {
-//   MagicLamp.clean();
-
-//   _(spies).each(function(spy) {
-//     spy.restore();
-//   });
-
-//   _(stubs).each(function(stub) {
-//     stub.restore();
-//   });
-// });
 
