f2f620c83800952dc73f93d37dc7c65994ebdbbf
tuuli <tuuli.pollanen@gmail.com>
2015-11-24 16:52:55 -0800

    Make transcription use subscription logic when the payment is set recurring, and extract the logic out into separate methods.

--- app/controllers/api/braintree_controller.rb
+++ app/controllers/api/braintree_controller.rb
@@ -7,6 +7,20 @@ class Api::BraintreeController < ApplicationController
   end
 
   def transaction
+    if params[:recurring]
+      manage_subscription(params)
+    else
+      manage_transaction(params)
+    end
+  end
+
+  def subscription
+    manage_subscription(params)
+  end
+
+  private
+
+  def manage_transaction(params)
     result = braintree::Transaction.make_transaction(transaction_options)
     if result.success?
       render json: { success: true, transaction_id: result.transaction.id }
@@ -15,26 +29,35 @@ class Api::BraintreeController < ApplicationController
     end
   end
 
-  def subscription
+  def manage_subscription(params)
+    find_or_create_user(params)
     result = braintree::Subscription.make_subscription(subscription_options)
     if result.success?
       render json: { success: true, subscription_id: result.subscription.id }
     else
-      # Subscription-specific errors can be accessed with results.errors.for(:subscription),
-      # but that would not include errors due to invalid parameters.
-      # Subscription-specific errors are raised e.g. if there's an attempt to update a deleted subscription.
       render json: { success: false, errors: result.errors }
     end
   end
 
-  private
+  def find_or_create_user(params)
+    # If user isn't associated with a token locally, create and persist the customer first
+    if default_payment_method_token.blank?
+      customer = braintree::Customer.create({ email: params[:email] })
+      if not customer.success?
+        # render customer creation failure json - it's pointless to continue if there's no user (subscription will fail)
+        render json: { success: false, errors: customer.errors }
+      else
+        # persist customer locally
+        Payment::BraintreeCustomer.create!({ email: params[:email], card_vault_token: customer.card.token})
+      end
+    end
+  end
 
   def transaction_options
     {
       nonce: params[:payment_method_nonce],
-      user: params[:user],
       amount: params[:amount].to_f,
-      recurring: params[:recurring] || false,
+      user: params[:user],
       store: Payment
     }
   end
