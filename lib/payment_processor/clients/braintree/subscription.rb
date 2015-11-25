168eeeb94d57c37b6c8e466b7823e5f10eee86fc
tuuli <tuuli.pollanen@gmail.com>
2015-11-24 16:53:41 -0800

    Remove methods and parameters for making a subscription through a transaction.

--- lib/payment_processor/clients/braintree/subscription.rb
+++ lib/payment_processor/clients/braintree/subscription.rb
@@ -6,13 +6,6 @@ module PaymentProcessor
           new(price, plan_id, payment_method_token).subscribe
         end
 
-        def self.make_subscription_from_transaction(result)
-          price = result.transaction.amount
-          plan_id = ENV['BRAINTREE_SUBSCRIPTION_PLAN_ID']
-          payment_method_token = result.transaction.credit_card_details.token
-          new(price, plan_id, payment_method_token).subscribe
-        end
-
         def initialize(price, plan_id, payment_method_token)
           @price = price
           @plan_id = plan_id
