51715e3aafa348de99bb805fe47eda4676b8ee90
Omar Sahyoun <osahyoun@gmail.com>
2015-12-02 20:32:40 +0100

    Sort out crazy factory girl indentation

--- spec/factories/payment_braintree_subscriptions.rb
+++ spec/factories/payment_braintree_subscriptions.rb
@@ -1,12 +1,11 @@
 FactoryGirl.define do
-  factory :payment_braintree_subscription, :class => 'Payment::BraintreeSubscription' do
+  factory :payment_braintree_subscription, class: 'Payment::BraintreeSubscription' do
     subscription_id "MyString"
-next_billing_date "2015-12-02 17:27:01"
-plan_id "MyString"
-price "MyString"
-status "MyString"
-merchant_account_id "MyString"
-customer_id "MyString"
+    next_billing_date "2015-12-02 17:27:01"
+    plan_id "MyString"
+    price "MyString"
+    status "MyString"
+    merchant_account_id "MyString"
+    customer_id "MyString"
   end
-
 end
