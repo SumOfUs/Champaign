ec5672443a80b8cfb8542bbc75da88dc03eaa227
Omar Sahyoun <osahyoun@gmail.com>
2015-12-03 18:15:07 +0100

    Use bundle exec

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,6 +1,8 @@
 Braintree::Configuration.environment = :sandbox
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
-
+# DEBUG:
+puts "Debug Settings:"
+p Settings
 Braintree::Configuration.merchant_id = Settings.braintree.merchant_id
 Braintree::Configuration.public_key = Settings.braintree.public_key
 Braintree::Configuration.private_key = Settings.braintree.private_key
