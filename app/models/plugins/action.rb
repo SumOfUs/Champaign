02646cfcc003e2d8c0c43eee1022fb369be50da5
Neal Donnelly <NealJMD@gmail.com>
2015-12-08 16:41:31 -0600

    refactor shared plugin form code into concern

--- app/models/plugins/action.rb
+++ app/models/plugins/action.rb
@@ -1,37 +1,12 @@
 class Plugins::Action < ActiveRecord::Base
+  include Plugins::HasForm
+
   belongs_to :page
-  belongs_to :form
-
   validates :cta, presence: true, allow_blank: false
-
-  after_create :create_form
 
   DEFAULTS = { cta: 'Sign the Petition' }
 
   def liquid_data(supplemental_data={})
-    attributes.merge(
-      form_id: form.try(:id),
-      fields: form_fields,
-      outstanding_fields: outstanding_fields(supplemental_data[:form_values]).map(&:to_s)
-    )
-  end
-
-  def form_fields
-    form ? form.form_elements.map(&:attributes) : []
-  end
-
-  def name
-    self.class.name.demodulize
-  end
-
-  def outstanding_fields(form_values)
-    return [] if form.blank?
-    FormValidator.new({form_id: form_id}.merge(form_values || {})).errors.keys
-  end
-
-  private
-
-  def create_form
-    update(form: Form.create(master: false, name: "action:#{id}"))
+    attributes.merge(form_liquid_data(supplemental_data))
   end
 end
--- app/models/plugins/fundraiser.rb
+++ app/models/plugins/fundraiser.rb
@@ -1,38 +1,13 @@
 class Plugins::Fundraiser < ActiveRecord::Base
+  include Plugins::HasForm
+
   belongs_to :page
-  belongs_to :form
   belongs_to :donation_band
 
   DEFAULTS = { title: 'Donate now' }
 
-  after_create :create_form
-
   def liquid_data(supplemental_data={})
     bands = donation_band.present? ? donation_band.internationalize.to_json : "null"
-    attributes.merge(
-      form_id: form_id,
-      fields: form_fields,
-      donation_bands: bands,
-      outstanding_fields: outstanding_fields(supplemental_data[:form_values]).map(&:to_s)
-    )
-  end
-
-  def form_fields
-    form ? form.form_elements.map(&:attributes) : []
-  end
-
-  def name
-    self.class.name.demodulize
-  end
-
-  def outstanding_fields(form_values)
-    return [] if form_id.blank?
-    FormValidator.new({form_id: form_id}.merge(form_values || {})).errors.keys
-  end
-
-  private
-
-  def create_form
-    update(form: Form.create(master: false, name: "fundraiser:#{id}"))
+    attributes.merge(form_liquid_data(supplemental_data)).merge(donation_bands: bands)
   end
 end
