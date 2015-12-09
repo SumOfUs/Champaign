feb8bce0908ef1efe7363c1cbf5f0220bbf3ba74
Eric Boersma <eric.boersma@gmail.com>
2015-12-09 10:55:57 -0500

    Changing Donation::BandFinder to work on find_by

--- lib/donations/band_finder.rb
+++ lib/donations/band_finder.rb
@@ -2,16 +2,10 @@ module Donations
   class BandFinder
     class << self
       def find_band(band_name, backup_id)
-        name_find = DonationBand.where(name: band_name).first
-        if name_find
-          name_find
-        else
-          begin
-            DonationBand.find(backup_id)
-          rescue
-            DonationBand.first
-          end
-        end
+        band = DonationBand.find_by(name: band_name)
+        band = DonationBand.find_by(id: backup_id) if band.blank?
+        band = DonationBand.first if band.blank?
+        band
       end
     end
   end
