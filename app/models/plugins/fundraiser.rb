181ed9d091f39116ca3678c38c6407f3ebce26dc
Eric Boersma <eric.boersma@gmail.com>
2015-12-10 16:25:07 -0500

    Tweaking the way we find donation bands to allow us to specify by default or URL Param.

--- app/models/plugins/fundraiser.rb
+++ app/models/plugins/fundraiser.rb
@@ -7,7 +7,7 @@ class Plugins::Fundraiser < ActiveRecord::Base
   DEFAULTS = { title: 'Donate now' }
 
   def liquid_data(supplemental_data={})
-    bands = Donations::BandFinder.find_band(supplemental_data[:url_params][:donation_band], donation_band.id)
+    bands = Donations::BandFinder.find_band(supplemental_data[:url_params][:donation_band], donation_band.id).internationalize.to_json
     attributes.merge(form_liquid_data(supplemental_data)).merge(donation_bands: bands)
   end
 end
