5a60bba1b1db34c193c653ab8fe8228f41d30a87
Eric Boersma <eric.boersma@gmail.com>
2015-12-10 17:02:40 -0500

    Fixing liquid_data to conform to the existing expectations of that method.

--- app/models/plugins/fundraiser.rb
+++ app/models/plugins/fundraiser.rb
@@ -7,7 +7,23 @@ class Plugins::Fundraiser < ActiveRecord::Base
   DEFAULTS = { title: 'Donate now' }
 
   def liquid_data(supplemental_data={})
-    bands = Donations::BandFinder.find_band(supplemental_data[:url_params][:donation_band], donation_band.id).internationalize.to_json
+    donation_band_name = ''
+    backup_id = nil
+
+    if supplemental_data.has_key? :url_params
+      donation_band_name = supplemental_data[:url_params][:donation_band]
+    end
+    if donation_band.present?
+      backup_id = donation_band.id
+    end
+
+    bands = Donations::BandFinder.find_band(donation_band_name, backup_id)
+
+    if bands
+      bands = bands.internationalize.to_json
+    else
+      bands = 'null'
+    end
     attributes.merge(form_liquid_data(supplemental_data)).merge(donation_bands: bands)
   end
 end
