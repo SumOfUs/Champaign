386335f03f8ba8586aea57b54ea2f271e4b068cb
Neal Donnelly <NealJMD@gmail.com>
2015-12-15 15:56:37 -0500

    make thermo spec more succinct, throw in some non-round numbers

--- spec/models/plugins/thermometer_spec.rb
+++ spec/models/plugins/thermometer_spec.rb
@@ -36,72 +36,62 @@ describe Plugins::Thermometer do
   end
 
   it 'correctly jumps between 100 and 500' do
-    test_page.action_count = 101
-    test_page.save
+    test_page.update_attributes(action_count: 101)
     thermometer.update_goal
     expect(thermometer.goal).to eq(200)
   end
 
   it 'correctly jumps between 500 and 1K' do
-    test_page.action_count = 501
-    test_page.save
+    test_page.update_attributes(action_count: 501)
     thermometer.update_goal
     expect(thermometer.goal).to eq(2000)
   end
 
   it 'correctly jumps between 1K and 10K' do
-    test_page.action_count = 1000
-    test_page.save
+    test_page.update_attributes(action_count: 1320)
     thermometer.update_goal
     expect(thermometer.goal).to eq(2000)
   end
 
   it 'correctly jumps between 10K and 25K' do
-    test_page.action_count = 10000
-    test_page.save
+    test_page.update_attributes(action_count: 10000)
     thermometer.update_goal
     expect(thermometer.goal).to eq(15000)
   end
 
   it 'correctly jumps between 25K and 100K' do
-    test_page.action_count = 25000
-    test_page.save
+    test_page.update_attributes(action_count: 25000)
     thermometer.update_goal
     expect(thermometer.goal).to eq(50000)
   end
 
   it 'correctly jumps between 100K and 250K' do
-    test_page.action_count = 100000
-    test_page.save
+    test_page.update_attributes(action_count: 120001)
     thermometer.update_goal
     expect(thermometer.goal).to eq(150000)
   end
 
   it 'correctly jumps between 250K and 1MM' do
-    test_page.action_count = 250000
-    test_page.save
+    test_page.update_attributes(action_count: 250000)
     thermometer.update_goal
     expect(thermometer.goal).to eq(500000)
   end
 
   it 'correctly jumps between 1MM and 2MM' do
-    test_page.action_count = 1000000
-    test_page.save
+    test_page.update_attributes(action_count: 1000000)
     thermometer.update_goal
     expect(thermometer.goal).to eq(1500000)
   end
 
   it 'correctly jumps over 2MM' do
-    test_page.action_count = 2000000
-    test_page.save
+    test_page.update_attributes(action_count: 2000000)
     thermometer.update_goal
     expect(thermometer.goal).to eq(3000000)
   end
 
   it 'correctly increments the goal even when the number is much higher' do
     expect(thermometer.goal).to eq(100)
-    test_page.action_count = 51248
-    test_page.save
+    test_page.update_attributes(action_count: 51248)
     thermometer.update_goal
     expect(thermometer.goal).to eq(75000)
   end
