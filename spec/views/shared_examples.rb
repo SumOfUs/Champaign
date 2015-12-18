4e9a3f7a58f1b2d6dde4313950c50c4f36e237b8
Neal Donnelly <NealJMD@gmail.com>
2015-12-18 17:33:06 +0000

    add remaining view smoke tests and fix missing translations

--- spec/views/shared_examples.rb
+++ spec/views/shared_examples.rb
@@ -1,52 +1,61 @@
 require 'rails_helper'
 
-shared_examples "view smoke test" do |model_sym|
+shared_examples "view smoke test" do |model_sym, actions|
 
+  actions ||= [:new, :edit, :show, :index]
   model_plural = model_sym.to_s.pluralize.to_sym
-  model_class = model_sym.to_s.camelcase.constantize.new
+  model_class = model_sym.to_s.camelcase.constantize
 
   before do
     view.lookup_context.view_paths.push "app/views/shared"
     view.lookup_context.view_paths.push "app/views/#{model_plural}"
   end
 
-  describe "new" do
-    it 'renders without error' do
-      assign model_sym, model_class
-      expect{ render template: "#{model_plural}/new" }.not_to raise_error
+  if actions.include? :new
+    describe "new" do
+      it 'renders without error' do
+        assign model_sym, model_class.new
+        expect{ render template: "#{model_plural}/new" }.not_to raise_error
+      end
     end
   end
 
-  describe "edit" do
-    it 'renders without error' do
-      assign model_sym, build(model_sym, id: 1)
-      expect{ render template: "#{model_plural}/edit" }.not_to raise_error
+  if actions.include? :edit
+    describe "edit" do
+      it 'renders without error' do
+        assign model_sym, build(model_sym, id: 1)
+        expect{ render template: "#{model_plural}/edit" }.not_to raise_error
+      end
     end
   end
 
-  describe "show" do
-    it 'renders without error' do
-      assign model_sym, build(model_sym, id: 1)
-      expect{ render template: "#{model_plural}/show" }.not_to raise_error
+  if actions.include? :show
+    describe "show" do
+      it 'renders without error' do
+        assign model_sym, build(model_sym, id: 1)
+        expect{ render template: "#{model_plural}/show" }.not_to raise_error
+      end
     end
   end
 
-  describe "index" do
-    it "renders no #{model_plural} without error" do
-      assign model_plural, Campaign.none
-      expect{ render template: "#{model_plural}/index" }.not_to raise_error
-    end
+  if actions.include? :index
+    describe "index" do
+      it "renders no #{model_plural} without error" do
+        assign model_plural, Campaign.none
+        expect{ render template: "#{model_plural}/index" }.not_to raise_error
+      end
 
-    it "renders multiple #{model_plural} without error" do
-      3.times { create model_sym }
-      assign model_plural, Campaign.all
-      expect{ render template: "#{model_plural}/index" }.not_to raise_error
-    end
+      it "renders multiple #{model_plural} without error" do
+        3.times { create model_sym }
+        assign model_plural, Campaign.all
+        expect{ render template: "#{model_plural}/index" }.not_to raise_error
+      end
 
-    it "renders a single campaign without error" do
-      create model_sym
-      assign model_plural, Campaign.all
-      expect{ render template: "#{model_plural}/index" }.not_to raise_error
+      it "renders a single campaign without error" do
+        create model_sym
+        assign model_plural, Campaign.all
+        expect{ render template: "#{model_plural}/index" }.not_to raise_error
+      end
     end
   end
 end
--- spec/views/smoke_tests.rb
+++ spec/views/smoke_tests.rb
@@ -5,6 +5,32 @@ describe 'campaigns/' do
   include_examples "view smoke test", :campaign
 end
 
+describe 'donation_bands/' do
+  include_examples "view smoke test", :donation_band, [:edit, :index, :new]
+end
+
+describe 'forms/' do
+  include_examples "view smoke test", :form, [:index, :new]
+
+  describe "edit" do
+    it 'renders without error' do
+      # form = create(:form_with_email, id: 1)
+      # puts "form has #{form.form_elements.size}"
+      assign :form, build(:form, id: 1)
+      assign :form_element, FormElement.new
+      expect{ render template: "forms/edit" }.not_to raise_error
+    end
+  end
+end
+
+describe 'liquid_layouts/' do
+  include_examples "view smoke test", :liquid_layout
+end
+
 describe 'liquid_partials/' do
   include_examples "view smoke test", :liquid_partial
+end
+
+describe 'pages/' do
+  include_examples "view smoke test", :page, [:index, :new, :show]
 end
