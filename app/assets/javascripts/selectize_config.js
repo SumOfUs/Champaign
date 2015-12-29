8382b5e889bc9f05f93d1eac579033ae82a5165f
Neal Donnelly <NealJMD@gmail.com>
2015-12-29 15:05:15 -0500

    make interface for selecting layout that displays description

--- app/assets/javascripts/selectize_config.js
+++ app/assets/javascripts/selectize_config.js
@@ -3,4 +3,10 @@ $(function(){
     plugins: ['remove_button'],
     closeAfterSelect: true
   });
+
+  $('.radio-group input[type="radio"]').on('change', function(e){
+    var $target = $(e.target);
+    $target.parents('.radio-group').find('.radio-group__option').removeClass('active');
+    $target.parents('.radio-group__option').addClass('active');
+  });
 });
