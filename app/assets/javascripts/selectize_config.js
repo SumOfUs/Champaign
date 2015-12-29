8a6a2df473c13236fcc56768c924029e0b97f081
Neal Donnelly <NealJMD@gmail.com>
2015-12-29 17:15:50 -0500

    change displayed layout name when it changes

--- app/assets/javascripts/selectize_config.js
+++ app/assets/javascripts/selectize_config.js
@@ -6,6 +6,8 @@ $(function(){
 
   $('.radio-group input[type="radio"]').on('change', function(e){
     var $target = $(e.target);
+    var name = $target.parents('.radio-group__option').find('.layout-settings__title').text();
+    $target.parents('.layout-settings').find('.layout-settings__current').text(name);
     $target.parents('.radio-group').find('.radio-group__option').removeClass('active');
     $target.parents('.radio-group__option').addClass('active');
   });
