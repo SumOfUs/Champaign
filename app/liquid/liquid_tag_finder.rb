f72d417a5d4a1095c65886e8dd9b02baa581149f
Neal Donnelly <NealJMD@gmail.com>
2015-12-30 16:16:18 -0500

    added documentation to the rather cryptic LiquidTagFinder class

--- app/liquid/liquid_tag_finder.rb
+++ app/liquid/liquid_tag_finder.rb
@@ -1,3 +1,16 @@
+# The <tt>LiquidTagFinder</tt> class is used to help the app understand
+# the needs and the setup of liquid layouts and partials by parsing
+# relevant liquid tags.
+#
+# An instance is initialized with a string holding the liquid markup
+# to be analyzed. Each instance exposes five public methods which are
+# documented inline.
+#
+# This class's functionality hinges on accessing instance variables
+# of the +Liquid+ parser's internal classes. For that reason, it may be
+# a bit fragile and break if the Liquid version changes. It was designed
+# with Liquid version 3.0.3.
+#
 class LiquidTagFinder
 
   def initialize(content)
@@ -8,25 +21,50 @@ class LiquidTagFinder
     @template ||= Liquid::Template.parse(@content)
   end
 
+  # Returns the name of every plugin referenced in the liquid markup.
+  # For example, if there is any liquid tag (whether flow control or
+  # display) that references +plugins.petition[ref].fields+, then
+  # 'petition' will be in the list returned by this method.
   def plugin_names
     markups = all_liquid_tags.map{ |li| li.instance_values['markup'] }
     plugins = markups.map{|markup| markup.match(/plugins\.([a-zA-Z0-9_]+)/) }
     plugins.select(&:present?).map{|matches| matches[1]}.uniq
   end
 
+  # Returns the names of every partial referenced in the liquid markup.
+  # More examples can be found in the LiquidTagFinderSpec
+  # For example:
+  #     {% include 'thermometer', ref: 'modal' %}
+  # would return
+  #     ['thermometer']
   def partial_names
     all_include_tags.map{ |incl| partial_name_from_include(incl) }
   end
 
+  # Returns the names of every partial referenced in the liquid markup,
+  # along with the ref passed with it. More examples in the spec.
+  # Refs serve to allow multiple of one plugin on a page.
+  # For example:
+  #     {% include 'thermometer', ref: 'modal' %}
+  # would return
+  #     [['thermometer', 'modal']]
   def partial_refs
     all_refs = all_include_tags.map{ |incl| [partial_name_from_include(incl), ref_from_include(incl)] }
     all_refs.uniq # one form for multiple refless/ same reffed templates
   end
 
+  # Looks for a liquid comment like
+  #   {% comment %} Description: this is the description {% endcomment %}
+  # If one is not found, it will return +nil+. If one is found, it will return
+  #   'this is the description'
   def description
     string_search(/description: *(.+)/i, string_comments.flatten)
   end
 
+  # Looks for a liquid comment like
+  #   {% comment %} Experimental: true {% endcomment %}
+  # If the tag is found and the value is +true+, this returns +true+.
+  # Otherwise it returns +false+.
   def experimental?
     status = string_search(/experimental: *(.+)/i, string_comments.flatten)
     status.present? ? status.match(/true/i).present? : false
