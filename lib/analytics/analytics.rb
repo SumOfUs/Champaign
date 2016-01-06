4fadd84d7d0ba0dffe45a18b0af91edb43237df4
Omar Sahyoun <osahyoun@gmail.com>
2016-01-06 18:23:21 +0000

    Store and present totals by day

--- lib/analytics/analytics.rb
+++ lib/analytics/analytics.rb
@@ -2,10 +2,9 @@ require 'redis'
 
 module Analytics
   def self.store
-    @redis ||= Redis.new(url: (ENV["REDIS_URL"] || 'redis://127.0.0.1:6379'))
+    @redis ||= Redis.new(url: (Settings.redis_url || 'redis://127.0.0.1:6379'))
   end
 end
-
 
 module Analytics
   class Page
@@ -17,22 +16,42 @@ module Analytics
       Analytics.store.get( key(new_members )).to_i
     end
 
-    def total_actions_over_time(period: :hours)
-      12.times.inject({}) do |memo, i|
-        hour = (Time.now.utc - i.send(:hour)).hour
-
-        memo[i] = Analytics.store.get(key_with_hour(hour)).to_i
-        memo
-      end
+    def total_actions_over_time(period: :hour, new_members: false)
+      send("total_by_#{period}", new_members)
     end
 
     def increment_actions(new_member: false)
       if new_member
-        Analytics.store.incr( key(new_member) )
+        incr( key(new_member) )
+        incr( key_with_hour( new_member: true ) )
+        incr( key_with_day(  new_member: true ) )
       end
 
-      Analytics.store.incr(key)
-      Analytics.store.incr( key_with_hour )
+      incr(key)
+      incr( key_with_hour )
+      incr( key_with_day  )
+    end
+
+    private
+
+    def total_by_hour(new_members_only)
+      12.times.inject({}) do |memo, i|
+        hour = (Time.now.utc - i.send(:hour)).hour
+
+        memo[i] = Analytics.store.get(key_with_hour(hour: hour, new_member: new_members_only)).to_i
+        memo
+      end
+    end
+
+    def total_by_day(new_members_only)
+      14.times.inject({}) do |memo, i|
+        date = Time.now.utc - i.send(:day)
+
+        day = date.strftime('%d/%m')
+
+        memo[day] = Analytics.store.get(key_with_day(day: date.day, new_member: new_members_only)).to_i
+        memo
+      end
     end
 
     def key(new_member = false)
@@ -41,9 +60,16 @@ module Analytics
       end
     end
 
-    def key_with_hour(hour = Time.now.utc.hour)
-      memo = "#{key}:hours:#{hour}"
-      memo
+    def key_with_hour(hour: Time.now.utc.hour, new_member: false)
+      "#{ key(new_member) }:hours:#{hour}"
+    end
+
+    def key_with_day(day: Time.now.utc.day, new_member: false)
+      "#{ key(new_member) }:days:#{day}"
+    end
+
+    def incr(key)
+      Analytics.store.incr( key )
     end
   end
 end
--- spec/lib/analytics/analytics_spec.rb
+++ spec/lib/analytics/analytics_spec.rb
@@ -20,14 +20,57 @@ describe Analytics do
   end
 
   describe '#total_actions_over_time' do
-    context 'over 12 hours' do
-
+    context 'by day' do
       before do
         Timecop.freeze('01-01-2000') do
           2.times{ subject.increment_actions }
+          subject.increment_actions(new_member: true)
+
+          Timecop.travel(1.day.ago) do
+            5.times{ subject.increment_actions }
+            3.times{ subject.increment_actions(new_member: true) }
+          end
+
+          Timecop.travel(3.days.ago) do
+            3.times{ subject.increment_actions }
+          end
+        end
+      end
+
+      it 'returns total actions by day' do
+        sample_of_expected_data = {
+          '01/01' => 3, '31/12' => 8, '29/12' => 3
+        }
+
+        Timecop.freeze('01-01-2000') do
+          expect(
+            subject.total_actions_over_time(period: :day)
+          ).to include( sample_of_expected_data )
+        end
+      end
+
+      it 'returns actions by new members by day' do
+        sample_of_expected_data = {
+          '01/01' => 1, '31/12' => 3, '29/12' => 0
+        }
+
+        Timecop.freeze('01-01-2000') do
+          expect(
+            subject.total_actions_over_time(period: :day, new_members: true)
+          ).to include( sample_of_expected_data )
+        end
+      end
+    end
+
+    context 'by hour' do
+      before do
+        Timecop.freeze('01-01-2000') do
+          2.times{ subject.increment_actions }
+          subject.increment_actions(new_member: true)
 
           Timecop.travel(1.hour.ago) do
             5.times{ subject.increment_actions }
+            3.times{ subject.increment_actions(new_member: true) }
           end
 
           Timecop.travel(3.hours.ago) do
@@ -44,12 +87,24 @@ describe Analytics do
 
       it 'returns total actions by hour' do
         sample_of_expected_data = {
-          0 => 2, 1 => 5, 3 => 3
+          0 => 3, 1 => 8, 3 => 3
         }
 
         Timecop.freeze('01-01-2000') do
           expect(
             subject.total_actions_over_time(period: :hour)
+          ).to include( sample_of_expected_data )
+        end
+      end
+
+      it 'returns actions by new members by hour' do
+        sample_of_expected_data = {
+          0 => 1, 1 => 3
+        }
+
+        Timecop.freeze('01-01-2000') do
+          expect(
+            subject.total_actions_over_time(period: :hour, new_members: true)
           ).to include( sample_of_expected_data )
         end
       end
