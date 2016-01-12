46f72e61cbc0ea15f28c40480fe441df983137b4
Eric Boersma <eric.boersma@gmail.com>
2016-01-12 11:21:38 -0500

    Adding some tests for full_name on Member model.

---
+++ spec/models/member_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Member do
+  let(:first_name) { 'Emilio' }
+  let(:last_name) { 'Estevez' }
+  let(:full_name) { "#{first_name} #{last_name}" }
+
+  it 'correctly joins first and last name' do
+    member = Member.new
+    member.first_name = first_name
+    member.last_name = last_name
+    expect(member.full_name).to eq(full_name)
+  end
+
+  it 'correctly splits full_name into first and last name' do
+    member = Member.new
+    member.full_name = full_name
+    expect(member.first_name).to eq(first_name)
+    expect(member.last_name).to eq(last_name)
+    expect(member.full_name).to eq(full_name)
+  end
+end
