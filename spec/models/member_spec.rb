42208488264a264ee9cfee8d275f44eb55539720
Eric Boersma <eric.boersma@gmail.com>
2016-01-13 16:10:12 -0500

    Add DB round trip to member specs.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -21,24 +21,30 @@ describe Member do
   it 'correctly splits full_name into first and last name' do
     member = Member.new
     member.name = full_name
-    expect(member.first_name).to eq(first_name)
-    expect(member.last_name).to eq(last_name)
-    expect(member.name).to eq(full_name)
+    member.save
+    new_member = Member.find(member.id)
+    expect(new_member.first_name).to eq(first_name)
+    expect(new_member.last_name).to eq(last_name)
+    expect(new_member.name).to eq(full_name)
   end
 
   it 'correctly handles unicode characters' do
     member = Member.new
     member.name = unicode_full
-    expect(member.first_name).to eq(unicode_first)
-    expect(member.last_name).to eq(unicode_last)
-    expect(member.name).to eq(unicode_full)
+    member.save
+    new_member = Member.find(member.id)
+    expect(new_member.first_name).to eq(unicode_first)
+    expect(new_member.last_name).to eq(unicode_last)
+    expect(new_member.name).to eq(unicode_full)
   end
 
   it 'correctly handles high-value unicode characters' do
     member = Member.new
     member.name = chinese_full
-    expect(member.first_name).to eq(chinese_first)
-    expect(member.last_name).to eq(chinese_last)
-    expect(member.name).to eq(chinese_full)
+    member.save
+    new_member = Member.find(member.id)
+    expect(new_member.first_name).to eq(chinese_first)
+    expect(new_member.last_name).to eq(chinese_last)
+    expect(new_member.name).to eq(chinese_full)
   end
 end
