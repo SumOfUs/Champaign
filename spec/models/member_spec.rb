a46ea286454f4a8f8c60515587e79404c69f7319
Eric Boersma <eric.boersma@gmail.com>
2016-01-13 15:58:38 -0500

    Adding unicode testing to Member spec

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -4,6 +4,12 @@ describe Member do
   let(:first_name) { 'Emilio' }
   let(:last_name) { 'Estevez' }
   let(:full_name) { "#{first_name} #{last_name}" }
+  let(:unicode_first) { 'Éöíñ'}
+  let(:unicode_last) { 'Ńūñèž' }
+  let(:unicode_full) { "#{unicode_first} #{unicode_last}" }
+  let(:chinese_first) { '台'}
+  let(:chinese_last) { '北' }
+  let(:chinese_full) { "#{chinese_first} #{chinese_last}"}
 
   it 'correctly joins first and last name' do
     member = Member.new
@@ -18,5 +24,21 @@ describe Member do
     expect(member.first_name).to eq(first_name)
     expect(member.last_name).to eq(last_name)
     expect(member.name).to eq(full_name)
+  end
+
+  it 'correctly handles unicode characters' do
+    member = Member.new
+    member.name = unicode_full
+    expect(member.first_name).to eq(unicode_first)
+    expect(member.last_name).to eq(unicode_last)
+    expect(member.name).to eq(unicode_full)
+  end
+
+  it 'correctly handles high-value unicode characters' do
+    member = Member.new
+    member.name = chinese_full
+    expect(member.first_name).to eq(chinese_first)
+    expect(member.last_name).to eq(chinese_last)
+    expect(member.name).to eq(chinese_full)
   end
 end
