ba7079624206fa00f8cc02348afffd8ee4b7bb87
tuuli <tuuli.pollanen@gmail.com>
2016-01-14 15:07:01 -0800

    Remove duplicate set_local method, and clean up the method in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_locale
+  before_filter :set_default_locale
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -15,39 +15,28 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def localize_from_page_id(id)
-    @page = Page.find_by(id: id)
-    if @page.present? && @page.language.present? && @page.language.code.present?
-      I18n.locale = @page.language.code
-    end
-  end
-
-  def set_locale
-    # I18n doesn't work on a per-request basis on a multi-threaded server, so what I'm doing is forcing it to
-    # check what localization should be use on a per-request basis. This gets called on all requests, and the
-    # case goes through different controllers, checks whether it could be a controller where internalization
-    # would be required (for api/actions and pages), and then sets the locality if appropriate.
-    #
-    # Note that this has broken form validation right now - I'm not exactly sure why that would be, but I get
-    # a 500 for the jquery request.
+  def set_locale(code)
     begin
-      controller = params[:controller]
-      case controller
-        when 'api/actions'
-          localize_from_page_id(params[:page_id])
-        when 'pages'
-          if params[:id].blank?
-            I18n.locale = I18n.default_locale
-          else
-            localize_from_page_id(params[:id])
-          end
-        else
-          I18n.locale = I18n.default_locale
-      end
+      I18n.locale = code
     rescue I18n::InvalidLocale
       # by setting the +i18n.enforce_available_locales+ flag to true but
       # catching the resulting error, it allows us to only set the locale
       # if it's one explicitly registered under +i18n.available_locales+
     end
+  end
+
+  # if @page.present? && @page.language.present? && @page.language.code.present?
+  #   I18n.locale = @page.language.code
+  # end
+
+  def localize_from_page_id
+    page = Page.find_by(id: params[:page_id])
+    if page.present? && page.language.present? && page.language.code.present?
+      set_locale(page.language.code)
+    end
+  end
+
+  def set_default_locale
+    I18n.locale = I18n.default_locale
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -52,6 +52,9 @@ class PagesController < ApplicationController
   private
 
   def render_liquid(layout)
+    if @page.present? && @page.language.present? && @page.language.code.present?
+      set_locale(@page.language.code)
+    end
     raise ActiveRecord::RecordNotFound unless @page.active? || user_signed_in?
     recognized_member = Member.find_from_request(akid: params[:akid], id: cookies.signed[:member_id])
     renderer = LiquidRenderer.new(@page, request_country: request_country, member: recognized_member, layout: layout, url_params: params)
