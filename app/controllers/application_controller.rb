d1af6489df180a8af8e894e695fa6e9e98280f49
tuuli <tuuli.pollanen@gmail.com>
2016-01-13 16:55:55 -0800

    Add rough outline to how locale could be set on a request-per-request basis.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -13,16 +15,35 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def localize_from_page_id
-    page = Page.find_by(id: params[:page_id])
-    if page.present? && page.language.present? && page.language.code.present?
-      set_locale(page.language.code)
+  def localize_from_page_id(id)
+    @page = Page.find_by(id: id)
+    if @page.present? && @page.language.present? && @page.language.code.present?
+      I18n.locale = @page.language.code
     end
   end
 
-  def set_locale(code)
+  def set_locale
+    # I18n doesn't work on a per-request basis on a multi-threaded server, so what I'm doing is forcing it to
+    # check what localization should be use on a per-request basis. This gets called on all requests, and the
+    # case goes through different controllers, checks whether it could be a controller where internalization
+    # would be required (for api/actions and pages), and then sets the locality if appropriate.
+    #
+    # Note that this has broken form validation right now - I'm not exactly sure why that would be, but I get
+    # a 500 for the jquery request.
     begin
-      I18n.locale = code
+      controller = params[:controller]
+      case controller
+        when 'api/actions'
+          localize_from_page_id(params[:page_id])
+        when 'pages'
+          if params[:id].blank?
+            I18n.locale = I18n.default_locale
+          else
+            localize_from_page_id(params[:id])
+          end
+        else
+          I18n.locale = I18n.default_locale
+      end
     rescue I18n::InvalidLocale
       # by setting the +i18n.enforce_available_locales+ flag to true but
       # catching the resulting error, it allows us to only set the locale
