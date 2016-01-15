64a8ecf48df51df54342aa011d77b1bcedeb0185
Neal Donnelly <NealJMD@gmail.com>
2016-01-15 10:17:07 -0600

    specify js test execution order to avoid collision

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -38,7 +38,8 @@ Teaspoon.configure do |config|
 
     # Specify a file matcher as a regular expression and all matching files will be loaded when the suite is run. These
     # files need to be within an asset path. You can add asset paths using the `config.asset_paths`.
-    suite.matcher = "{spec/javascripts,app/assets}/**/*_spec.{js,js.coffee,coffee}"
+    # Currently all tests are required in spec_helper to allow specification of execution order
+    # suite.matcher = "{spec/javascripts,app/assets}/**/*_spec.{js,js.coffee,coffee}"
 
     # Load additional JS files, but requiring them in your spec helper is the preferred way to do this.
     #suite.javascripts = []
