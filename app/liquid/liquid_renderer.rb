3f605eab4ab7885920a49d7ad0fd3b85cb43f223
Omar Sahyoun <osahyoun@gmail.com>
2016-01-25 17:36:31 +0100

    Separate live data from cacheable.

--- app/liquid/liquid_renderer.rb
+++ app/liquid/liquid_renderer.rb
@@ -1,8 +1,6 @@
 class LiquidRenderer
   include Rails.application.routes.url_helpers
 
-  CACHE_KEY_PREFIX = "rendered_liquid"
-
   def initialize(page, layout:, location: nil, member: nil, url_params: {})
     @page = page
     @layout = layout
@@ -12,8 +10,8 @@ class LiquidRenderer
   end
 
   def render
-    Rails.cache.fetch(cache_key) do
-      template.render( data ).html_safe
+    Rails.cache.fetch(key.for_markup) do
+      template.render( JSON.parse(data) ).html_safe
     end
   end
 
@@ -23,20 +21,26 @@ class LiquidRenderer
 
   def data
     return @data if @data
+
+    @data = Rails.cache.fetch(key.for_data) do
+
     plugin_data = Plugins.data_for_view(@page, {form_values: @member.try(:attributes), donation_band: @url_params[:donation_band]})
-    @data ||= plugin_data.
-                merge( @page.liquid_data ).
-                merge( images: images ).
-                merge( primary_image: image_urls(@page.image_to_display) ).
-                merge( LiquidHelper.globals(page: @page) ).
-                merge( shares: Shares.get_all(@page) ).
-                merge( url_params: @url_params ).
-                merge( follow_up_url: follow_up_page_path(@page.id)).
-                merge( member: @member.try(:liquid_data) ).
-                merge( location: location).
-                merge( outstanding_fields: outstanding_fields(plugin_data) ).
-                merge( donation_bands: donation_bands(plugin_data) ).
-                deep_stringify_keys
+
+    plugin_data.
+      merge( @page.liquid_data ).
+      merge( images: images ).
+      merge( primary_image: image_urls(@page.image_to_display) ).
+      merge( LiquidHelper.globals(page: @page) ).
+      merge( shares: Shares.get_all(@page) ).
+      merge( follow_up_url: follow_up_page_path(@page.id)).
+      merge( outstanding_fields: outstanding_fields(plugin_data) ).
+      merge( donation_bands: donation_bands(plugin_data) ).
+      to_json
+    end
+  end
+
+  def data_per_member
+    JSON.parse(data).stringify_keys.merge(member_data)
   end
 
   def images
@@ -44,6 +48,14 @@ class LiquidRenderer
   end
 
   private
+
+  def member_data
+    {
+      url_params: @url_params,
+      member:     @member.try(:liquid_data),
+      location:   location
+    }
+  end
 
   def outstanding_fields(plugin_data)
     isolate_from_plugin_data(plugin_data, :outstanding_fields)
@@ -85,8 +97,29 @@ class LiquidRenderer
     { urls: { large: img.content.url(:large), small: img.content.url(:thumb) } }
   end
 
-  def cache_key
-    "#{CACHE_KEY_PREFIX}:#{@page.cache_key}:#{@layout.cache_key}"
+  def key
+    Key.new(@page, @layout)
+  end
+
+  class Key
+    def initialize(page, layout)
+      @page = page
+      @layout = layout
+    end
+
+    def for_data
+      "client_data:" << base
+    end
+
+    def for_markup
+      "liquid_markup:" << base
+    end
+
+    private
+
+    def base
+      "#{@page.cache_key}:#{@layout.cache_key}"
+    end
   end
 end
 
