db15ebe5b867ad2f69775e13b7129767902ffa06
tuuli <tuuli.pollanen@gmail.com>
2016-01-27 11:00:24 -0800

    Move :around block that resets locale so that it applies to every locale spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,40 +2,7 @@ require 'rails_helper'
 
 describe ApplicationController do
 
-  describe 'localize_from_page_id' do
-
-    let(:english) { create :language, code: 'en' }
-    let!(:page) { create :page, language: english }
-
-    before :each do
-      allow(controller).to receive(:set_locale)
-    end
-
-    after :each do
-      I18n.locale = :en
-    end
-
-    it 'does nothing if page_id is blank' do
-      allow(controller).to receive(:params).and_return({page_id: nil})
-      controller.send(:localize_from_page_id)
-      expect(controller).not_to have_received(:set_locale)
-    end
-
-    it 'does nothing if page has no language' do
-      page.update_attributes(language_id: nil)
-      allow(controller).to receive(:params).and_return({page_id: page.id})
-      controller.send(:localize_from_page_id)
-      expect(controller).not_to have_received(:set_locale)
-    end
-
-    it 'sets locale with page language code' do
-      allow(controller).to receive(:params).and_return({page_id: page.id})
-      controller.send(:localize_from_page_id)
-      expect(controller).to have_received(:set_locale).with('en')
-    end
-  end
-
-  describe 'set_locale' do
+  context 'localization' do
 
     around(:each) do |spec|
       I18n.locale = I18n.default_locale
@@ -43,23 +10,56 @@ describe ApplicationController do
       I18n.locale = I18n.default_locale
     end
 
-    it 'sets the locale if it is a known locale' do
-      expect(I18n.locale).to eq :en
-      expect{ controller.send(:set_locale, 'fr') }.not_to raise_error
-      expect(I18n.locale).to eq :fr
+    describe 'localize_from_page_id' do
+
+      let(:english) { create :language, code: 'en' }
+      let!(:page) { create :page, language: english }
+
+      before :each do
+        allow(controller).to receive(:set_locale)
+      end
+
+      it 'does nothing if page_id is blank' do
+        allow(controller).to receive(:params).and_return({page_id: nil})
+        controller.send(:localize_from_page_id)
+        expect(controller).not_to have_received(:set_locale)
+      end
+
+      it 'does nothing if page has no language' do
+        page.update_attributes(language_id: nil)
+        allow(controller).to receive(:params).and_return({page_id: page.id})
+        controller.send(:localize_from_page_id)
+        expect(controller).not_to have_received(:set_locale)
+      end
+
+      it 'sets locale with page language code' do
+        allow(controller).to receive(:params).and_return({page_id: page.id})
+        controller.send(:localize_from_page_id)
+        expect(controller).to have_received(:set_locale).with('en')
+      end
     end
 
-    it 'does nothing when passed an unknown locale' do
-      expect(I18n.locale).to eq :en
-      expect{ controller.send(:set_locale, 'es') }.not_to raise_error
-      expect(I18n.locale).to eq :en
+    describe 'set_locale' do
+
+      it 'sets the locale if it is a known locale' do
+        expect(I18n.locale).to eq :en
+        expect{ controller.send(:set_locale, 'fr') }.not_to raise_error
+        expect(I18n.locale).to eq :fr
+      end
+
+      it 'does nothing when passed an unknown locale' do
+        expect(I18n.locale).to eq :en
+        expect{ controller.send(:set_locale, 'es') }.not_to raise_error
+        expect(I18n.locale).to eq :en
+      end
+
+      it 'does nothing when passed a blank locale' do
+        expect(I18n.locale).to eq :en
+        expect{ controller.send(:set_locale, nil) }.not_to raise_error
+        expect(I18n.locale).to eq :en
+      end
     end
 
-    it 'does nothing when passed a blank locale' do
-      expect(I18n.locale).to eq :en
-      expect{ controller.send(:set_locale, nil) }.not_to raise_error
-      expect(I18n.locale).to eq :en
-    end
   end
 
 end
