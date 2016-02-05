f0b4c501dc91f02835c45287b76976817e658f9b
Neal Donnelly <NealJMD@gmail.com>
2016-02-05 16:07:01 -0600

    add paranoid akid specs, failing member update spec

--- spec/requests/api/actions_spec.rb
+++ spec/requests/api/actions_spec.rb
@@ -22,41 +22,59 @@ describe "Api Actions" do
     end
 
     describe 'akid manipulation' do
-      before do
-        post "/api/pages/#{page.id}/actions", params
+
+      context 'new member' do
+        before do
+          post "/api/pages/#{page.id}/actions", params
+        end
+
+        it 'persists action' do
+          expect(page.actions.count).to eq(1)
+        end
+
+        it 'saves akid on action' do
+          expect(
+              Action.where('form_data @> ?', {akid: '123.456.fcvd'}.to_json).first
+          ).to eq(page.actions.first)
+        end
+
+        it 'saves actionkit_user_id on member' do
+          expect(Member.last.actionkit_user_id).to eq '456'
+        end
+
+        it "Posts action to SQS Queue" do
+          expected_params = {
+              queue_url: 'http://example.com',
+
+              message_body: {
+                  type: "action",
+                  params: {
+                      slug: page.slug,
+                      body: {
+                          email: "hello@example.com",
+                          page_id: page.id.to_s,
+                          form_id: form.id.to_s,
+                          source: 'fb',
+                          akid: '123.456.fcvd'
+                      }
+                  }
+              }.to_json
+          }
+
+          expect(sqs_client).to have_received(:send_message).with(expected_params)
+        end
       end
 
-      it 'persists action' do
-        expect(page.actions.count).to eq(1)
+      context 'existing member' do
+
+        let(:member) { create :member, actionkit_user_id: '7777', email: params[:email]}
+
+        it 'overwrites existing actionkit_user_id' do
+          post "/api/pages/#{page.id}/actions", params
+          expect(member.reload.actionkit_user_id).to eq '456'
+        end
       end
 
-      it 'saves akid' do
-        expect(
-            Action.where('form_data @> ?', {akid: '123.456.fcvd'}.to_json).first
-        ).to eq(page.actions.first)
-      end
-
-      it "Posts action to SQS Queue" do
-        expected_params = {
-            queue_url: 'http://example.com',
-
-            message_body: {
-                type: "action",
-                params: {
-                    slug: page.slug,
-                    body: {
-                        email: "hello@example.com",
-                        page_id: page.id.to_s,
-                        form_id: form.id.to_s,
-                        source: 'fb',
-                        akid: '123.456.fcvd'
-                    }
-                }
-            }.to_json
-        }
-
-        expect(sqs_client).to have_received(:send_message).with(expected_params)
-      end
     end
 
     describe 'referring akid' do
@@ -87,50 +105,66 @@ describe "Api Actions" do
     end
   end
 
-  describe 'invalid akids' do
-    let(:page) { create(:page) }
-    let(:form) { create(:form_with_email) }
-    let(:params) do
-      {
-          email: 'hello@example.com',
-          form_id: form.id,
-          akid: 'this_isnt_a_valid_akid'
-      }
-    end
+  ['long_string_with_underscore', '1234.5678', '2', '?&=', '2..2', '..2'].each do |invalid_akid|
+    describe "invalid akid '#{invalid_akid}'" do
+      let(:page) { create(:page) }
+      let(:form) { create(:form_with_email) }
+      let(:params) do
+        {
+            email: 'hello@example.com',
+            form_id: form.id,
+            akid: invalid_akid
+        }
+      end
 
-    before do
-      post "/api/pages/#{page.id}/actions", params
-    end
+      context 'existing member' do
 
-    it 'does not assign an actionkit_user_id to the created Member' do
-      expect(Member.first.actionkit_user_id).to be(nil)
-    end
+        let(:member) { create :member, actionkit_user_id: '1234', email: params[:email]}
 
-    it 'sends the bad akid through the form data for record keeping' do
-      expect(Action.where('form_data @> ?', {akid: 'this_isnt_a_valid_akid'}.to_json).first).to eq(page.actions.first)
-    end
+        it 'does not overwrite existing actionkit_user_id' do
+          post "/api/pages/#{page.id}/actions", params
+          expect(member.reload.actionkit_user_id).to eq '1234'
+        end
 
-    it 'does not include a referring_user_uri in the queue message' do
-      expected_params = {
-          queue_url: 'http://example.com',
+      end
 
-          message_body: {
-              type: 'action',
-              params: {
-                  slug: page.slug,
-                  body: {
-                      email: 'hello@example.com',
-                      page_id: page.id.to_s,
-                      form_id: form.id.to_s,
-                      akid: 'this_isnt_a_valid_akid'
+      context 'new member' do
+        before do
+          post "/api/pages/#{page.id}/actions", params
+        end
+
+        it 'responds with success' do
+          expect(response).to be_success
+        end
+
+        it 'does not assign an actionkit_user_id to the created Member' do
+          expect(Member.first.actionkit_user_id).to be_blank
+        end
+
+        it 'sends the bad akid through the form data for record keeping' do
+          expect(Action.where('form_data @> ?', {akid: invalid_akid}.to_json).first).to eq(page.actions.first)
+        end
+
+        it 'does not include a referring_user_uri in the queue message' do
+          expected_params = {
+              queue_url: 'http://example.com',
+
+              message_body: {
+                  type: 'action',
+                  params: {
+                      slug: page.slug,
+                      body: {
+                          email: 'hello@example.com',
+                          page_id: page.id.to_s,
+                          form_id: form.id.to_s,
+                          akid: invalid_akid
+                      }
                   }
-              }
-          }.to_json
-      }
-      expect(sqs_client).to have_received(:send_message).with(expected_params)
+              }.to_json
+          }
+          expect(sqs_client).to have_received(:send_message).with(expected_params)
+        end
+      end
     end
   end
 end
-
-
-
