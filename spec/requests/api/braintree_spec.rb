7fa743fcad197e2bb91636597d30a5beb6931ecf
Neal Donnelly <NealJMD@gmail.com>
2016-02-08 14:57:06 -0600

    first specs in bt integration spec rewrite, failures show gaps

--- spec/requests/api/braintree_spec.rb
+++ spec/requests/api/braintree_spec.rb
@@ -2,308 +2,173 @@ require 'rails_helper'
 
 describe "Braintree API" do
 
-  let(:page) { create(:page, title: 'foo-bar-raa') }
-
-  before do
-    Settings.merge!(braintree: {
-      merchants: {
-        USD: 'sumofus',
-        EUR: 'EUR',
-        GBP: 'GBP'
-      }
-    })
+  let(:page) { create(:page, title: 'Cash rules everything around me') }
+  let(:form) { create(:form) }
+  before :each do
+    allow(ChampaignQueue).to receive(:push)
   end
 
-  def post_transaction(opts = {})
-    post "/api/braintree/pages/#{page.id}/transaction", {
-      currency: :USD,
-      payment_method_nonce: 'fake-valid-nonce',
-      amount: 100.00,
-      recurring: false,
-      user: { email: 'foo@example.com' }
-    }.merge(opts)
-  end
 
-  def post_subscription(opts = {})
-    post "/api/braintree/pages/#{page.id}/subscription", {
-      user: { email: customer.email }, price: '100.00', currency: :USD, payment_method_nonce: 'fake-valid-nonce',
+  describe 'making a transaction' do
+    describe 'success' do
 
-    }.merge(opts)
-  end
-
-  def body
-    JSON.parse(response.body).with_indifferent_access
-  end
-
-  describe 'making a subscription' do
-    let!(:customer) { create(:payment_braintree_customer, email: 'foo@example.com', card_vault_token: '4y5dr6' )}
-
-    context "successful subscription" do
-      it 'creates subscription' do
-        VCR.use_cassette('braintree_subscription_success') do
-          post_subscription
-          record = Payment::BraintreeSubscription.first
-          expect(body[:subscription_id]).to match(/[a-z0-9]{6}/)
-          expect(record.subscription_id).to eq(body[:subscription_id])
-        end
+      let(:basic_params) do
+        {
+          currency: 'EUR',
+          payment_method_nonce: 'fake-valid-nonce',
+          amount: 123.05,
+          recurring: false
+        }
       end
-    end
-
-    it 'creates a token and successfully subscribes a user whose e-mail is not associated with a token' do
-      VCR.use_cassette('braintree_subscription_success_no_token') do
-        post_subscription(user: { email: 'foo+1234@example.com'}, payment_method_nonce: 'fake-valid-visa-nonce')
-        expect(body[:success]).to be true
-        expect(body[:subscription_id]).to match(/[a-z0-9]{6}/)
-      end
-    end
-  end
-
-  describe "making a transaction" do
-    it 'gets a client token' do
-      VCR.use_cassette("braintree_client_token") do
-        get '/api/braintree/token'
-
-        expect(body).to have_key(:token)
-        expect(body[:token]).to be_a String
-        expect(body[:token]).to_not include(' ')
-        expect(body[:token].length).to be > 5
-      end
-    end
-
-    context "successful" do
-      subject { Payment::BraintreeTransaction.first }
-
-
-      context "with paypal" do
-        it 'sets payment type' do
-          VCR.use_cassette("transaction_paypal_success") do
-            post_transaction( payment_method_nonce: 'fake-paypal-one-time-nonce' )
-          end
-
-          expect(subject.payment_instrument_type).to eq('paypal_account')
-        end
+      let(:user_params) do
+        {
+          form_id: form.id, 
+          name: "Joe Ferris", 
+          email: "paul+joe@sumofus.org", 
+          postal: "11225", 
+          country: "US"
+        }
       end
 
-      context "one off" do
-        before do
-          allow(ChampaignQueue).to receive(:push)
+      context 'Member exists' do
 
-          VCR.use_cassette("transaction_success") do
-            post_transaction
+        let!(:member) { create :member, email: user_params[:email], postal: nil }
+
+        context 'BraintreeCustomer exists' do
+
+          let!(:customer) { create :payment_braintree_customer, member: member }
+
+          context 'with basic params' do
+
+            let(:params) { basic_params.merge(user: user_params) }
+            subject do
+              VCR.use_cassette("transaction success") do
+                post api_braintree_transaction_path(page.id), params
+              end
+            end
+
+            it "creates an Action associated with the Page and Member" do
+              expect{ subject }.to change{ Action.count }.by 1
+              expect(Action.last.page).to eq page
+              expect(Action.last.member).to eq member
+            end
+
+            it "stores amount, currency, and transaction_id in form_data on the Action" do
+              expect{ subject }.to change{ Action.count }.by 1
+              form_data = Action.last.form_data
+              puts "form_data:",form_data
+              expect(form_data['amount']).to eq '123.05'
+              expect(form_data['currency']).to eq 'EUR'
+              expect(form_data['transaction_id']).to eq Transaction.last.id
+            end
+
+            it "creates a Transaction associated with the page" do
+              expect{ subject }.to change{ Payment::BraintreeTransaction.count }.by 1
+              expect(Payment::BraintreeTransaction.last.page).to eq page
+            end
+
+            it "updates Payment::BraintreeCustomer including last four for credit card" do
+              previous_last_4 = customer.card_last_4
+              expect{ subject }.to change{ Payment::BraintreeCustomer.count }.by 0
+              expect( customer.reload.card_last_4 ).not_to eq previous_last_4
+            end
+
+            it "posts donation action to queue with key data" do
+              subject
+              expect( ChampaignQueue ).to have_received(:push).with({
+                type: "donation",
+                params: {
+                  donationpage: {
+                    name: "cash-rules-everything-around-me-donation",
+                    payment_account: "Default Import Stub"
+                  },
+                  order: {
+                    amount: "123.05",
+                    card_num: "1881",
+                    card_code: "007",
+                    exp_date_month: "12",
+                    exp_date_year: "2020",
+                    currency: "EUR"
+                  },
+                  user: {
+                    # Double check - it's only sending email and country
+                    email:"paul+joe@sumofus.org",
+                    country:"US"
+                  }
+                }
+              })
+            end
+
+            it "increments action count on page" do
+              expect{ subject }.to change{ page.reload.action_count }.by 1
+            end
+
+            it "passes the params to braintree" do
+              allow(Braintree::Transaction).to receive(:sale).and_call_original
+              subject
+              expect(Braintree::Transaction).to have_received(:sale).with({
+                amount: 123.05,
+                payment_method_nonce: "fake-valid-nonce",
+                merchant_account_id: "EUR",
+                options: {
+                  submit_for_settlement: true,
+                  store_in_vault_on_success: false
+                },
+                customer: {
+                  first_name: "Joe",
+                  last_name: "Ferris",
+                  email: "paul+joe@sumofus.org"
+                },
+                customer_id: customer.customer_id
+              })
+            end
+
+            it "leaves a cookie with the member_id" do
+              expect(cookies['member_id']).to eq nil
+              subject
+
+              # we can't access signed cookies in request specs, so check for hashed value
+              expect(cookies['member_id']).not_to eq nil
+              expect(cookies['member_id'].length).to be > 20
+            end
+
+            it "updates the Member’s fields with any new data" do
+              expect(member.first_name).not_to eq 'Joe'
+              expect(member.last_name).not_to eq 'Ferris'
+              expect(member.postal).to eq nil
+              expect{ subject }.to change{ Member.count }.by 0
+              member.reload
+              expect(member.first_name).to eq 'Joe'
+              expect(member.last_name).to eq 'Ferris'
+              expect(member.postal).to eq '11225'
+            end
           end
-        end
 
-        it 'returns a transaction_id' do
-          expect(body[:transaction_id]).to match(/[a-z0-9]{6}/)
-        end
+          context 'with different params' do
+            it "passes first_name and last_name to braintree if included as full_name" do
 
-        it 'creates an action' do
-          expect(Action.first.page).to eq(page)
-        end
+            end
 
-        it 'creates a member' do
-          expect(Member.first.email).to eq('foo@example.com')
-        end
+            it "passes first_name and last_name to braintree if included as first name, last_name" do
 
-        it 'sets the member cookie' do
-          # we can't access signed cookies in request specs, so
-          # we're checking against a hashed value.
-          expect(cookies['member_id']).not_to eq nil
-          expect(cookies['member_id'].length).to be > 20
-        end
+            end
 
-        it 'posts donation to worker' do
-          expected = {
-            type: 'donation',
-            params: {
-              donationpage: {
-                name: 'foo-bar-raa-donation',
-                payment_account: 'Default Import Stub',
-              },
-              order: {
-                amount: '100.0',
-                card_num: '1881',
-                card_code: '007',
-                exp_date_month: '12',
-                exp_date_year: '2020',
-                currency: 'USD'
-              },
-              user: {
-                email: 'foo@example.com',
-                country: nil
-              }
-            }
-          }
-
-          expect(ChampaignQueue).to have_received(:push).with(expected)
-        end
-
-
-        describe 'transaction associations' do
-          it 'with page' do
-            expect(subject.page).to eq(page)
-          end
-
-          it 'with customer' do
-            pending
-            fail
-            expect(subject.customer).to eq(Payment::BraintreeCustomer.first)
-          end
-
-          it 'with action' do
-            pending
-            fail
-            expect(subject.action).to eq(Action.first)
-          end
-        end
-
-        it 'records transaction to store' do
-          expect(subject.transaction_id).to eq(body[:transaction_id])
-          expect(subject.transaction_type).to eq('sale')
-          expect(subject.amount).to eq('100.0')
-          expect(subject.merchant_account_id).to eq('USD')
-          expect(subject.currency).to eq('USD')
-        end
-
-        context 'customer' do
-          subject(:customer) { Payment::BraintreeCustomer.first }
-
-          it 'persists braintree customer' do
-            expect(customer).to_not be nil
-            expect(customer.customer_id).to match(/\d{8}/)
-            expect(customer.card_vault_token).to match(/[a-z0-9]{6}/)
-          end
-
-          it 'associates with member' do
-            expect(customer.member).to eq(Member.first)
-          end
-        end
-      end
-
-      context 'recurring' do
-        before do
-          VCR.use_cassette("transaction_recurring_success") do
-            post_transaction(recurring: true)
+            it 'passes PYPL'
           end
         end
 
+        context 'BraintreeCustomer is new' do
 
-        it 'raises when subscription already exists'
-
-        it 'records transaction to store' do
-          customer = Payment::BraintreeCustomer.first
-          expect(customer).to_not be nil
-          expect(customer.customer_id).to match(/\d{8}/)
-          expect(customer.member.email).to eq('foo@example.com')
-          expect(customer.card_vault_token).to match(/[a-z0-9]{6}/)
-
-          expect(body[:subscription_id]).to match(/[a-z0-9]{6}/)
-        end
-
-        it 'sets the member cookie' do
-          # we can't access signed cookies in request specs, so
-          # we're checking against a hashed value.
-          expect(cookies['member_id']).not_to eq nil
-          expect(cookies['member_id'].length).to be > 20
         end
       end
 
-      context 'repeat donation' do
-        before do
-          VCR.use_cassette("repeat_transaction_success") do
-            post_transaction(amount: 20.00)
-          end
-        end
+      context 'Member is new' do
+        context 'BraintreeCustomer is new' do
 
-        it 'uses existing customer_id' do
-          expect(Payment::BraintreeCustomer.count).to eq(1)
-        end
-
-      end
-    end
-  end
-
-  context 'unsuccessful transaction' do
-
-    context "processor related" do
-
-      it 'processor declined' do
-        VCR.use_cassette("braintree_processor_declined") do
-          post_transaction(amount: 2100)
-          expect(
-            body[:errors].first
-          ).to eq({
-            "declined"=> true,
-            "code"    => "2100",
-            "message" => "Processor Declined"
-          })
-        end
-      end
-
-      it 'gateway rejected' do
-        VCR.use_cassette("braintree_gateway_rejected") do
-          post_transaction(amount: 5001)
-          expect(
-            body[:errors].first
-          ).to eq({
-            "declined"=> true,
-            "code"    => '',
-            "message" => "application_incomplete"
-          })
-        end
-      end
-
-      it 'settlement declined for paypal'
-    end
-
-    it 'raises if no merchant account exists' do
-      expect{
-        post_transaction(currency: 'JPY')
-      }.to raise_error(PaymentProcessor::Exceptions::InvalidCurrency)
-    end
-
-    it 'returns user validation errors' do
-      VCR.use_cassette("transaction_failure_invalid_amount") do
-        post_transaction(amount: -10)
-
-        expect(body.keys).to contain_exactly('success','errors')
-        expect(body[:success]).to be false
-        expect(body[:errors].first[:message]).to eq("Amount cannot be negative.")
-      end
-    end
-
-    it 'does not set the member cookie' do
-      VCR.use_cassette("transaction_failure_invalid_amount") do
-        post_transaction(amount: -10)
-        expect(cookies['member_id']).to eq nil
-      end
-    end
-
-    context 'invalid nonce' do
-      it 'raises braintree validations error' do
-        VCR.use_cassette("transaction_failure_invalid_nonce") do
-          expect{
-            post_transaction(payment_method_nonce: 'fake-coinbase-nonce')
-          }.to raise_error(Braintree::ValidationsFailed)
-        end
-      end
-    end
-
-    context 'Customer ID is required' do
-      let(:customer){ double(:customer, customer_id: 'x') }
-
-      before do
-        allow(::Payment).to receive(:customer){ customer }
-      end
-
-      it 'raises braintree validations error' do
-        VCR.use_cassette("transaction_failure_customer_id_missing") do
-          expect{
-            post_transaction
-          }.to raise_error(Braintree::ValidationsFailed)
         end
       end
     end
   end
 end
+
 
