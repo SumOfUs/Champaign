64b0104785a100726853fae46222ab22af951581
Neal Donnelly <NealJMD@gmail.com>
2016-02-09 17:37:50 -0600

    add specs for case with existing braintree customer

--- spec/requests/api/braintree_spec.rb
+++ spec/requests/api/braintree_spec.rb
@@ -77,8 +77,8 @@ describe "Braintree API" do
               expect(transaction.customer_id).to eq customer.customer_id
               expect(transaction.status).to eq 'success'
 
-              expect(transaction.payment_method_token).to be_blank
-              expect(transaction.transaction_id).not_to be_blank # it's for subscription
+              expect(transaction.payment_method_token).not_to be_blank
+              expect(transaction.transaction_id).not_to be_blank
             end
 
             it "updates Payment::BraintreeCustomer including last four for credit card" do
@@ -178,6 +178,23 @@ describe "Braintree API" do
               end
             end
 
+            it "creates a Transaction associated with the page storing relevant info" do
+              expect{ subject }.to change{ Payment::BraintreeTransaction.count }.by 1
+              transaction = Payment::BraintreeTransaction.last
+
+              expect(transaction.page).to eq page
+              expect(transaction.amount).to eq '123.05'
+              expect(transaction.currency).to eq 'EUR'
+              expect(transaction.merchant_account_id).to eq 'EUR'
+              expect(transaction.payment_instrument_type).to eq 'paypal_account'
+              expect(transaction.transaction_type).to eq 'sale'
+              expect(transaction.customer_id).to eq customer.customer_id
+              expect(transaction.status).to eq 'success'
+
+              expect(transaction.payment_method_token).not_to be_blank
+              expect(transaction.transaction_id).not_to be_blank
+            end
+
             it "does not change Payment::BraintreeCustomer" do
               expect{ subject }.to change{ Payment::BraintreeCustomer.count }.by 0
               expect( customer ).to eq Payment::BraintreeCustomer.find(customer.id)
@@ -205,6 +222,187 @@ describe "Braintree API" do
 
         context 'BraintreeCustomer is new' do
 
+          context 'with basic params' do
+
+            let(:params) { basic_params.merge(user: user_params) }
+            subject do
+              VCR.use_cassette("transaction success basic new customer") do
+                post api_braintree_transaction_path(page.id), params
+              end
+            end
+
+            it "creates an Action associated with the Page and Member" do
+              expect{ subject }.to change{ Action.count }.by 1
+              expect(Action.last.page).to eq page
+              expect(Action.last.member).to eq member
+            end
+
+            it "stores amount, currency, card_num, is_subscription, and transaction_id in form_data on the Action" do
+              expect{ subject }.to change{ Action.count }.by 1
+              form_data = Action.last.form_data
+              expect(form_data['card_num']).to eq '1881'
+              expect(form_data['is_subscription']).to eq false
+              expect(form_data['amount']).to eq '123.05'
+              expect(form_data['currency']).to eq 'EUR'
+              expect(form_data['transaction_id']).to eq Payment::BraintreeTransaction.last.transaction_id
+            end
+
+            it "creates a Transaction associated with the page storing relevant info" do
+              expect{ subject }.to change{ Payment::BraintreeTransaction.count }.by 1
+              transaction = Payment::BraintreeTransaction.last
+
+              expect(transaction.page).to eq page
+              expect(transaction.amount).to eq '123.05'
+              expect(transaction.currency).to eq 'EUR'
+              expect(transaction.merchant_account_id).to eq 'EUR'
+              expect(transaction.payment_instrument_type).to eq 'credit_card'
+              expect(transaction.transaction_type).to eq 'sale'
+              expect(transaction.customer_id).to eq Payment::BraintreeCustomer.last.customer_id
+              expect(transaction.status).to eq 'success'
+
+              expect(transaction.payment_method_token).not_to be_blank
+              expect(transaction.transaction_id).not_to be_blank
+            end
+
+            it "creates new Payment::BraintreeCustomer including customer_id and last four for credit card" do
+              expect{ subject }.to change{ Payment::BraintreeCustomer.count }.by 1
+              customer = Payment::BraintreeCustomer.last
+              expect( customer.customer_id ).not_to be_blank
+              expect( customer.card_last_4 ).to eq '1881'
+            end
+
+            it "posts donation action to queue with key data" do
+              subject
+              expect( ChampaignQueue ).to have_received(:push).with({
+                type: "donation",
+                params: {
+                  donationpage: {
+                    name: "cash-rules-everything-around-me-donation",
+                    payment_account: "Default Import Stub"
+                  },
+                  order: {
+                    amount: "123.05",
+                    card_num: "1881",
+                    card_code: "007",
+                    exp_date_month: "12",
+                    exp_date_year: "2020",
+                    currency: "EUR"
+                  },
+                  user: {
+                    email: "joe.ferris@sumofus.org",
+                    country: "US",
+                    postal: "11225",
+                    address1: '25 Elm Drive',
+                    first_name: 'Joe',
+                    last_name: 'Ferris'
+                  }
+                }
+              })
+            end
+
+            it "increments action count on page" do
+              expect{ subject }.to change{ page.reload.action_count }.by 1
+            end
+
+            it "passes the params to braintree" do
+              allow(Braintree::Transaction).to receive(:sale).and_call_original
+              subject
+              expect(Braintree::Transaction).to have_received(:sale).with({
+                amount: 123.05,
+                payment_method_nonce: "fake-valid-nonce",
+                merchant_account_id: "EUR",
+                options: {
+                  submit_for_settlement: true,
+                  store_in_vault_on_success: true
+                },
+                customer: {
+                  first_name: "Joe",
+                  last_name: "Ferris",
+                  email: "joe.ferris@sumofus.org"
+                },
+                billing: {
+                  first_name: "Joe",
+                  last_name: "Ferris",
+                  street_address: "25 Elm Drive",
+                  postal_code: '11225',
+                  country_code_alpha2: 'US'
+                }
+              })
+            end
+
+            it "leaves a cookie with the member_id" do
+              expect(cookies['member_id']).to eq nil
+              subject
+
+              # we can't access signed cookies in request specs, so check for hashed value
+              expect(cookies['member_id']).not_to eq nil
+              expect(cookies['member_id'].length).to be > 20
+            end
+
+            it "updates the Member’s fields with any new data" do
+              expect(member.first_name).not_to eq 'Joe'
+              expect(member.last_name).not_to eq 'Ferris'
+              expect(member.postal).to eq nil
+              expect{ subject }.to change{ Member.count }.by 0
+              member.reload
+              expect(member.first_name).to eq 'Joe'
+              expect(member.last_name).to eq 'Ferris'
+              expect(member.postal).to eq '11225'
+            end
+          end
+
+          context 'with Paypal' do
+
+            let(:params) { basic_params.merge(user: user_params, payment_method_nonce: 'fake-paypal-one-time-nonce') }
+
+            subject do
+              VCR.use_cassette("transaction success paypal new customer") do
+                post api_braintree_transaction_path(page.id), params
+              end
+            end
+
+            it "creates a Transaction associated with the page storing relevant info" do
+              expect{ subject }.to change{ Payment::BraintreeTransaction.count }.by 1
+              transaction = Payment::BraintreeTransaction.last
+
+              expect(transaction.page).to eq page
+              expect(transaction.amount).to eq '123.05'
+              expect(transaction.currency).to eq 'EUR'
+              expect(transaction.merchant_account_id).to eq 'EUR'
+              expect(transaction.payment_instrument_type).to eq 'paypal_account'
+              expect(transaction.transaction_type).to eq 'sale'
+              expect(transaction.customer_id).to eq Payment::BraintreeCustomer.last.customer_id
+              expect(transaction.status).to eq 'success'
+
+              expect(transaction.payment_method_token).not_to be_blank
+              expect(transaction.transaction_id).not_to be_blank
+            end
+
+            it "creates a Payment::BraintreeCustomer with customer_id and PYPL for last 4" do
+              expect{ subject }.to change{ Payment::BraintreeCustomer.count }.by 1
+              customer = Payment::BraintreeCustomer.last
+              expect(customer.customer_id).not_to be_blank
+              expect(customer.card_last_4).to eq 'PYPL'
+            end
+
+            it "stores PYPL as card_num on the Action" do
+              expect{ subject }.to change{ Action.count }.by 1
+              form_data = Action.last.form_data
+              expect(form_data['card_num']).to eq 'PYPL'
+              expect(form_data['is_subscription']).to eq false
+              expect(form_data['amount']).to eq '123.05'
+              expect(form_data['currency']).to eq 'EUR'
+              expect(form_data['transaction_id']).to eq Payment::BraintreeTransaction.last.transaction_id
+            end
+
+            it 'passes PYPL as card_num to queue' do
+              subject
+              expect( ChampaignQueue ).to have_received(:push).with(a_hash_including(
+                params: a_hash_including( order: a_hash_including(card_num: 'PYPL') )
+              ))
+            end
+
+          end
         end
       end
 
