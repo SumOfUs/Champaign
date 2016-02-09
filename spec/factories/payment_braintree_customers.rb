45e7e5b6b230f60f336393e2721852ca604da30f
Neal Donnelly <NealJMD@gmail.com>
2016-02-09 11:04:38 -0600

    update Customer on transaction success

--- spec/factories/payment_braintree_customers.rb
+++ spec/factories/payment_braintree_customers.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
   factory :payment_braintree_customer, :class => 'Payment::BraintreeCustomer' do
-    card_type "MyString"
+    card_type { Faker::Business.credit_card_type }
     card_bin ""
-    cardholder_name "MyString"
+    cardholder_name { Faker::Name.name }
     card_debit "MyString"
-    card_last_4 "MyString"
+    card_last_4 { Faker::Number.number(4) }
     card_vault_token "MyString"
     card_unqiue_number_identifier "MyString"
     email { Faker::Internet.email }
     first_name "MyString"
     last_name "MyString"
-    customer_id "MyString"
+    customer_id 1
   end
 end
