072af3a91810b013a6cc423da6e2ecba380a9077
Neal Donnelly <NealJMD@gmail.com>
2016-02-18 13:40:36 -0600

    get remaining base case tests passing

--- spec/requests/api/braintree_failure_spec.rb
+++ spec/requests/api/braintree_failure_spec.rb
@@ -34,6 +34,7 @@ describe "Braintree API" do
 
   let(:page) { create(:page, title: 'The more money we come across, the more problems we see') }
   let(:form) { create(:form) }
+  let(:token_format) { /[a-z0-9]{1,36}/i }
   let(:user) do
     {
       form_id:  form.id,
@@ -48,7 +49,7 @@ describe "Braintree API" do
   let(:params) do
     {
       currency:   'EUR',
-      amount:     '2000.00', # amount is hooked into BT to cause failure
+      amount:     '2002.00', # triggers credit limit exceeded
       recurring:  false,
       payment_method_nonce: 'fake-valid-nonce',
       user: user
@@ -62,6 +63,9 @@ describe "Braintree API" do
   describe "unsuccessfuly" do
     describe "making a transaction" do
       describe "when Member exists" do
+
+        let!(:member) { create :member, email: user[:email], postal: nil }
+
         describe "when BraintreeCustomer is new" do
           describe "with basic params" do
             subject do
@@ -69,11 +73,34 @@ describe "Braintree API" do
                 post api_braintree_transaction_path(page.id), params
               end
             end
+
             include_examples "creates nothing"
-            it "passes the params to braintree"
-            it "does not update the member"
-            it "serializes errors in JSON"
-            it "creates a Transaction associated with the page storing relevant info"
+
+            it "does not update the member" do
+              expect{ subject }.not_to change{ member.reload }
+            end
+
+            it "serializes errors in JSON" do
+              subject
+              errors = {success: false, errors: [{declined: true, code: "2002", message: "Limit Exceeded"}]}
+              expect( response.body ).to eq errors.to_json
+            end
+
+            it "creates a Transaction associated with the page storing relevant info" do
+              expect{ subject }.to change{ Payment::BraintreeTransaction.count }.by 1
+              transaction = Payment::BraintreeTransaction.last
+              expect(transaction.page).to eq page
+              expect(transaction.amount).to eq 2002
+              expect(transaction.currency).to eq 'EUR'
+              expect(transaction.merchant_account_id).to eq 'EUR'
+              expect(transaction.payment_instrument_type).to eq 'credit_card'
+              expect(transaction.transaction_type).to eq 'sale'
+              expect(transaction.customer_id).to eq nil
+              expect(transaction.status).to eq 'failure'
+              expect(transaction.processor_response_code).to eq '2002'
+              expect(transaction.payment_method_token).to eq nil
+              expect(transaction.transaction_id).to match a_string_matching(token_format)
+            end
           end
           describe "with Paypal" do
             # include_examples "creates nothing"
@@ -86,7 +113,6 @@ describe "Braintree API" do
           describe "with Paypal" do
             # include_examples "creates nothing"
             it "does not update the member"
-            it "passes the params to braintree"
             it "does not update the BraintreeCustomer in the database"
             it "creates a Transaction associated with the page storing relevant info"
             it "serializes errors in JSON"
