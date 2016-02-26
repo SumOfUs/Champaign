8b92c4db9ae29bb7a43940ace0baa4a1252bcfa5
tuuli <tuuli.pollanen@gmail.com>
2016-02-26 14:24:11 -0800

    ;___________________;

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,9 +33,8 @@
 
 //= require show_errors
 //= require dropzone_image_upload
-//= require selectize_config.js
-//= require search.js
-//= require configure_quill_editor
+//= require selectize_config
+//= require search
 //= require configure_wysiwyg
 //= require_tree ./plugins/admin
 
