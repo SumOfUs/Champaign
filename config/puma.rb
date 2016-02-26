b253e368366933c2c86517f15405a2daecddf7e9
Omar Sahyoun <osahyoun@gmail.com>
2016-02-26 10:42:13 +0000

    Get puma up again - more testing

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['MAX_THREADS'] || 1)
-threads threads_count, threads_count
+threads 1, threads_count
 
 preload_app!
 
@@ -8,9 +8,3 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RAILS_ENV'] || 'development'
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-  Rails.logger.debug "***** Puma worker has booted *****"
-end
