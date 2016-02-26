cf70eb35d8f5bf37b21a1d31b3bd10b41ffe8638
tuuli <tuuli.pollanen@gmail.com>
2016-02-25 17:40:05 -0800

    When search parameters have been passed to /pages/, make the filtering experience easier by pre-filling the filter form

--- app/assets/javascripts/selectize_config.js
+++ app/assets/javascripts/selectize_config.js
@@ -1,6 +1,15 @@
 $(function(){
-  $('.selectize-container').selectize({
+  $select = $('.selectize-container').selectize({
     plugins: ['remove_button'],
     closeAfterSelect: true
   });
+
+ $('.page-filter__reset').click( function() {
+   $select.map(function(index, item){
+     item.selectize.clear();
+   });
+   $(this).closest('form').find('.form-control').map(function(index, item){
+     $(item).val("");
+   });
+ });
 });
