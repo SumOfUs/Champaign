e8a92f19e14ac35d58de964db2431b1ab90b1b07
tuuli <tuuli.pollanen@gmail.com>
2016-03-16 15:33:01 -0700

    Factories for braintree payment tokens and for associating a customer created with the factory with several of them

---
+++ spec/factories/braintree_payment_method_token.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :braintree_payment_method_token, :class => 'Payment::BraintreePaymentMethodToken' do
+    braintree_customer_id { Faker::Number.number(6) }
+    braintree_payment_method_token "MyString"
+  end
+end
--- spec/factories/payment_braintree_customers.rb
+++ spec/factories/payment_braintree_customers.rb
@@ -11,5 +11,31 @@ FactoryGirl.define do
     first_name "MyString"
     last_name "MyString"
     customer_id { Faker::Number.number(6) }
+
+    trait :with_payment_method_tokens do
+      # payment_methods is declared as a transient attribute and available in
+      # attributes on the factory, as well as the callback via the evaluator
+      transient do
+        payment_methods 3
+      end
+
+      # the after(:create) yields two values; the user instance itself and the
+      # evaluator, which stores all values from the factory, including transient
+      after(:create) do |customer, evaluator|
+        tokens = []
+        evaluator.payment_methods.times do |p|
+          tokens.push({
+                          braintree_payment_method_token: Faker::Lorem.word,
+                          braintree_customer_id: customer.id
+                      })
+        end
+        # Make sure that the token described in customer.default_payment_method_token is a part of the customer's
+        # newly generated braintree payment method tokens.
+        tokens.last[:braintree_payment_method_token] = customer.default_payment_method_token
+        tokens.map { |t| FactoryGirl.create(:braintree_payment_method_token, t) }
+      end
+
+    end
+
   end
 end
