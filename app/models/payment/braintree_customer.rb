c3b5d556b72148fa009c6fe9762f2daadec8ef5b
tuuli <tuuli.pollanen@gmail.com>
2016-03-16 15:32:07 -0700

    Migrations and model changes for multiple payment braintree tokens per customer

--- app/models/payment/braintree_customer.rb
+++ app/models/payment/braintree_customer.rb
@@ -1,3 +1,4 @@
 class Payment::BraintreeCustomer < ActiveRecord::Base
   belongs_to :member
+  has_many :braintree_payment_method_tokens, class_name: Payment::BraintreePaymentMethodToken
 end
--- db/migrate/20160314215202_multiple_payment_method_tokens.rb
+++ db/migrate/20160314215202_multiple_payment_method_tokens.rb
@@ -1,7 +1,7 @@
 class MultiplePaymentMethodTokens < ActiveRecord::Migration
   def change
-    create_table :braintree_payment_method_tokens do |t|
-      t.string :customer_id
+    create_table :payment_braintree_payment_method_tokens do |t|
+      t.string :braintree_customer_id
       t.string :braintree_payment_method_token
       t.timestamps null: false
     end
