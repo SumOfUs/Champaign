abfee405188a0195177c2f43f157116a0018ce7e
tuuli <tuuli.pollanen@gmail.com>
2016-03-18 17:33:14 -0700

    Add some more logic towards using tokens from a table instead of storing their value as strings...

---
+++ db/migrate/20160318231720_make_transaction_payment_method_token_foreign_key.rb
@@ -0,0 +1,5 @@
+class MakeTransactionPaymentMethodTokenForeignKey < ActiveRecord::Migration
+  def change
+    rename_column :payment_braintree_transactions, :payment_method_token, :payment_method_token_id
+  end
+end
