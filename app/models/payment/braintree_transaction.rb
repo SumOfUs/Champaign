8142e8a15f5780413f8e5504c5a4fabe77baeaa2
tuuli <tuuli.pollanen@gmail.com>
2016-04-06 09:33:15 -0700

    Make transaction look for customer by customer_id.

--- app/models/payment/braintree_transaction.rb
+++ app/models/payment/braintree_transaction.rb
@@ -1,6 +1,6 @@
 class Payment::BraintreeTransaction < ActiveRecord::Base
   belongs_to :page
   belongs_to :payment_method, class_name: 'Payment::BraintreePaymentMethod'
-  belongs_to :customer,       class_name: 'Payment::BraintreeCustomer'
+  belongs_to :customer,       class_name: 'Payment::BraintreeCustomer', primary_key: 'customer_id'
   enum status: [:success, :failure]
 end
