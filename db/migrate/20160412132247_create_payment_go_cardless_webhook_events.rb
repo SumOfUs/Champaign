069881c5b90e8401d2de0b3e8615f4f10f73fd50
Omar Sahyoun <osahyoun@gmail.com>
2016-04-12 15:51:47 +0100

    Store event body

--- db/migrate/20160412132247_create_payment_go_cardless_webhook_events.rb
+++ db/migrate/20160412132247_create_payment_go_cardless_webhook_events.rb
@@ -4,6 +4,7 @@ class CreatePaymentGoCardlessWebhookEvents < ActiveRecord::Migration
       t.string :event_id, index: true
       t.string :resource_type
       t.string :action
+      t.text :body
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -347,6 +347,7 @@ ActiveRecord::Schema.define(version: 20160412132247) do
     t.string   "event_id"
     t.string   "resource_type"
     t.string   "action"
+    t.text     "body"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
