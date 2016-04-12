672c2094709638757b4aaf0e8258ce4993a5f7ba
tuuli <tuuli.pollanen@gmail.com>
2016-04-12 12:25:11 -0700

    Extract subscription and transaction specific logic into their own classes, and make them inherit from a super class with the shared methods

--- lib/payment_processor/go_cardless/subscription.rb
+++ lib/payment_processor/go_cardless/subscription.rb
@@ -1,35 +1,25 @@
 module PaymentProcessor
   module GoCardless
-    class Transaction < Populator
-      # = GoCardless::Transaction
+    class Subscription < Populator
+      # = GoCardless::Subscription
       #
       # Wrapper around GoCardless's Ruby SDK. This class essentially just stuffs parameters
       # into the keys that are expected by GoCardless's class.
       #
       # == Usage
       #
-      # Call <tt>PaymentProcessor::Clients::GoCardless::Transaction.make_transaction</tt>
+      # Call <tt>PaymentProcessor::Clients::GoCardless::Subscription.make_subscription</tt>
       #
-      # === Options #
-      # * +:amount+   - Billing amount (required)
-      # * +:currency+ - Billing currency (required)
-      # * +:user+     - Hash of information describing the customer. Must include email, and name (required)
-      # * +:customer+ - Instance of existing GoCardless customer. Must respond to +customer_id+ (optional)
       attr_reader :result, :action
 
-      def self.make_transaction(params, session_id)
-        new(params, session_id).transaction
+      def self.make_subscription(params, session_id)
+        new(params, session_id).subscription
       end
 
       def initialize(params, session_id)
         @amount = (params[:amount].to_f * 100).to_i # Price in pence/cents
         @redirect_flow_id = params[:redirect_flow_id]
         @session_token = session_id
-      end
-
-      def transaction
-        transaction = client.payments.create(params: transaction_params)
-        # TODO: persist transaction locally
       end
 
       def subscription
