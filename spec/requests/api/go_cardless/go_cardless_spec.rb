9dbbf46d0f33d275dc9dafd3ea1d0a11d8b5d9b2
Neal Donnelly <NealJMD@gmail.com>
2016-04-29 15:17:46 -0600

    add failure specs for subscriptions and transactions

--- spec/requests/api/go_cardless/go_cardless_spec.rb
+++ spec/requests/api/go_cardless/go_cardless_spec.rb
@@ -6,6 +6,13 @@ describe "GoCardless API" do
   let(:usd_amount) { 9.99 }
   let(:gbp_amount) { 11.55 }
 
+  let(:bad_request_errors) do
+    [{
+      code: 422,
+      message: "Our technical team has been notified. Please double check your info or try a different payment method."
+    }]
+  end
+
   before :each do
     allow_any_instance_of(Money).to receive(:exchange_to).and_return(
       instance_double(Money, cents: (gbp_amount*100).to_i)
@@ -76,10 +83,7 @@ describe "GoCardless API" do
 
       it 'assigns errors to be the relevant error message' do
         subject
-        expect(assigns(:errors)).to eq([{
-          code: 422,
-          message: "Our technical team has been notified. Please double check your info or try a different payment method."
-        }])
+        expect(assigns(:errors)).to eq(bad_request_errors)
       end
     end
 
@@ -181,203 +185,315 @@ describe "GoCardless API" do
       allow_any_instance_of(GoCardlessPro::Services::RedirectFlowsService).to receive(:complete).and_return(completed_flow)
     end
 
-    shared_examples 'donation action' do
-      it 'creates a PaymentMethod record that stores the mandate id' do
-        expect{ subject }.to change{ Payment::GoCardless::PaymentMethod.count }.by 1
-        mandate = Payment::GoCardless::PaymentMethod.last
-        expect(mandate.go_cardless_id).to eq mandate_id
-      end
+    context 'successful' do
 
-      it 'creates an Action associated with the Page and Member' do
-        expect{ subject }.to change{ Action.count }.by 1
-        action = Action.last
-        expect(action.page).to eq page
-        expect(action.member).to eq (member.blank? ? Member.last : member)
-      end
-
-      it 'increments redis counters' do
-        allow(Analytics::Page).to receive(:increment).and_return(7777)
-        subject
-        expect(Analytics::Page).to have_received(:increment).with(page.id, new_member: member.blank?)
-      end
-
-      it 'leaves a cookie with the member_id' do
-        expect(cookies['member_id']).to eq nil
-        subject
-
-        # we can't access signed cookies in request specs, so check for hashed value
-        expect(cookies['member_id']).not_to be_nil
-        expect(cookies['member_id'].length).to be > 20
-      end
-
-      it 'increments action count on Page' do
-        expect{ subject }.to change{ page.reload.action_count }.by 1
-      end
-
-      it "redirects to the page's follow-up path" do
-        subject
-        expect(response.status).to redirect_to(follow_up_page_path(page))
-      end
-    end
-
-    describe 'transaction' do
-
-      let(:params) { base_params.merge(recurring: false) }
-      let(:converted_money) { instance_double(Money, cents: 9001) }
-
-      subject do
-        VCR.use_cassette('go_cardless successful transaction') do
-          get api_go_cardless_transaction_path(page.id), params
-        end
-      end
-
-      shared_examples 'successful transaction' do
-        it 'passes the correct data to the GoCardless Payment SDK' do
-          payment_service = instance_double(GoCardlessPro::Services::PaymentsService, create: double(id: 'asdf'))
-          allow_any_instance_of(GoCardlessPro::Client).to receive(:payments).and_return(payment_service)
-          expect(payment_service).to receive(:create).with(sdk_params)
-          subject
+      shared_examples 'donation action' do
+        it 'creates a PaymentMethod record that stores the mandate id' do
+          expect{ subject }.to change{ Payment::GoCardless::PaymentMethod.count }.by 1
+          mandate = Payment::GoCardless::PaymentMethod.last
+          expect(mandate.go_cardless_id).to eq mandate_id
         end
 
-        it 'posts donation action to queue with correct data' do
-          allow( ChampaignQueue ).to receive(:push)
-          expect( ChampaignQueue ).to receive(:push).with(donation_push_params)
-          subject
-        end
-
-        it 'stores amount, currency, is_subscription, and transaction_id in form_data on the Action' do
-          expect{ subject }.to change{ Action.count }.by(1)
-          form_data = Action.last.form_data
-          expect(form_data['is_subscription']).to be(false)
-          expect(form_data['amount']).to eq(gbp_amount.to_s)
-          expect(form_data['currency']).to eq('GBP')
-          expect(form_data['transaction_id']).to eq(Payment::GoCardless::Transaction.last.go_cardless_id)
-          expect(form_data).not_to have_key('subscription_id')
-        end
-
-        it 'creates a Transaction record associated with the Page' do
-          expect{ subject }.to change{ Payment::GoCardless::Transaction.count }.by 1
-          payment = Payment::GoCardless::Transaction.last
-          expect(payment.go_cardless_id).to match(/^PM[0-9A-Z]+/)
-          expect(payment.currency).to eq 'GBP'
-          expect(payment.amount).to eq gbp_amount
-        end
-      end
-
-      describe 'when Member exists' do
-        let!(:member) { create :member, email: email }
-
-        include_examples 'successful transaction'
-        include_examples 'donation action'
-
-        it "updates the Member's fields with any new data" do
-          expect(member.email).to eq email
-          expect(member.country).to be_blank
-          expect(member.postal).to be_blank
-          expect{ subject }.not_to change{ Member.count }
-          member.reload
-          expect(member.country).to eq "US"
-          expect(member.postal).to eq "11225"
-        end
-      end
-
-      describe 'when Member is new' do
-        let(:member) { nil }
-
-        include_examples 'successful transaction'
-        include_examples 'donation action'
-
-        it "populates the Member’s fields with form data" do
-          expect{ subject }.to change{ Member.count }.by 1
-          member = Member.last
-          expect(member.country).to eq "US"
-          expect(member.postal).to eq "11225"
-        end
-      end
-    end
-
-    describe 'subscription' do
-
-      let(:params) { base_params.merge(recurring: true) }
-
-      subject do
-        VCR.use_cassette('go_cardless successful subscription') do
-          get api_go_cardless_transaction_path(page.id), params
-        end
-      end
-
-      shared_examples 'successful subscription' do
-        it 'passes the correct data to the GoCardless Payment SDK' do
-          sdk_params[:params] = sdk_params[:params].merge(
-            name: "donation",
-            interval_unit: "monthly",
-            day_of_month: "1"
-          )
-          subscriptions_service = instance_double(GoCardlessPro::Services::SubscriptionsService, create: double(id: 'asdf'))
-          allow_any_instance_of(GoCardlessPro::Client).to receive(:subscriptions).and_return(subscriptions_service)
-          expect(subscriptions_service).to receive(:create).with(sdk_params)
-          subject
-        end
-
-        it 'posts donation action to queue with correct data' do
-          allow( ChampaignQueue ).to receive(:push)
-
-          subject
-
-          donation_push_params[:params][:action][:fields] = {
-            recurring_id: Member.first.id,
-            recurrence_number: 0,
-            payment_provider: "go_cardless"
-          }
-
-          expect( ChampaignQueue ).to have_received(:push).with(donation_push_params)
-        end
-
-        it 'stores amount, currency, is_subscription, and subscription_id in form_data on the Action' do
+        it 'creates an Action associated with the Page and Member' do
           expect{ subject }.to change{ Action.count }.by 1
-          form_data = Action.last.form_data
-          expect(form_data['is_subscription']).to eq true
-          expect(form_data['amount']).to eq gbp_amount.to_s
-          expect(form_data['currency']).to eq 'GBP'
-          expect(form_data['subscription_id']).to eq Payment::GoCardless::Subscription.last.go_cardless_id
-          expect(form_data).not_to have_key('transaction_id')
+          action = Action.last
+          expect(action.page).to eq page
+          expect(action.member).to eq (member.blank? ? Member.last : member)
         end
 
-        it 'does not yet create a transaction record' do
+        it 'increments redis counters' do
+          allow(Analytics::Page).to receive(:increment).and_return(7777)
+          subject
+          expect(Analytics::Page).to have_received(:increment).with(page.id, new_member: member.blank?)
+        end
+
+        it 'leaves a cookie with the member_id' do
+          expect(cookies['member_id']).to eq nil
+          subject
+
+          # we can't access signed cookies in request specs, so check for hashed value
+          expect(cookies['member_id']).not_to be_nil
+          expect(cookies['member_id'].length).to be > 20
+        end
+
+        it 'increments action count on Page' do
+          expect{ subject }.to change{ page.reload.action_count }.by 1
+        end
+
+        it "redirects to the page's follow-up path" do
+          subject
+          expect(response.status).to redirect_to(follow_up_page_path(page))
+        end
+      end
+
+      describe 'transaction' do
+
+        let(:params) { base_params.merge(recurring: false) }
+        let(:converted_money) { instance_double(Money, cents: 9001) }
+
+        subject do
+          VCR.use_cassette('go_cardless successful transaction') do
+            get api_go_cardless_transaction_path(page.id), params
+          end
+        end
+
+        shared_examples 'successful transaction' do
+          it 'passes the correct data to the GoCardless Payment SDK' do
+            payment_service = instance_double(GoCardlessPro::Services::PaymentsService, create: double(id: 'asdf'))
+            allow_any_instance_of(GoCardlessPro::Client).to receive(:payments).and_return(payment_service)
+            expect(payment_service).to receive(:create).with(sdk_params)
+            subject
+          end
+
+          it 'posts donation action to queue with correct data' do
+            allow( ChampaignQueue ).to receive(:push)
+            expect( ChampaignQueue ).to receive(:push).with(donation_push_params)
+            subject
+          end
+
+          it 'stores amount, currency, is_subscription, and transaction_id in form_data on the Action' do
+            expect{ subject }.to change{ Action.count }.by(1)
+            form_data = Action.last.form_data
+            expect(form_data['is_subscription']).to be(false)
+            expect(form_data['amount']).to eq(gbp_amount.to_s)
+            expect(form_data['currency']).to eq('GBP')
+            expect(form_data['transaction_id']).to eq(Payment::GoCardless::Transaction.last.go_cardless_id)
+            expect(form_data).not_to have_key('subscription_id')
+          end
+
+          it 'creates a Transaction record associated with the Page' do
+            expect{ subject }.to change{ Payment::GoCardless::Transaction.count }.by 1
+            payment = Payment::GoCardless::Transaction.last
+            expect(payment.go_cardless_id).to match(/^PM[0-9A-Z]+/)
+            expect(payment.currency).to eq 'GBP'
+            expect(payment.amount).to eq gbp_amount
+          end
+        end
+
+        describe 'when Member exists' do
+          let!(:member) { create :member, email: email }
+
+          include_examples 'successful transaction'
+          include_examples 'donation action'
+
+          it "updates the Member's fields with any new data" do
+            expect(member.email).to eq email
+            expect(member.country).to be_blank
+            expect(member.postal).to be_blank
+            expect{ subject }.not_to change{ Member.count }
+            member.reload
+            expect(member.country).to eq "US"
+            expect(member.postal).to eq "11225"
+          end
+        end
+
+        describe 'when Member is new' do
+          let(:member) { nil }
+
+          include_examples 'successful transaction'
+          include_examples 'donation action'
+
+          it "populates the Member’s fields with form data" do
+            expect{ subject }.to change{ Member.count }.by 1
+            member = Member.last
+            expect(member.country).to eq "US"
+            expect(member.postal).to eq "11225"
+          end
+        end
+      end
+
+      describe 'subscription' do
+
+        let(:params) { base_params.merge(recurring: true) }
+
+        subject do
+          VCR.use_cassette('go_cardless successful subscription') do
+            get api_go_cardless_transaction_path(page.id), params
+          end
+        end
+
+        shared_examples 'successful subscription' do
+          it 'passes the correct data to the GoCardless Payment SDK' do
+            sdk_params[:params] = sdk_params[:params].merge(
+              name: "donation",
+              interval_unit: "monthly",
+              day_of_month: "1"
+            )
+            subscriptions_service = instance_double(GoCardlessPro::Services::SubscriptionsService, create: double(id: 'asdf'))
+            allow_any_instance_of(GoCardlessPro::Client).to receive(:subscriptions).and_return(subscriptions_service)
+            expect(subscriptions_service).to receive(:create).with(sdk_params)
+            subject
+          end
+
+          it 'posts donation action to queue with correct data' do
+            allow( ChampaignQueue ).to receive(:push)
+
+            subject
+
+            donation_push_params[:params][:action][:fields] = {
+              recurring_id: Member.first.id,
+              recurrence_number: 0,
+              payment_provider: "go_cardless"
+            }
+
+            expect( ChampaignQueue ).to have_received(:push).with(donation_push_params)
+          end
+
+          it 'stores amount, currency, is_subscription, and subscription_id in form_data on the Action' do
+            expect{ subject }.to change{ Action.count }.by 1
+            form_data = Action.last.form_data
+            expect(form_data['is_subscription']).to eq true
+            expect(form_data['amount']).to eq gbp_amount.to_s
+            expect(form_data['currency']).to eq 'GBP'
+            expect(form_data['subscription_id']).to eq Payment::GoCardless::Subscription.last.go_cardless_id
+            expect(form_data).not_to have_key('transaction_id')
+          end
+
+          it 'does not yet create a transaction record' do
+            expect{ subject }.not_to change{ Payment::GoCardless::Transaction.count }
+          end
+        end
+
+        describe 'when Member exists' do
+          let!(:member) { create :member, email: email }
+
+          include_examples 'successful subscription'
+          include_examples 'donation action'
+
+          it "updates the Member's fields with any new data" do
+            expect(member.email).to eq email
+            expect(member.country).to be_blank
+            expect(member.postal).to be_blank
+            expect{ subject }.not_to change{ Member.count }
+            member.reload
+            expect(member.country).to eq "US"
+            expect(member.postal).to eq "11225"
+          end
+        end
+
+        describe 'when Member is new' do
+          let(:member) { nil }
+
+          include_examples 'successful subscription'
+          include_examples 'donation action'
+
+          it "populates the Member’s fields with form data" do
+            expect{ subject }.to change{ Member.count }.by 1
+            member = Member.last
+            expect(member.country).to eq "US"
+            expect(member.postal).to eq "11225"
+          end
+        end
+      end
+    end
+
+    context 'unsuccessful', :focus do
+
+      shared_examples 'correctly handles errors' do
+
+        it 'does not create a PaymentMethod' do
+          expect{ subject }.not_to change{ Payment::GoCardless::PaymentMethod.count }
+        end
+
+        it 'does not create a Transaction' do
           expect{ subject }.not_to change{ Payment::GoCardless::Transaction.count }
         end
-      end
 
-      describe 'when Member exists' do
-        let!(:member) { create :member, email: email }
+        it 'does not create a Subscription' do
+          expect{ subject }.not_to change{ Payment::GoCardless::Subscription.count }
+        end
 
-        include_examples 'successful subscription'
-        include_examples 'donation action'
+        it 'does not create an Action' do
+          expect{ subject }.not_to change{ Action.count }
+        end
 
-        it "updates the Member's fields with any new data" do
-          expect(member.email).to eq email
-          expect(member.country).to be_blank
-          expect(member.postal).to be_blank
+        it 'does not create a Member' do
           expect{ subject }.not_to change{ Member.count }
-          member.reload
-          expect(member.country).to eq "US"
-          expect(member.postal).to eq "11225"
+        end
+
+        it 'does not push to the queue' do
+          allow( ChampaignQueue ).to receive(:push)
+          expect( ChampaignQueue ).not_to receive(:push)
+          subject
+        end
+
+        it 'does not leave a cookie' do
+          expect(cookies['member_id']).to eq nil
+          subject
+          expect(cookies['member_id']).to eq nil
+        end
+
+        it 'does not increment redis counters' do
+          allow(Analytics::Page).to receive(:increment).and_return(7777)
+          subject
+          expect(Analytics::Page).not_to have_received(:increment)
+        end
+
+        it 'does not change the action count on Page' do
+          expect{ subject }.not_to change{ page.reload.action_count }
+        end
+
+        it 'assigns errors to relevant errors' do
+          subject
+          expect(assigns(:errors)).to eq(bad_request_errors)
+        end
+
+        it 'assigns page to current page' do
+          subject
+          expect(assigns(:page)).to eq page
+        end
+
+        it 'renders payment/donation_errors' do
+          subject
+          expect(response).to render_template 'payment/donation_errors'
         end
       end
 
-      describe 'when Member is new' do
-        let(:member) { nil }
+      describe 'mandate retrieval' do
 
-        include_examples 'successful subscription'
-        include_examples 'donation action'
-
-        it "populates the Member’s fields with form data" do
-          expect{ subject }.to change{ Member.count }.by 1
-          member = Member.last
-          expect(member.country).to eq "US"
-          expect(member.postal).to eq "11225"
-        end
       end
+
+      describe 'redirect flow completion' do
+
+      end
+
+      describe 'transaction' do
+
+        before :each do
+          # to make the creation fail
+          allow_any_instance_of(GoCardlessPro::Resources::Mandate).to receive(:id).and_return(nil)
+        end
+
+        let(:params) { base_params.merge(recurring: false) }
+
+        subject do
+          VCR.use_cassette('go_cardless unsuccessful transaction') do
+            get api_go_cardless_transaction_path(page.id), params
+          end
+        end
+
+        include_examples 'correctly handles errors'
+
+      end
+
+      describe 'subscription' do
+
+        before :each do
+          # to make the creation fail
+          allow_any_instance_of(GoCardlessPro::Resources::Mandate).to receive(:id).and_return(nil)
+        end
+
+        let(:params) { base_params.merge(recurring: true) }
+
+        subject do
+          VCR.use_cassette('go_cardless unsuccessful subscription') do
+            get api_go_cardless_transaction_path(page.id), params
+          end
+        end
+
+        include_examples 'correctly handles errors'
+      end
+
     end
 
   end
