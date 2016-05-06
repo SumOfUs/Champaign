cb1deab9aa3fdbe1a562dbf9b590841469a3e0f9
Neal Donnelly <NealJMD@gmail.com>
2016-05-06 13:47:28 -0600

    finish spec for error processing class

--- spec/lib/payment_processor/go_cardless/error_processing_spec.rb
+++ spec/lib/payment_processor/go_cardless/error_processing_spec.rb
@@ -10,37 +10,150 @@ module PaymentProcessor
           environment:  Settings.gocardless.environment.to_sym
         ) 
       end
+      let(:request_id) { 'dd50eaaf-8213' }
 
       before :each do
         allow(Rails.logger).to receive(:error)
       end
 
-      subject{ described_class.new(@error).process }
+      subject{ described_class.new(error).process }
 
-      describe 'with a GoCardless internal error', :pending do
+      describe 'with a GoCardless internal error' do
 
-        before :each do
-          begin
-            client.redirect_flows.create(params: {})
-          rescue GoCardlessPro::GoCardlessError => e
-            @error = e
-          end
+        let(:error) do
+          instance_double(
+            GoCardlessPro::GoCardlessError,
+            class: GoCardlessPro::GoCardlessError,
+            type: 'gocardless',
+            message: '500 - internal server error',
+            code: 500,
+            request_id: request_id
+          )
         end
 
         it 'logs the error to the Rails logger' do
-          expect(Rails.logger).to receive(:error).with('')
+          expect(Rails.logger).to receive(:error).with("GoCardlessPro::GoCardlessError: 500 - internal server error. Please report to GoCardless support staff. (request: #{request_id})")
           subject
         end
 
         it 'passes back the correct error' do
-          expect(subject).to equal([{code: 500, message: I18n.t('fundraiser.unknown_error')}])
+          expect(subject).to eq [{code: 500, message: I18n.t('fundraiser.unknown_error')}]
         end
       end
 
-      describe 'with an API usage error'
-      describe 'with a state error'
-      describe 'with a validation error'
-      describe 'with a generic error'
+      describe 'with an API usage error' do
+        let(:error) do
+          instance_double(
+            GoCardlessPro::InvalidApiUsageError,
+            class: GoCardlessPro::InvalidApiUsageError,
+            type: 'invalid_api_usage',
+            message: 'Invalid document structure',
+            code: 400,
+            backtrace: ["  undefined method `type' for nil:NilClass",
+                        "# ./lib/payment_processor/go_cardless/error_processing.rb:11:in `process'",
+                        "# ./spec/lib/payment_processor/go_cardless/error_processing_spec.rb:18:in `block (2 levels) in <module:GoCardless>'"],
+            request_id: request_id,
+            errors: [{
+              "reason": "invalid_document_structure",
+              "message": "Invalid document structure"
+            }]
+          )
+        end
+
+        it 'logs the error to the Rails logger' do
+          expect(Rails.logger).to receive(:error).with("GoCardlessPro::InvalidApiUsageError:   undefined method `type' for nil:NilClass\n# ./lib/payment_processor/go_cardless/error_processing.rb:11:in `process'\n# ./spec/lib/payment_processor/go_cardless/error_processing_spec.rb:18:in `block (2 levels) in <module:GoCardless>' (request: dd50eaaf-8213)")
+          subject
+        end
+
+        it 'passes back the correct error' do
+          expect(subject).to eq [{code: 400, message: "Our technical team has been notified. Please double check your info or try a different payment method."}]
+        end
+      end
+
+      describe 'with a state error' do
+        let(:error) do
+          instance_double(
+            GoCardlessPro::InvalidStateError,
+            class: GoCardlessPro::InvalidStateError,
+            type: 'invalid_state',
+            message: 'This payment has already been canceled',
+            code: 409,
+            errors: [{
+              'reason' =>'cancellation_failed',
+              'message' =>'This payment has already been canceled'
+            }],
+            request_id: request_id
+          )
+        end
+
+        it 'logs the error to the Rails logger' do
+          expect(Rails.logger).to receive(:error).with("GoCardlessPro::InvalidStateError: This payment has already been canceled. This payment has already been canceled (request: #{request_id})")
+          subject
+        end
+
+        it 'passes back the correct error' do
+          expect(subject).to eq [{code: 409, message: "This payment has already been canceled"}]
+        end
+      end
+
+      describe 'with a validation error' do
+        let(:error) do
+          instance_double(
+            GoCardlessPro::Error,
+            class: GoCardlessPro::Error,
+            type: 'validation_failed',
+            message: "Validation failed",
+            code: 422,
+            errors: [
+              {
+                "field" => "branch_code",
+                "message" => "must be a number",
+                "request_pointer" => "/customer_bank_accounts/branch_code"
+              }, {
+                "field" => "branch_code",
+                "message" => "is the wrong length (should be 8 characters)",
+                "request_pointer" => "/customer_bank_accounts/branch_code"
+              }
+            ],
+            request_id: request_id
+          )
+        end
+
+        it 'logs the error to the Rails logger' do
+          expect(Rails.logger).to receive(:error).with("GoCardlessPro::Error: branch_code must be a number. branch_code is the wrong length (should be 8 characters) (request: #{request_id})")
+          subject
+        end
+
+        it 'passes back the correct error' do
+          expect(subject).to eq [{code: 422, attribute: "branch_code", message: "branch_code must be a number"}, {code: 422, attribute: "branch_code", message: "branch_code is the wrong length (should be 8 characters)"}]
+        end
+      end
+
+      describe 'with a generic error' do
+        let(:error) do
+          instance_double(
+            GoCardlessPro::Error,
+            class: GoCardlessPro::Error,
+            type: '',
+            message: 'Unknown error',
+            code: 500,
+            request_id: request_id,
+            backtrace: ["  undefined method `type' for nil:NilClass",
+                        "# ./lib/payment_processor/go_cardless/error_processing.rb:11:in `process'",
+                        "# ./spec/lib/payment_processor/go_cardless/error_processing_spec.rb:18:in `block (2 levels) in <module:GoCardless>'"]
+          )
+        end
+
+        it 'logs the error to the Rails logger' do
+          expect(Rails.logger).to receive(:error).with("GoCardlessPro::Error:   undefined method `type' for nil:NilClass\n# ./lib/payment_processor/go_cardless/error_processing.rb:11:in `process'\n# ./spec/lib/payment_processor/go_cardless/error_processing_spec.rb:18:in `block (2 levels) in <module:GoCardless>' (request: #{request_id})")
+          subject
+        end
+
+        it 'passes back the correct error' do
+          expect(subject).to eq [code: 500, message: "Our technical team has been notified. Please double check your info or try a different payment method."]
+        end
+      end
+
 
     end
   end
