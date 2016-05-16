0d546459935efb46c5559a462bf58c0676aacd75
Neal Donnelly <NealJMD@gmail.com>
2016-05-16 10:53:10 -0400

    fix locale on view smoke tests to prevent occasional failures

--- spec/views/smoke_tests_spec.rb
+++ spec/views/smoke_tests_spec.rb
@@ -1,55 +1,62 @@
 require 'rails_helper'
 require_relative 'shared_examples'
 
-describe 'campaigns/' do
-  include_examples "view smoke test", :campaign
-end
+describe 'renderin smoke tests' do
 
-describe 'donation_bands/' do
-  include_examples "view smoke test", :donation_band, [:edit, :index, :new]
-end
-
-describe 'forms/' do
-  include_examples "view smoke test", :form, [:index, :new]
-
-  describe "edit" do
-    it 'renders without error' do
-      assign :form, build(:form, id: 1)
-      assign :form_element, FormElement.new
-      expect{ render template: "forms/edit" }.not_to raise_error
-    end
-  end
-end
-
-describe 'liquid_layouts/' do
-  include_examples "view smoke test", :liquid_layout
-end
-
-describe 'liquid_partials/' do
-  include_examples "view smoke test", :liquid_partial
-end
-
-describe 'pages/' do
-  before :each do
-    allow(view).to receive(:user_signed_in?).and_return(true)
+  before :all do
+    I18n.locale = :en
   end
 
-  include_examples "view smoke test", :page, [:new, :show]
+  describe 'campaigns/' do
+    include_examples "view smoke test", :campaign
+  end
 
-  describe "edit" do
-    it 'renders without error' do
-      assign :page, build(:page, id: 1)
-      assign :variations, [build(:share_facebook, id: 1)]
-      expect{ render template: "pages/edit" }.not_to raise_error
+  describe 'donation_bands/' do
+    include_examples "view smoke test", :donation_band, [:edit, :index, :new]
+  end
+
+  describe 'forms/' do
+    include_examples "view smoke test", :form, [:index, :new]
+
+    describe "edit" do
+      it 'renders without error' do
+        assign :form, build(:form, id: 1)
+        assign :form_element, FormElement.new
+        expect{ render template: "forms/edit" }.not_to raise_error
+      end
     end
   end
 
-  describe "index" do
-    it 'renders without error' do
-      3.times { build(:page) }
-      assign :pages, Page.all
-      assign :search_params, {}
-      expect{ render template: "pages/index" }.not_to raise_error
+  describe 'liquid_layouts/' do
+    include_examples "view smoke test", :liquid_layout
+  end
+
+  describe 'liquid_partials/' do
+    include_examples "view smoke test", :liquid_partial
+  end
+
+  describe 'pages/' do
+    before :each do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+    end
+
+    include_examples "view smoke test", :page, [:new, :show]
+
+    describe "edit" do
+      it 'renders without error' do
+        assign :page, build(:page, id: 1)
+        assign :variations, [build(:share_facebook, id: 1)]
+        expect{ render template: "pages/edit" }.not_to raise_error
+      end
+    end
+
+    describe "index" do
+      it 'renders without error' do
+        3.times { build(:page) }
+        assign :pages, Page.all
+        assign :search_params, {}
+        expect{ render template: "pages/index" }.not_to raise_error
+      end
     end
   end
 end
