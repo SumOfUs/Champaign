557fbd890bc00557ca9457aed60a26c67fc97894
Omar Sahyoun <osahyoun@gmail.com>
2016-06-06 14:16:27 +0100

    UX

--- app/controllers/page_cloner_controller.rb
+++ app/controllers/page_cloner_controller.rb
@@ -1,4 +1,8 @@
-class PageClonerController < ApplicationController
+class ClonePagesController < ApplicationController
   def new
+    @page = Page.find params[:id]
+  end
+
+  def create
   end
 end
