da2f51e5cbdd0e935cf3e76698a0457172375975
Omar Sahyoun <osahyoun@gmail.com>
2016-06-08 13:47:04 +0100

    Use before action

--- app/controllers/clone_pages_controller.rb
+++ app/controllers/clone_pages_controller.rb
@@ -1,19 +1,19 @@
 class ClonePagesController < ApplicationController
   before_action :authenticate_user!
+  before_filter :find_page
 
   def new
-    page
   end
 
   def create
-    new_page = PageCloner.clone(page, params[:page][:title])
+    new_page = PageCloner.clone(@page, params[:page][:title])
     QueueManager.push(new_page, job_type: :create)
     redirect_to edit_page_path(new_page)
   end
 
   private
 
-  def page
+  def find_page
     @page ||= Page.find params[:id]
   end
 end
