b77a45a59505c4d6a671e2d737fea6dbaacfe7f7
tuuli <tuuli.pollanen@gmail.com>
2016-06-14 11:49:12 -0700

    Use HTTParty instead of Typofuss for AK helper

--- lib/action_kit/helper.rb
+++ lib/action_kit/helper.rb
@@ -4,9 +4,8 @@ module ActionKit
 
     def check_petition_name_is_available(name)
       resp = ActionKit::Client.get('petitionpage', params: {_limit: 1, name: name})
-
-      if resp.response_code == 200
-        JSON.parse(resp.response_body)['meta']['total_count'] == 0
+      if resp.code == 200
+        JSON.parse(resp.body)['meta']['total_count'] == 0
       else
         false
       end
