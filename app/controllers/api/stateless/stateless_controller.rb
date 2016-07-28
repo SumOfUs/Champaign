0aee628485d177ad5b1896f22173a8e4b24dc32b
Vincent Martinez <vincent@strict.so>
2016-07-28 18:44:43 +0100

    Rename authenticate_user_ -> authenticate_member_ (semantics)

--- app/controllers/api/stateless/stateless_controller.rb
+++ app/controllers/api/stateless/stateless_controller.rb
@@ -12,13 +12,13 @@ module Api
       protected
 
       def authenticate_request!
-        @current_member = authenticate_user_from_token
+        @current_member = authenticate_member_from_token
         raise Exceptions::UnauthorizedError unless @current_member
       end
 
       private
 
-      def authenticate_user_from_token
+      def authenticate_member_from_token
         _, token = request.headers['authorization'].split
         payload = decode_jwt(token)
         Member.find(payload[:id])
