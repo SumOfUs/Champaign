23963a42b7e7bd7161c3291c16e7c55bb6dd33bf
Vincent Martinez <vincent@strict.so>
2016-07-28 10:52:08 +0100

    Use Api::Stateless for stateless controllers and update specs

---
+++ app/controllers/api/stateless/stateless_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+module Api
+  module Stateless
+    # Api::StatelessController handles all stateless api requests
+    # with token authentication.
+    class StatelessController < ApplicationController
+      include ExceptionHandler
+      include AuthToken
+
+      protect_from_forgery with: :null_session
+
+      protected
+
+      def authenticate_request!
+        @current_member = authenticate_user_from_token
+        raise Exceptions::UnauthorizedError unless @current_member
+      end
+
+      private
+
+      def authenticate_user_from_token
+        _, token = request.headers['authorization'].split
+        payload = decode_jwt(token)
+        Member.find(payload[:id])
+      rescue
+        raise Exceptions::UnauthorizedError
+      end
+    end
+  end
+end
