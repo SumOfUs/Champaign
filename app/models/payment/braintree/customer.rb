8390ea373a0858931a36f8fd438902a90f1684df
tuuli <tuuli.pollanen@gmail.com>
2016-08-09 17:24:57 +0200

    Modify view specs and add a request spec for listing subscriptions through the stateless api

--- app/models/payment/braintree/customer.rb
+++ app/models/payment/braintree/customer.rb
@@ -23,6 +23,8 @@
 class Payment::Braintree::Customer < ActiveRecord::Base
   belongs_to :member
   has_many   :payment_methods, class_name: 'Payment::Braintree::PaymentMethod', foreign_key: 'customer_id'
+  has_many   :subscriptions, class_name: 'Payment::Braintree::Subscription', foreign_key: 'customer_id', primary_key: 'customer_id'
+  has_many   :transactions, class_name: 'Payment::Braintree::Transaction', foreign_key: 'customer_id', primary_key: 'customer_id'
 
   def default_payment_method
     payment_methods.order('created_at desc').first
