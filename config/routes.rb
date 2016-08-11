7d86e1bea7039a44441f8426e831acf0e765bebf
tuuli <tuuli.pollanen@gmail.com>
2016-08-11 16:06:43 +0200

    Add support for listing standalone transactions

--- config/routes.rb
+++ config/routes.rb
@@ -150,6 +150,12 @@ Rails.application.routes.draw do
     end
 
     namespace :stateless, defaults: { format: 'json' } do
+      namespace :braintree do
+        resources :payment_methods
+        resources :subscriptions
+        resources :transactions
+      end
+
       namespace :auth do
         post :password
         post :facebook
