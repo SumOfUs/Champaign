a851336df40d4e4167a01ada828f08b98d99d29e
tuuli <tuuli.pollanen@gmail.com>
2016-08-11 16:06:43 +0200

    Add support for listing standalone transactions

---
+++ app/controllers/api/stateless/payment_helper.rb
@@ -0,0 +1,29 @@
+module PaymentHelper
+  module Braintree
+    extend self
+
+    def customer(member)
+      ::Payment::Braintree::Customer.find_by!(member_id: member.id)
+    end
+
+    def payment_methods_for_member(member)
+      customer(member).payment_methods.order('created_at desc')
+    end
+
+    def payment_method_for_member(member:, id:)
+      customer(member).payment_methods.find(id)
+    end
+
+    def subscriptions_for_member(member)
+      customer(member).subscriptions.order('created_at desc')
+    end
+
+    def subscription_for_member(member:, id:)
+      customer(member).subscriptions.find(id)
+    end
+
+    def transactions_for_member(member)
+      customer(member).transactions.where(subscription_id: nil)
+    end
+  end
+end
