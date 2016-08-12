e281a16dda7bf308574ef7f3813ed44c68c99e18
Neal Donnelly <NealJMD@gmail.com>
2016-08-11 18:04:53 -0600

    rough pass move SumfOfUs styles to external repo

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module Champaign
     config.active_record.observers = :liquid_partial_observer
     config.assets.paths += Settings.custom_assets_paths.split(":") if Settings.custom_assets_paths.present?
 
+    config.assets.paths += Settings.external_asset_paths.try(:split, ":") if Settings.external_asset_paths.present?
     # We're using Redis as our cache. Configure that here.
     # we use 'redis' as the host name because that's configured by docker
     # during our setup as the host where our redis instance is stored.
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(liquid.css liquid.js sumofus.css sumofus.js)
+Rails.application.config.assets.precompile += %w(
+  liquid.css
+  liquid.js
+  member-facing.css
+  sumofus.js
+)
