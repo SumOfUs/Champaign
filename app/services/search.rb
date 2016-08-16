d0e9241f3540abc1150bcbfe4870b3357370c334
Neal Donnelly <NealJMD@gmail.com>
2016-08-16 14:57:53 -0600

    excise remaining references to long-deprecated Widgets

--- app/services/search.rb
+++ app/services/search.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
-# require_relative 'widget_searcher'
-# require_relative 'page_searcher'
-
 module Search
   # matches content with a WHERE x LIKE query
   def self.full_text_search(collection, field, query)
