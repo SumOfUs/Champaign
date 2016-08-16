d8bfd7d1de48744a0180630dd42b2a002947e16d
Neal Donnelly <NealJMD@gmail.com>
2016-08-16 17:22:40 -0600

    delete and consolidate a ton of CSS cruft

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(
-  liquid.css
   liquid.js
   member-facing.css
   sumofus.js
