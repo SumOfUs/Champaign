d539578781cee7984663279574a46de70f9c33dd
tuuli <tuuli.pollanen@gmail.com>
2016-08-17 15:55:49 +0200

    Add update route that doesn't push to the queue yet

--- app/controllers/api/stateless/members_controller.rb
+++ app/controllers/api/stateless/members_controller.rb
@@ -6,6 +6,14 @@ module Api
       def show
         @current_member
       end
+
+      def update
+        permitted_params = params.require(:member).permit(:first_name, :last_name, :email, :country, :city, :postal, :address1, :address2)
+        @current_member.update(permitted_params)
+        unless @current_member.save
+          render json: { success: false, errors: @current_member.errors.messages }, status: 422
+        end
+      end
     end
   end
 end
