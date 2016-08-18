72266b116c16df44fac941b1705f2870a48468eb
Neal Donnelly <NealJMD@gmail.com>
2016-08-18 13:16:07 -0600

    move sumofus specific translations to sou-assets, import them

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,19 @@ module Champaign
     config.active_record.observers = :liquid_partial_observer
     config.assets.paths += Settings.custom_assets_paths.split(":") if Settings.custom_assets_paths.present?
 
-    config.assets.paths += Settings.external_asset_paths.try(:split, ":") if Settings.external_asset_paths.present?
+    if Settings.external_asset_paths.present?
+      path_list = Settings.external_asset_paths.try(:split, ":")
+      config.assets.paths += path_list
+
+      if Settings.external_translation_path.present?
+        path_list.each do |directory|
+          puts "Adding paths to i18n load path:"
+          puts Dir[File.join(directory, Settings.external_translation_path, '*.{rb,yml}')]
+          config.i18n.load_path += Dir[File.join(directory, Settings.external_translation_path, '*.{rb,yml}')]
+        end
+      end
+    end
+
     # We're using Redis as our cache. Configure that here.
     # we use 'redis' as the host name because that's configured by docker
     # during our setup as the host where our redis instance is stored.
