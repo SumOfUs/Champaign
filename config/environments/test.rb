79c5843dcc51aa43893f8e4ae09a80f45a11ed8b
Neal Donnelly <NealJMD@gmail.com>
2016-08-26 13:18:25 -0600

    spec Api::PagesHelper and fix teaspoon failure

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,9 +46,6 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # to allow services consuming images through the API to have absolute URLs
-  config.action_controller.asset_host = 'http://localhost:3000'
-
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
 
