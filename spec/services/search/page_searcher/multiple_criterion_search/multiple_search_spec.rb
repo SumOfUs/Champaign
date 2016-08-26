e90dd99d4541f24ebe752c02ce501c4c16fbd8b1
Vincent Martínez <eyko@users.noreply.github.com>
2016-08-26 12:18:41 +0100

    One click donations fix lint errors (#641)
    * [lint] Style/GuardClause
    
    * [lint] Style/ModuleFunction
    
    * [lint] Metrics/LineLength: Line is too long
    
    * [lint] Style/IndentArray: make array indentation consistent
    
    * [lint] Style/RegexpLiteral: use `%r{...}` instead of `/.../` when regex forward slashes
    
    * [lint] Style/EachWithObject
    
    * Run rubocop in CI

--- spec/services/search/page_searcher/multiple_criterion_search/multiple_search_spec.rb
+++ spec/services/search/page_searcher/multiple_criterion_search/multiple_search_spec.rb
@@ -12,16 +12,16 @@ describe 'Search ::' do
       it 'finds a page that matches the search query by tags, language and text content' do
         expect(content_tag_language_searcher.search)
           .to match_array([
-                            matches_by_content_language_campaign_tags_layout,
-                            matches_by_content_language_tags_layout
-                          ])
+            matches_by_content_language_campaign_tags_layout,
+            matches_by_content_language_tags_layout
+          ])
       end
 
       it 'finds pages that match the search query by content, language, campaign and tags ' do
         expect(content_language_campaign_tags_searcher.search)
           .to match_array([
-                            matches_by_content_language_campaign_tags_layout
-                          ])
+            matches_by_content_language_campaign_tags_layout
+          ])
       end
 
       it 'does not find a match when the search parameters contain a liquid layout there is no match for' do
@@ -31,10 +31,10 @@ describe 'Search ::' do
       it 'finds pages when searching by matching tags and layout' do
         expect(layout_tags_searcher.search)
           .to match_array([
-                            matches_by_content_language_campaign_tags_layout,
-                            matches_by_content_language_tags_layout,
-                            content_tag_plugin_layout_match
-                          ])
+            matches_by_content_language_campaign_tags_layout,
+            matches_by_content_language_tags_layout,
+            content_tag_plugin_layout_match
+          ])
       end
 
       context 'uses OR queries for categories where it makes sense (campaign, language) and not for other categories' do
@@ -45,15 +45,13 @@ describe 'Search ::' do
         end
 
         it 'finds all pages that match the specified array of campaigns' do
-          expect(
-            finds_pages_for_all_campaigns.search
-          ).to match_array([
-                             content_tag_plugin_layout_match,
-                             title_language_campaign_match,
-                             matches_by_content_language_campaign_tags_layout,
-                             matches_by_content_language_campaign,
-                             matches_by_content_language_tags_layout
-                           ])
+          expect(finds_pages_for_all_campaigns.search).to match_array([
+            content_tag_plugin_layout_match,
+            title_language_campaign_match,
+            matches_by_content_language_campaign_tags_layout,
+            matches_by_content_language_campaign,
+            matches_by_content_language_tags_layout
+          ])
         end
 
         it 'finds no pages when a search is done with a combination of tags that exists for no page' do
@@ -62,11 +60,11 @@ describe 'Search ::' do
 
         it 'searches for pages that belong to any campaign of a particular language' do
           expect(campaign_language_searcher.search).to match_array([
-                                                                     title_language_campaign_match,
-                                                                     matches_by_content_language_campaign_tags_layout,
-                                                                     matches_by_content_language_campaign,
-                                                                     matches_by_content_language_tags_layout
-                                                                   ])
+            title_language_campaign_match,
+            matches_by_content_language_campaign_tags_layout,
+            matches_by_content_language_campaign,
+            matches_by_content_language_tags_layout
+          ])
         end
       end
     end
--- spec/support/helper_functions.rb
+++ spec/support/helper_functions.rb
@@ -12,17 +12,17 @@ module HelperFunctions
 
   def create_tags
     Tag.create!([
-                  { tag_name: '*Welcome_Sequence', actionkit_uri: '/rest/v1/tag/1000/' },
-                  { tag_name: '#Animal_Rights', actionkit_uri: '/rest/v1/tag/944/' },
-                  { tag_name: '#Net_Neutrality', actionkit_uri: '/rest/v1/tag/1078/' },
-                  { tag_name: '*FYI_and_VIP', actionkit_uri: '/rest/v1/tag/980/' },
-                  { tag_name: '@Germany', actionkit_uri: '/rest/v1/tag/1036/' },
-                  { tag_name: '@NewZealand', actionkit_uri: '/rest/v1/tag/1140/' },
-                  { tag_name: '@France', actionkit_uri: '/rest/v1/tag/1128/' },
-                  { tag_name: '#Sexism', actionkit_uri: '/rest/v1/tag/1208/' },
-                  { tag_name: '#Disability_Rights', actionkit_uri: '/rest/v1/tag/1040/' },
-                  { tag_name: '@Austria', actionkit_uri: '/rest/v1/tag/1042/' }
-                ])
+      { tag_name: '*Welcome_Sequence', actionkit_uri: '/rest/v1/tag/1000/' },
+      { tag_name: '#Animal_Rights', actionkit_uri: '/rest/v1/tag/944/' },
+      { tag_name: '#Net_Neutrality', actionkit_uri: '/rest/v1/tag/1078/' },
+      { tag_name: '*FYI_and_VIP', actionkit_uri: '/rest/v1/tag/980/' },
+      { tag_name: '@Germany', actionkit_uri: '/rest/v1/tag/1036/' },
+      { tag_name: '@NewZealand', actionkit_uri: '/rest/v1/tag/1140/' },
+      { tag_name: '@France', actionkit_uri: '/rest/v1/tag/1128/' },
+      { tag_name: '#Sexism', actionkit_uri: '/rest/v1/tag/1208/' },
+      { tag_name: '#Disability_Rights', actionkit_uri: '/rest/v1/tag/1040/' },
+      { tag_name: '@Austria', actionkit_uri: '/rest/v1/tag/1042/' }
+    ])
   end
 
   def error_messages_from_response(response)
