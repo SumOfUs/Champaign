065e33ed80b6f1e77ca7e3088a87a4b9f0dd8959
Neal Donnelly <NealJMD@gmail.com>
2016-08-26 16:30:21 -0600

    skip authenticity token for api members controller (lost in rebase)

--- app/controllers/api/members_controller.rb
+++ app/controllers/api/members_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class Api::MembersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     workhorse = CreateMemberForApiMembersController.new(member_params)
     if workhorse.create
