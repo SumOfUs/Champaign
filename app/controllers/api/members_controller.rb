c010042d69e79fe8488119007fe5b8388ce6a4ac
Neal Donnelly <NealJMD@gmail.com>
2016-08-26 15:28:37 -0600

    move logic from MembersController#create to CreateMemberForApiMembersController

--- app/controllers/api/members_controller.rb
+++ app/controllers/api/members_controller.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 class Api::MembersController < ApplicationController
   def create
-    validator = FormValidator.new(member_params, member_validation)
-
-    if validator.valid?
-      member = Member.find_or_initialize_by(email: member_params[:email])
-      member.assign_attributes(member_params)
-      if member.save
-        member.send_to_ak
-        render json: { member: member }
-      else
-        render json: { errors: member.errors }, status: :unprocessable_entity
-      end
+    workhorse = CreateMemberForApiMembersController.new(member_params)
+    if workhorse.create
+      render json: { member: workhorse.member }
     else
-      render json: { errors: validator.errors }, status: :unprocessable_entity
+      render json: { errors: workhorse.errors }, status: :unprocessable_entity
     end
   end
 
@@ -23,12 +15,4 @@ class Api::MembersController < ApplicationController
     params.permit(:name, :email, :country, :postal)
   end
 
-  def member_validation
-    [
-      { name: 'email', data_type: 'email', required: true },
-      { name: 'country', data_type: 'country', required: false },
-      { name: 'postal', data_type: 'postal', required: false },
-      { name: 'name', data_type: 'text', required: true }
-    ]
-  end
 end
