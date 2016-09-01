867d2f66639ec2a0e684dd81844e5ae22ad4e31f
Neal Donnelly <NealJMD@gmail.com>
2016-09-01 15:22:45 +0100

    add basic model and table for Survey plugin

---
+++ db/migrate/20160901130828_create_plugins_survey.rb
@@ -0,0 +1,11 @@
+class CreatePluginsSurvey < ActiveRecord::Migration
+  def change
+    create_table :plugins_surveys do |t|
+      t.integer  "page_id"
+      t.boolean  "active",      default: false
+      t.string   "ref"
+
+      t.timestamps
+    end
+  end
+end
