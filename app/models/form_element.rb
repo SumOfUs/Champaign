bac2cd81ffa38366569c3ac58c8382f9fcb67922
Neal Donnelly <NealJMD@gmail.com>
2016-09-05 20:30:23 -0500

    back-end and super-rough front-end for creating multiple choice survey qs

--- app/models/form_element.rb
+++ app/models/form_element.rb
@@ -26,6 +26,8 @@ class FormElement < ActiveRecord::Base
   validates :name, :label, :data_type, presence: true
   validates_with ActionKitFields
 
+  validate :choices_is_valid
+
   # Array of possible field types.
   VALID_TYPES = %w(
     text
@@ -37,10 +39,53 @@ class FormElement < ActiveRecord::Base
     postal
     hidden
     instruction
+    choice
   ).freeze
   validates :data_type, inclusion: { in: VALID_TYPES }
 
+  def liquid_data
+    return attributes.symbolize_keys unless data_type == 'choice'
+    attributes.symbolize_keys.merge(choices: formatted_choices)
+  end
+
+  def formatted_choices
+    choices.map do |choice|
+      if choice.class == String
+        { label: choice, value: choice, id: choice_id(choice) }
+      elsif choice.class == Hash
+        choice.symbolize_keys.merge(id: choice_id(choice['value']))
+      else
+        choice
+      end
+    end
+  end
+
+  def choices_is_valid
+    return if choices.nil?
+    if choices.class != Array
+      errors.add(:choices, 'must be an array of options')
+      return
+    end
+    choices.each do |choice|
+      if choice.class == Hash
+        if !choice.has_key?('label') || !choice.has_key?('value')
+          errors.add(:choices, 'must have a label and value for each dictionary option')
+          break
+        end
+      elsif choice.class == String
+        next
+      else
+        errors.add(:choices, 'must be an array containing strings or dictionaries')
+        break
+      end
+    end
+  end
+
   private
+
+  def choice_id(choice)
+    "#{name}_#{choice}".gsub(/[^a-z0-9_ ]/i, '').gsub(/[ _]+/, '_').underscore
+  end
 
   def set_position
     last_position = (form.form_elements.maximum(:position) || -1) + 1
