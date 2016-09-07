e5ea7d0c993ee66e0b9bc5e6e7e7d81794a8c922
Neal Donnelly <NealJMD@gmail.com>
2016-09-07 15:50:45 -0600

    fix path list for translations

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,8 @@ module Champaign
     config.active_record.observers = :liquid_partial_observer
 
     if Settings.external_asset_paths.present?
-      config.assets.paths += Settings.external_asset_paths.split(":")
+      path_list = Settings.external_asset_paths.try(:split, ":")
+      config.assets.paths += path_list
 
       if Settings.external_translation_path.present?
         path_list.each do |directory|
