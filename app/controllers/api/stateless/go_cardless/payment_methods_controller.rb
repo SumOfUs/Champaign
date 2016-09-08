abaec3fd068c9151574b8f1984ae95684d29e4dc
tuuli <tuuli.pollanen@gmail.com>
2016-09-08 12:06:21 +0200

    Add logic to cancel GoCardless mandates

--- app/controllers/api/stateless/go_cardless/payment_methods_controller.rb
+++ app/controllers/api/stateless/go_cardless/payment_methods_controller.rb
@@ -8,6 +8,17 @@ module Api
         def index
           @payment_methods = PaymentHelper::GoCardless.payment_methods_for_member(@current_member)
         end
+
+        def destroy
+          begin
+            @payment_method = PaymentHelper::GoCardless.payment_method_for_member(member: @current_member, id: params[:id])
+            PaymentProcessor::GoCardless::Populator.client.mandates.cancel(@payment_method.go_cardless_id)
+            @payment_method.update(cancelled_at: Time.now)
+            render json: {success: true}
+          rescue => e
+            render json: {success: false, errors: e.errors}, status: e.code
+          end
+        end
       end
     end
   end
--- spec/requests/api/stateless/go_cardless/payment_method_spec.rb
+++ spec/requests/api/stateless/go_cardless/payment_method_spec.rb
@@ -6,15 +6,6 @@ describe 'API::Stateless GoCardless PaymentMethods' do
   include AuthToken
   let!(:member) { create(:member, email: 'test@example.com') }
   let!(:customer) { create(:payment_go_cardless_customer, member: member) }
-  let!(:payment_method) do
-    create(:payment_go_cardless_payment_method,
-           customer: customer,
-           id: 1432,
-           go_cardless_id: 9898,
-           scheme: 'bacs',
-           next_possible_charge_date: Date.tomorrow,
-           created_at: Time.now)
-  end
 
   before :each do
     member.create_authentication(password: 'password')
@@ -26,6 +17,15 @@ describe 'API::Stateless GoCardless PaymentMethods' do
   end
 
   describe 'GET index' do
+    let!(:payment_method) do
+      create(:payment_go_cardless_payment_method,
+             customer: customer,
+             id: 1432,
+             go_cardless_id: 9898,
+             scheme: 'bacs',
+             next_possible_charge_date: Date.tomorrow,
+             created_at: Time.now)
+    end
     it 'returns payment methods for member' do
       get '/api/stateless/go_cardless/payment_methods', nil, auth_headers
       expect(response.status).to eq(200)
@@ -35,5 +35,47 @@ describe 'API::Stateless GoCardless PaymentMethods' do
                                                               next_possible_charge_date: /^\d{4}-\d{2}-\d{2}/,
                                                               created_at: /^\d{4}-\d{2}-\d{2}/)
     end
+  end
+
+  describe 'DELETE destroy' do
+    let!(:delete_me) do
+      create(:payment_go_cardless_payment_method,
+             customer: customer,
+             id: 123,
+             go_cardless_id: 'MD0000T982CR19',
+             scheme: 'bacs',
+             next_possible_charge_date: Date.tomorrow,
+             created_at: Time.now)
+    end
+
+    let!(:i_dont_exist) do
+      create(:payment_go_cardless_payment_method,
+             customer: customer,
+             id: 3490,
+             go_cardless_id: 'nosuchthingongocardless',
+             scheme: 'bacs',
+             next_possible_charge_date: Date.tomorrow,
+             created_at: Time.now)
+    end
+
+    it 'cancels the mandate on GoCardless and sets the cancelled_at field in the local record' do
+      Timecop.freeze
+      VCR.use_cassette('stateless api cancel go_cardless mandate') do
+        delete "/api/stateless/go_cardless/payment_methods/#{delete_me.id}", nil, auth_headers
+        expect(response.success?).to eq true
+        expect(Payment::GoCardless::PaymentMethod.find(delete_me.id).cancelled_at)
+          .to be_within(1.second).of Time.now
+      end
+    end
+
+    it 'returns errors and does not update the local record if GoCardless returns an error' do
+      VCR.use_cassette('stateless api cancel go_cardless payment method failure') do
+        delete "/api/stateless/go_cardless/payment_methods/#{i_dont_exist.id}", nil, auth_headers
+        expect(response.success?).to eq false
+        expect(json_hash['errors']).to eq([{'reason' => 'resource_not_found', 'message' => 'Resource not found'}])
+        expect(Payment::GoCardless::PaymentMethod.find(i_dont_exist.id).cancelled_at).to be nil
+      end
+    end
+
   end
 end
