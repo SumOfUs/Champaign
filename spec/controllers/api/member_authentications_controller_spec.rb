9a307896fbdc4f2ac7fe431c74b0d75f9eefbbdb
Omar Sahyoun <osahyoun@gmail.com>
2016-09-12 17:46:35 +0100

    Render and process member registration

--- spec/controllers/api/member_authentications_controller_spec.rb
+++ spec/controllers/api/member_authentications_controller_spec.rb
@@ -8,12 +8,27 @@ describe Api::MemberAuthenticationsController do
     before do
       allow(MemberAuthenticationBuilder).to receive(:build) { auth }
 
-      post :create, email: 'test@example.com', password: 'p', password_confirmation: 'p'
+      post :create, email: 'test@example.com', password: 'p', password_confirmation: 'p', page_id: '1'
     end
 
     it 'builds authentication' do
       expect(MemberAuthenticationBuilder).to have_received(:build)
         .with(password: 'p', password_confirmation: 'p', email: 'test@example.com')
+    end
+
+    context 'successfully creates authentication' do
+      it 'returns with js snippet to redirect' do
+        expect(response.body).to match("window.location = '/pages/1/follow-up'")
+      end
+    end
+
+    context 'unsuccessfully creates authentication' do
+      let(:auth) { double('auth', valid?: false, errors: [{foo: :bar}]) }
+
+      it 'returns errors as json' do
+        expect(response.status).to eq(422)
+        expect(response.body).to eq([{foo: :bar}].to_json)
+      end
     end
   end
 end
