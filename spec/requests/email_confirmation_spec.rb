85c194056c8cdd17e127f1a39eb9af68129aad89
tuuli <tuuli.pollanen@gmail.com>
2016-09-16 14:45:21 +0200

    Add test for confirmed_at

--- spec/requests/email_confirmation_spec.rb
+++ spec/requests/email_confirmation_spec.rb
@@ -2,17 +2,22 @@
 require 'rails_helper'
 
 describe 'Email Confirmation when signing up to express donations' do
-  let!(:member) do
-    create(:member,
-           email: 'test@example.com',
-           authentication: create(:member_authentication,
-                                  password: 'password',
-                                  token: 'imarealtoken1235'))
+  let!(:member) { create(:member, email: 'test@example.com' ) }
+
+  let!(:auth) do
+    create(:member_authentication,
+           member: member,
+           password: 'password',
+           confirmed_at: nil,
+           token: 'imarealtoken1235')
   end
 
   it 'Authenticates the user if the token and email it gets match on a member authentication record' do
     get '/email_confirmation?email=test%40example.com&amp;token=imarealtoken1235'
-    expect(response.body).to include('You have successfully signed up for express donations')
+    Timecop.freeze do
+      expect(response.body).to include('You have successfully signed up for express donations')
+      expect(member.authentication.confirmed_at).to be_within(1.second).of Time.now
+    end
   end
 
   it 'Logs an error and renders errors if the token and email address do not match' do
@@ -24,5 +29,6 @@ describe 'Email Confirmation when signing up to express donations' do
                                'There was an issue signing up for express donations.',
                                'Your confirmation token appears to be invalid.'
                              )
+    expect(member.authentication.confirmed_at).to be nil
   end
 end
