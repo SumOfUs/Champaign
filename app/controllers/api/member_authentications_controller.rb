4c71be022e5de6c6615f726e1c27b09f6fc5723e
Omar Sahyoun <osahyoun@gmail.com>
2016-09-16 15:21:43 +0100

    Respect the rubocop

--- app/controllers/api/member_authentications_controller.rb
+++ app/controllers/api/member_authentications_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class Api::MemberAuthenticationsController < ApplicationController
-
   def new
     @page = Page.find params[:page_id]
     view = File.read("#{Rails.root}/app/liquid/views/layouts/member_registration.liquid")
     template = Liquid::Template.parse(view)
     @rendered = template.render('page_id' => params[:page_id], 'email' => params[:email]).html_safe
 
-    render "pages/show", layout: 'sumofus'
+    render 'pages/show', layout: 'sumofus'
   end
 
   def create
@@ -19,7 +18,7 @@ class Api::MemberAuthenticationsController < ApplicationController
     )
 
     if auth.valid?
-      render js: "window.location = '#{ follow_up_page_path(params[:page_id]) }'"
+      render js: "window.location = '#{follow_up_page_path(params[:page_id])}'"
     else
       render json: auth.errors, status: :unprocessable_entity
     end
