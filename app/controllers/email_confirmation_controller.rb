6fc127840ca1cfcffb2e1799a50f85024a53654d
Omar Sahyoun <osahyoun@gmail.com>
2016-09-23 12:16:57 +0100

    Create a session cookie when member confirms registration

--- app/controllers/email_confirmation_controller.rb
+++ app/controllers/email_confirmation_controller.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 class EmailConfirmationController < ApplicationController
+  include AuthToken
+
   def verify
-    @errors = AuthTokenVerifier.verify(params)
+    verifier = AuthTokenVerifier.new(params[:token]).verify
+
+    ## FIXME seed and fetch from DB
+    #
     view = File.read("#{Rails.root}/app/liquid/views/layouts/email-confirmation-follow-up.liquid")
     template = Liquid::Template.parse(view)
 
-    @rendered = template.render('errors' => @errors, 'success' => @errors.blank?).html_safe
+    if verifier.success?
+      cookies.signed['authentication_id'] = encode_jwt(verifier.authentication.member.token_payload)
+    end
+
+    @rendered = template.render('errors' => verifier.errors).html_safe
     render 'email_confirmation/follow_up', layout: 'generic'
   end
 end
