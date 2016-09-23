2679e507429835924f84639ce8e0cfd59c4efa2d
Omar Sahyoun <osahyoun@gmail.com>
2016-09-23 18:52:43 +0100

    Moves PaymentMethodFetcher into its own file and adds spec covereage

---
+++ spec/services/payment_method_fetcher_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe PaymentMethodFetcher do
+  let(:member)    { create(:member) }
+  let(:customer)  { create(:payment_braintree_customer, member: member) }
+  let!(:method_a) { create(:payment_braintree_payment_method, :stored, token: 'a', customer: customer) }
+  let!(:method_b) { create(:payment_braintree_payment_method, :stored, token: 'b', customer: customer) }
+
+  formatter = lambda do |m|
+    {
+      id: m.id,
+      last_4: m.last_4,
+      instrument_type: m.instrument_type,
+      card_type: m.card_type,
+      email: m.email,
+      token: m.token
+    }
+  end
+
+  context 'without filter' do
+    subject { PaymentMethodFetcher.new(member) }
+
+    it 'fetches all methods without filter' do
+      expected = Payment::Braintree::PaymentMethod.all.map(&formatter)
+
+      expect(subject.fetch).to match_array(expected)
+    end
+  end
+
+  context 'with filter' do
+    subject { PaymentMethodFetcher.new(member, filter: ['a']) }
+
+    it 'fetches all methods without filter' do
+      expected = [method_a].map(&formatter)
+
+      expect(subject.fetch).to match_array(expected)
+    end
+  end
+end
