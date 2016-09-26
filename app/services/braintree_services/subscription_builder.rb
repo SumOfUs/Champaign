a8dbf53d827e4c16ffd808afa5df162e39636ee4
Omar Sahyoun <osahyoun@gmail.com>
2016-09-26 09:43:58 +0100

    Extracts service classes into their own files

---
+++ app/services/braintree_services/subscription_builder.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module BraintreeServices
+  class SubscriptionBuilder
+    attr_reader :subscription, :payment_options
+
+    def initialize(subscription, payment_options)
+      @subscription = subscription
+      @payment_options = payment_options
+    end
+
+    def build
+      Payment::Braintree::Subscription.create!(attributes)
+    end
+
+    def attributes
+      {
+        subscription_id: subscription.id,
+        payment_method: payment_options.payment_method,
+        amount: payment_options.amount,
+        merchant_account_id: payment_options.merchant_account_id,
+        customer: payment_options.customer,
+        currency: payment_options.currency,
+        billing_day_of_month: subscription.billing_day_of_month,
+        page_id: payment_options.page.id
+      }
+    end
+  end
+end
