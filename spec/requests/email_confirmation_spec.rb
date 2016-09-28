4f97cf6a0d89cc66985ccbd580d69275e7ce71ea
Omar Sahyoun <osahyoun@gmail.com>
2016-09-28 15:26:00 +0100

    Style tweaks

--- spec/requests/email_confirmation_spec.rb
+++ spec/requests/email_confirmation_spec.rb
@@ -9,14 +9,14 @@ describe 'Email Confirmation when signing up to express donations' do
   it 'confirms user authentication if the token matches' do
     get '/email_confirmation?token=1234'
 
-    expect(response.body).to include('You have successfully signed up for express donations')
+    expect(response.body).to include('successfully confirmed your account')
     expect(auth.reload.confirmed_at).to_not be_nil
   end
 
   it 'renders error if the token does not match' do
     get '/email_confirmation?token=abcd'
 
-    expect(response.body).to match(/issue signing up for express donations/)
+    expect(response.body).to match(/there was an issue confirming your account/)
     expect(auth.reload.confirmed_at).to be nil
   end
 end
