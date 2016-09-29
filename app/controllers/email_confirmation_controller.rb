bfb0cb4a4ac27f9deae304dda2c07186a20aff63
tuuli <tuuli.pollanen@gmail.com>
2016-09-29 11:32:45 -0700

    Tidy up the email confirmation controller just a little bit

--- app/controllers/email_confirmation_controller.rb
+++ app/controllers/email_confirmation_controller.rb
@@ -3,28 +3,39 @@ class EmailConfirmationController < ApplicationController
   include AuthToken
 
   def verify
-    verifier = AuthTokenVerifier.new(params[:token]).verify
-
-    ## FIXME seed and fetch from DB
-    #
-    view = File.read("#{Rails.root}/app/liquid/views/layouts/email-confirmation-follow-up.liquid")
-    template = Liquid::Template.parse(view)
 
     if verifier.success?
-      minutes_in_a_year = 1.year.abs / 60
-      encoded_jwt = encode_jwt(verifier.authentication.member.token_payload, minutes_in_a_year)
-
-      cookies.signed['authentication_id'] = {
-        value: encoded_jwt,
-        expires: 1.year.from_now
-      }
+      bake_cookies
     end
 
     @rendered = template.render(
       'errors' => verifier.errors,
       'members_dashboard_url' => Settings.members.dashboard_url
     ).html_safe
-
+    ## FIXME seed and fetch from DB
+    #
     render 'email_confirmation/follow_up', layout: 'generic'
+
   end
+
+  private
+
+  def verifier
+    @verifier ||= AuthTokenVerifier.new(params[:token]).verify
+  end
+
+  def bake_cookies
+    minutes_in_a_year = 1.year.abs / 60
+    encoded_jwt = encode_jwt(verifier.authentication.member.token_payload, minutes_in_a_year)
+
+    cookies.signed['authentication_id'] = {
+      value: encoded_jwt,
+      expires: 1.year.from_now
+    }
+  end
+
+  def template
+    @template ||= Liquid::Template.parse(File.read("#{Rails.root}/app/liquid/views/layouts/email-confirmation-follow-up.liquid"))
+  end
+
 end
