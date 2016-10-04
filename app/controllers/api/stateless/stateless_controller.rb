4faadf32cb48e8145318e9fe1aed64d3628f150c
Vincent Martinez <vincent@strict.so>
2016-10-04 18:42:01 +0100

    Read `authorization` from header or cookie

--- app/controllers/api/stateless/stateless_controller.rb
+++ app/controllers/api/stateless/stateless_controller.rb
@@ -11,19 +11,28 @@ module Api
 
       protected
 
+      # Authenticates a user from a token or a cookie.
+      # Tries token first then falls back to
       def authenticate_request!
-        @current_member = authenticate_member_from_token
+        @current_member ||= authenticate_member_from_token || authenticate_member_from_cookie
         raise Exceptions::UnauthorizedError unless @current_member
       end
 
       private
 
       def authenticate_member_from_token
+        return nil if request.headers['authorization'].nil?
         _, token = request.headers['authorization'].split
         payload = decode_jwt(token)
         Member.find(payload[:id])
       rescue
         raise Exceptions::UnauthorizedError
+      end
+
+      def authenticate_member_from_cookie
+        return nil if cookies.encrypted['authorization'].nil?
+        payload = decode_jwt(cookies.encrypted['authorization'])
+        Member.find(payload[:id])
       end
     end
   end
