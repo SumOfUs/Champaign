f1ed9a4325eb23d19b5576010846a4d5fa92a7b9
Omar Sahyoun <osahyoun@gmail.com>
2016-10-10 10:56:25 +0100

    Don't pass express_cookies/express_account for GoCardlesss donations GoCardless isn't currently a part of one-click, and having it return 0 for cookie and account flags to AK will wrongly suggest that the member hasn't activated or signed up to one-click.

--- app/services/action_queue.rb
+++ app/services/action_queue.rb
@@ -193,6 +193,10 @@ module ActionQueue
     def get_payment_account
       "GoCardless #{data[:currency]}"
     end
+
+    def user_data
+      super.except(:user_express_cookie, :user_express_account)
+    end
   end
 
   class DonationAction
