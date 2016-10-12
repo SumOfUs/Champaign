8604b60ed7eabaa4af193644dbb595a66b4e6425
Omar Sahyoun <osahyoun@gmail.com>
2016-10-12 12:36:41 +0100

    PaymentMethod has many Subscriptions

--- app/models/payment/braintree/payment_method.rb
+++ app/models/payment/braintree/payment_method.rb
@@ -11,7 +11,11 @@
 #
 
 class Payment::Braintree::PaymentMethod < ActiveRecord::Base
-  belongs_to :customer,     class_name: 'Payment::Braintree::Customer'
+  belongs_to :customer, class_name: 'Payment::Braintree::Customer'
+
+  has_many   :subscriptions, class_name: 'Payment::Braintree::Subscription',
+                             foreign_key: 'payment_method_id'
+
   has_many   :transactions, class_name: 'Payment::Braintree::Transaction',
                             foreign_key: 'payment_method_id'
 
