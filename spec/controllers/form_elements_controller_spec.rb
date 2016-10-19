02853315b66741b1d20bce71ace811a4665614ff
Neal Donnelly <NealJMD@gmail.com>
2016-10-19 14:55:24 -0700

    fix controller tests and code style

--- spec/controllers/form_elements_controller_spec.rb
+++ spec/controllers/form_elements_controller_spec.rb
@@ -60,22 +60,59 @@ describe FormElementsController do
 
   describe 'DELETE #destroy' do
     before do
+      allow(FormElement).to receive(:includes) { FormElement }
       allow(FormElement).to receive(:find) { element }
       allow(element).to receive(:destroy)
+      allow(element).to receive(:can_destroy?) { should_destroy }
 
       delete :destroy, form_id: '1', id: '2', format: :json
     end
 
-    it 'authenticates session' do
-      expect(request.env['warden']).to have_received(:authenticate!)
+    describe 'successfully' do
+      let(:should_destroy) { true }
+
+      it 'authenticates session' do
+        expect(request.env['warden']).to have_received(:authenticate!)
+      end
+
+      it 'finds form element' do
+        expect(FormElement).to have_received(:find).with('2')
+      end
+
+      it 'destroys element' do
+        expect(element).to have_received(:destroy)
+      end
+
+      it 'checks that the element can be destroyed' do
+        expect(element).to have_received(:can_destroy?)
+      end
+
+      it 'responds with 200' do
+        expect(response.status).to eq 200
+      end
     end
 
-    it 'finds form element' do
-      expect(FormElement).to have_received(:find).with('2')
-    end
+    describe 'unsuccessfully' do
+      let(:element) { instance_double('FormElement', valid?: true, errors: { base: ['cannot be deleted'] }) }
+      let(:should_destroy) { false }
 
-    it 'destroys element' do
-      expect(element).to have_received(:destroy)
+      before :each do
+        allow(element).to receive(:errors) { { base: ['cannot be deleted'] } }
+      end
+
+      let(:should_destroy) { false }
+
+      it 'does not destroy element' do
+        expect(element).to_not have_received(:destroy)
+      end
+
+      it 'responds with 422' do
+        expect(response.status).to eq 422
+      end
+
+      it 'includes errors' do
+        expect(response.body).to eq '{"errors":{"base":["cannot be deleted"]},"name":"form_element"}'
+      end
     end
   end
 end
