1073ca2dfa4f5ec48435b07aed3a51d3ca4796c0
Neal Donnelly <NealJMD@gmail.com>
2016-10-21 20:14:39 -0700

    make pages api show 30 most recent

--- app/services/page_service.rb
+++ app/services/page_service.rb
@@ -5,14 +5,15 @@ module PageService
   def list(language: nil, limit: 30)
     Page.language(language)
       .limit(limit)
-      .order('created_at desc')
+      .order('updated_at desc')
       .published
   end
 
-  def list_featured(language: nil)
+  def list_featured(language: nil, limit: 30)
     Page.language(language)
       .featured
-      .order('created_at desc')
+      .limit(limit)
+      .order('updated_at desc')
       .published
   end
 end
