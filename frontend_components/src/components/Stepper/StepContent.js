6c58eb674752052dd17d9bf1ddf5e8ad6b0f6e41
Vincent Martinez <vincent@strict.so>
2016-11-16 00:04:07 +0000

    Fix StepContent name

--- frontend_components/src/components/Stepper/StepContent.js
+++ frontend_components/src/components/Stepper/StepContent.js
@@ -1,16 +1,15 @@
 import React, { Component, Children } from 'react';
 
 type OwnProps = {
-  title: string;
   visible: boolean;
   children: ReactChildren;
 };
 
-export default class StepWrapper extends Component {
+export default class StepContent extends Component {
   props: OwnProps;
 
   render() {
-    const { currentStep, children, visible } = this.props;
+    const { children, visible } = this.props;
 
     if (!visible) return null;
 
