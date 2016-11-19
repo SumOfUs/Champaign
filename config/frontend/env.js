2ef202b9818c2472deff0a293861797ac4f1e828
Vincent Martinez <vincent@strict.so>
2016-11-19 03:49:38 +0000

    [ReactIntegration] Step 6: Tweak sandbox configuration [see details]
    Webpack has been configured to have two entry points: one is the sandbox
    with live reloading working nice and dandy. The other is called `application`
    and will be used as the entrypoint in our rails app (with the webpack-rails
    javascript / stylesheet include helpers). This second entrypoint / chunk
    might not hot-reload properly (yet).

--- config/frontend/env.js
+++ config/frontend/env.js
@@ -1,10 +1,10 @@
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
 
-var REACT_APP = /^REACT_APP_/i;
+const REACT_APP = /^FRONTEND_/i;
 
 function getClientEnvironment(publicUrl) {
-  var processEnv = Object
+  const processEnv = Object
     .keys(process.env)
     .filter(key => REACT_APP.test(key))
     .reduce((env, key) => {
