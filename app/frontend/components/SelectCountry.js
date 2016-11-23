8026498e61fe2df7341513e1a1c8bbcb72b1436b
Vincent Martinez <vincent@strict.so>
2016-11-23 18:55:31 +0000

    Create `FieldShape` component (for custom forms)
    FieldShape reads a field definition and renders the expected field type.

---
+++ app/frontend/components/SelectCountry.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react';
+import Select from 'react-select';
+import countryData from 'country-data/data/countries.json';
+import sortBy from 'lodash/sortBy';
+
+export type Country = { value: string; label: string; };
+
+type Props = {
+  name: string;
+  value?: string;
+  onChange: (value: any) => void;
+  options?: Country[];
+  placeholder?: string | React$Element<any>;
+  disabled?: boolean;
+  multiple?: boolean;
+};
+
+const countries = sortBy(countryData
+  .filter(c => c.status === 'assigned')
+  .filter(c => c.ioc !== '')
+  .map(c => ({ value: c.alpha2, label: c.name })), 'label');
+
+export default function SelectCountry(props: Props) {
+  return <Select {...props} options={props.options || countries} />;
+}
+
+
