f59271c690b3a95c01d366d0a14c4c0ea5eabb99
Rodrigo Pavano <rodrigopavano@gmail.com>
2016-12-02 18:22:33 -0300

    Load Rails translations from frontend app

--- app/frontend/ComponentWrapper.js
+++ app/frontend/ComponentWrapper.js
@@ -2,21 +2,19 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { IntlProvider } from 'react-intl';
+import loadTranslations from './util/TranslationsLoader';
 
 class ComponentWrapper extends Component {
   props: {
     store: Store;
     children?: React$Element<any>;
-  };
-
-  state = {
-    language: 'en',
+    locale: string;
   };
 
   render() {
     return (
       <Provider store={this.props.store}>
-        <IntlProvider locale={'en'}>
+        <IntlProvider locale={this.props.locale} messages={ loadTranslations(this.props.locale) }>
           <div className="App">
             {this.props.children}
           </div>
