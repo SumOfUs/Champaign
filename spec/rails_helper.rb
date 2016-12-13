df4214a3685519374d5d4030b3f856760aba5c75
Neal Donnelly <NealJMD@gmail.com>
2016-12-13 14:55:36 -0800

    make ruby tests ignore webpack, run on CircleCI, turn off teaspoon

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
