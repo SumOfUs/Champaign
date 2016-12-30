2c9bed572d8b8c6c3b3e4fede068cb949f1ed1fe
Rodrigo Pavano <rodrigopavano@gmail.com>
2016-12-30 20:46:08 -0300

    Styled call tool form
    * Added title to Plugins::CallTool

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229184851) do
+ActiveRecord::Schema.define(version: 20161230231232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -425,6 +425,7 @@ ActiveRecord::Schema.define(version: 20161229184851) do
     t.json     "targets"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "plugins_fundraisers", force: :cascade do |t|
