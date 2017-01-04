5c5f0f38826d51324121ba41bf062233f836d319
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-01-04 19:12:30 -0300

    Implemented CallTool::Target as an object

--- spec/factories/plugins_call_tools.rb
+++ spec/factories/plugins_call_tools.rb
@@ -1,8 +1,29 @@
+# == Schema Information
+#
+# Table name: plugins_call_tools
+#
+#  id         :integer          not null, primary key
+#  page_id    :integer
+#  active     :boolean
+#  ref        :string
+#  form_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#  title      :string
+#  targets    :json             is an Array
+#
+
 FactoryGirl.define do
   factory :call_tool, class: 'Plugins::CallTool' do
     association :page
-    targets do
-      { US: [{id: 1, name: Faker::Name.name, phone: Faker::PhoneNumber.phone_number}] }
-    end
+    targets { 3.times.map { build(:call_tool_target) } }
+  end
+
+  factory :call_tool_target, class: 'CallTool::Target' do
+    skip_create
+    country_name {'United Kingdom'}
+    name { Faker::Name.name }
+    title { Faker::Name.title }
+    phone_number { Faker::PhoneNumber.cell_phone }
   end
 end
