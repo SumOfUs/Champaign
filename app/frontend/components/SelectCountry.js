7236eaa54c410217c5a9e0625168d7d18d1850ae
Omar Sahyoun <osahyoun@gmail.com>
2017-01-18 14:39:56 +0000

    Localise countries

--- app/frontend/components/SelectCountry.js
+++ app/frontend/components/SelectCountry.js
@@ -1,34 +1,48 @@
 // @flow
 import React from 'react';
 import SweetSelect from './SweetSelect/SweetSelect';
-import countryData from 'country-data/data/countries.json';
-import sortBy from 'lodash/sortBy';
+import countriesEn from './../locales/countries/en.json';
+import countriesDe from './../locales/countries/de.json';
+import countriesEs from './../locales/countries/es.json';
+import countriesFr from './../locales/countries/fr.json';
+import {injectIntl, intlShape} from 'react-intl';
 import type { Element } from 'react';
 
 export type Country = { value: string; label: string; };
 
 type Props = {
-  name: string;
+  name?: string;
   value?: string;
-  onChange: (value: any) => void;
+  onChange?: (value: any) => void;
   options?: Country[];
   label?: Element<any> | string;
   disabled?: boolean;
   multiple?: boolean;
+  intl: intlShape;
 };
 
-const countries = sortBy(countryData
-  .filter(c => c.status === 'assigned')
-  .filter(c => c.ioc !== '')
-  .map(c => ({ value: c.alpha2, label: c.name })), 'label');
+const countriesByLocale = {
+  en: countriesEn,
+  es: countriesEs,
+  de: countriesDe,
+  fr: countriesFr,
+};
 
-export default function SelectCountry(props: Props) {
+const SelectCountry = (props: Props) => {
+  let countries = countriesByLocale[props.intl.locale];
+
+  countries = Object.keys(countries).map((c) => {
+    return {value: c, label: countries[c]};
+  });
+
   return (
     <SweetSelect
       {...props}
       options={props.options || countries}
     />
   );
-}
+};
 
+export { SelectCountry };
 
+export default injectIntl(SelectCountry);
