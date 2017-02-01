b3fcdc43733fb1811ce19bb73bfc854fabee7397
Neal Donnelly <NealJMD@gmail.com>
2017-01-31 18:56:45 -0800

    make searchable post-action redirect more intuitive

--- app/assets/javascripts/campaigner-facing/selectize_config.js
+++ app/assets/javascripts/campaigner-facing/selectize_config.js
@@ -3,4 +3,16 @@ $(function(){
     plugins: ['remove_button'],
     closeAfterSelect: true
   });
+
+  $('.selectize-container--clear-on-open').selectize({
+    onDropdownOpen: function() {
+      lastVal = this.getValue();
+      this.clear();
+    },
+    onDropdownClose: function() {
+      if (this.items.length < 1) this.setValue(lastVal);
+    },
+    closeAfterSelect: true
+  });
+
 });
