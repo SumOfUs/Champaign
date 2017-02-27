4a7f4060e485d69f0d8144e80c46d09797315ba3
NealJMD <NealJMD@gmail.com>
2017-02-27 11:32:08 -0800

    allow uploading target phone numbers with extensions (#896)

--- app/services/twiml_generator.rb
+++ app/services/twiml_generator.rb
@@ -1,16 +1,29 @@
 # frozen_string_literal: true
 class TwimlGenerator
-  def self.run(call)
-    Twilio::TwiML::Response.new do |r|
-      if call.sound_clip.present?
-        r.Play ActionController::Base.new.view_context.asset_url call.sound_clip.url
+  class << self
+    def run(call)
+      Twilio::TwiML::Response.new do |r|
+        if call.sound_clip.present?
+          r.Play ActionController::Base.new.view_context.asset_url call.sound_clip.url
+        end
+
+        r.Dial action: action_url(call) do |d|
+          set_number(d, call)
+        end
+      end.text
+    end
+
+    def action_url(call)
+      Rails.application.routes.url_helpers.call_log_url(call)
+    end
+
+    def set_number(d, call)
+      number = call.target_phone_number.split('ext')
+      if number.length > 1
+        d.Number number[0], sendDigits: number[1]
+      else
+        d.Number number[0]
       end
-
-      r.Dial call.target_phone_number, action: action_url(call)
-    end.text
-  end
-
-  def self.action_url(call)
-    Rails.application.routes.url_helpers.call_log_url(call)
+    end
   end
 end
