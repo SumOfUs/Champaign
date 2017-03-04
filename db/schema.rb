4257377d0597d07c984899d63952135673637933
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-03-04 14:00:44 -0300

    Removed target_index field from calls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304151306) do
+ActiveRecord::Schema.define(version: 20170304165947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20170304151306) do
     t.integer  "page_id"
     t.integer  "member_id"
     t.string   "member_phone_number"
-    t.integer  "target_index"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.jsonb    "log",                 default: {}, null: false
