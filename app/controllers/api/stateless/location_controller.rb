7e534b13dc68cae20a0d586714db8c16573f1032
Vincent Martinez <vincent@strict.so>
2017-03-07 16:48:12 +0000

    Add location endpoint and specs
    /api/stateless/location returns the location from Geocoder, and extends it
    with a currency.
    
    When there’s a timeout (or when the location can’t be determined), we return a 504 Gateway Timeout error.

---
+++ app/controllers/api/stateless/location_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Api
+  module Stateless
+    class LocationController < StatelessController
+      def index
+        render json: { location: location_with_currency }
+      rescue Api::Exceptions::LocationNotFound
+        head status: 504
+      end
+
+      private
+
+      def location
+        @location ||= request.location
+        raise Api::Exceptions::LocationNotFound unless @location
+        @location.data
+      end
+
+      def location_with_currency
+        @location_with_currency ||= location.merge(
+          currency: Donations::Utils.currency_from_country_code(location['country_code'])
+        )
+      end
+    end
+  end
+end
