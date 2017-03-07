7af5e7a8229bdc99808fece80463385180464913
Vincent Martinez <vincent@strict.so>
2017-03-07 16:48:12 +0000

    Add location endpoint and specs
    /api/stateless/location returns the location from Geocoder, and extends it
    with a currency.
    
    When there’s a timeout (or when the location can’t be determined), we return a 504 Gateway Timeout error.

--- lib/api/exceptions.rb
+++ lib/api/exceptions.rb
@@ -2,6 +2,7 @@
 module Api
   module Exceptions
     class AuthenticationError < StandardError; end
+    class LocationNotFound < StandardError; end
     class UnauthorizedError < AuthenticationError; end
     class InvalidTokenError < AuthenticationError; end
     class ExpiredTokenError < AuthenticationError; end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'rspec/json_expectations'
 require 'database_cleaner'
 require 'devise'
 require 'support/helper_functions'
