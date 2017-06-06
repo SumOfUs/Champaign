7ac08b8dcefd4dcc18373d37bba8d06afb2c58a0
Vincent Martinez <vincent@strict.so>
2017-06-06 20:35:02 +0100

    [wip] Member facing webpacker bundle in progress
    broken: external js assets

--- app/javascript/packs/fundraiser_plugin.jsx
+++ app/javascript/packs/fundraiser_plugin.jsx
@@ -1,18 +1,16 @@
-// @flow
 import React from 'react';
 import { render } from 'react-dom';
 import ComponentWrapper from '../components/ComponentWrapper';
 import FundraiserView from '../fundraiser/FundraiserView';
 import configureStore from '../state';
-import type { AppState } from '../state/reducers';
 
 const store = configureStore({});
 
-window.document.addEventListener('DOMContentLoaded', initialState => {
+window.mountFundraiser = function mountFundraiser(initialState) {
   render(
     <ComponentWrapper store={store} locale={initialState['locale']}>
       <FundraiserView />
     </ComponentWrapper>,
     document.getElementById('fundraiser-component')
   );
-});
+};
