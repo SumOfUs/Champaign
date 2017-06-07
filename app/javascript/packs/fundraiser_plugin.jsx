ccce672faede3cae454506ebec186d37f3ca3cf1
Vincent Martinez <vincent@strict.so>
2017-06-07 21:22:56 +0100

    Fix sou-assets external_js_path require, fix member-facing bundle

--- app/javascript/packs/fundraiser_plugin.jsx
+++ app/javascript/packs/fundraiser_plugin.jsx
@@ -1,16 +1,19 @@
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import ComponentWrapper from '../components/ComponentWrapper';
 import FundraiserView from '../fundraiser/FundraiserView';
 import configureStore from '../state';
 
-const store = configureStore({});
+const store = window.champaignStore;
 
-window.mountFundraiser = function mountFundraiser(initialState) {
+window.mountFundraiser = function(root, data) {
+  store.dispatch({ type: 'initialize_page', payload: window.champaign.page });
+  store.dispatch({ type: 'parse_champaign_data', payload: data });
   render(
-    <ComponentWrapper store={store} locale={initialState['locale']}>
+    <ComponentWrapper store={store} locale={data.locale}>
       <FundraiserView />
     </ComponentWrapper>,
-    document.getElementById('fundraiser-component')
+    document.getElementById(data.el)
   );
 };
--- app/javascript/util/bootstrap.js
+++ app/javascript/util/bootstrap.js
@@ -1,21 +1,9 @@
-// @flow
-//
-// Bootstrap takes care of initializing a global store,
-// but does not import any components or app modules
-
-import type { Store } from 'redux';
-import type { AppState } from '../state/reducers';
-
+import 'babel-polyfill';
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
 import deLocaleData from 'react-intl/locale-data/de';
 import frLocaleData from 'react-intl/locale-data/fr';
 import esLocaleData from 'react-intl/locale-data/es';
-
-import configureStore from '../state';
-
-const initialState = {};
-export const store: Store<AppState, *> = configureStore(initialState);
 
 addLocaleData([
   ...enLocaleData,
@@ -23,5 +11,3 @@ addLocaleData([
   ...frLocaleData,
   ...esLocaleData,
 ]);
-
-window.champaignStore = store;
