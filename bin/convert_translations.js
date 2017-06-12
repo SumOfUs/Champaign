a1f5011d49794ab1e589485c9a00649d6ed8f03a
Vincent Martinez <vincent@strict.so>
2017-06-12 19:16:02 +0100

    Fix Jest+Enzyme (React) tests

--- bin/convert_translations.js
+++ bin/convert_translations.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
-const YAML = require('yamljs');
-const path = require('path');
-const fs = require('fs');
+const YAML = require("yamljs");
+const path = require("path");
+const fs = require("fs");
 
-const DEST_PATH = path.resolve('app', 'frontend', 'locales');
+const DEST_PATH = path.resolve("app", "javascript", "util", "locales");
 
 try {
   fs.mkdirSync(DEST_PATH);
-} catch (e) { /* do nothing, ignore EEXIST */ }
+} catch (e) {
+  /* do nothing, ignore EEXIST */
+}
 
-['en', 'de', 'fr'].forEach(convert);
+["en", "de", "fr"].forEach(convert);
 
 function convert(language) {
   const destFile = path.resolve(DEST_PATH, `member_facing.${language}.json`);
-  const text = fs.readFileSync(`./config/locales/member_facing.${language}.yml`, 'utf8');
+  const text = fs.readFileSync(
+    `./config/locales/member_facing.${language}.yml`,
+    "utf8"
+  );
   const data = YAML.parse(text);
   fs.writeFileSync(destFile, JSON.stringify(data));
 }
