5df5339e732f9b1c10d835e078bafa5bd16f4c18
Vincent Martinez <vincent@strict.so>
2017-06-14 16:12:57 +0100

    [cleanup] remove debugging console.log

--- app/javascript/campaigner_facing/tooltips.js
+++ app/javascript/campaigner_facing/tooltips.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 
 $.subscribe('pages:new pages:edit form:edit pages:analytics', function() {
-  console.log('pages:edit was triggered');
   $('[data-toggle="tooltip"]').tooltip();
 });
