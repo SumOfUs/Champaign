07a6774109575e3c9de16bf10d86a4518a322ba4
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-06-15 11:40:16 -0300

    Fixed stateless auth and members specs

--- spec/requests/api/stateless/auth_spec.rb
+++ spec/requests/api/stateless/auth_spec.rb
@@ -20,39 +20,39 @@ describe 'API::Stateless Authentication' do
 
     it 'returns a 401 Unauthorized when credentials are wrong' do
       credentials = { email: 'invalid', password: 'invalid' }
-      post('/api/stateless/auth/password', credentials: credentials)
+      post('/api/stateless/auth/password', params: { credentials: credentials })
       expect(response.status).to eq(401)
     end
 
     it 'returns a 200 OK when authentication is successful' do
       credentials = { email: member.email, password: 'password' }
-      post('/api/stateless/auth/password', credentials: credentials)
+      post '/api/stateless/auth/password', params: { credentials: credentials }
       expect(response.status).to eq(200)
     end
 
     it 'returns the member when authentication is successful' do
       credentials = { email: member.email, password: 'password' }
-      post('/api/stateless/auth/password', credentials: credentials)
+      post '/api/stateless/auth/password', params: { credentials: credentials }
       expect(json_ostruct.member.email).to eq(member.email)
     end
 
     it 'returns a valid JWT token when authentication is successful' do
       credentials = { email: member.email, password: 'password' }
-      post('/api/stateless/auth/password', credentials: credentials)
+      post '/api/stateless/auth/password', params: { credentials: credentials }
       expect { decode_jwt(json_ostruct.token) }.to_not raise_error
     end
   end
 
   context 'When a route requires authentication' do
     it 'returns 401 Unauthorized if no valid token was provided' do
-      get('/api/stateless/auth/test_authentication')
+      get '/api/stateless/auth/test_authentication'
       expect(response.status).to eq(401)
     end
 
     it 'returns 401 Unauthorized if the token is expired' do
       token = encode_jwt(member.token_payload, -1)
       headers = { authorization: "Bearer #{token}" }
-      get('/api/stateless/auth/test_authentication', nil, headers)
+      get '/api/stateless/auth/test_authentication', headers: headers
 
       expect(response.status).to eq(401)
     end
@@ -60,7 +60,7 @@ describe 'API::Stateless Authentication' do
     it 'returns 200 OK if a valid token was provided' do
       token = encode_jwt(member.token_payload, 0.5)
       headers = { authorization: "Bearer #{token}" }
-      get('/api/stateless/auth/test_authentication', nil, headers)
+      get '/api/stateless/auth/test_authentication', headers: headers
 
       expect(response.status).to eq(200)
     end
