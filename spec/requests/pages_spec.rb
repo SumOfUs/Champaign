becc5ce61af076c203c7013a4e482808ca4dc869
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-06-15 15:23:41 -0300

    Fixed pages spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -27,7 +27,7 @@ describe 'pages' do
       follow_up_layout = create :liquid_layout, default_follow_up_layout: nil
       liquid_layout = create :liquid_layout, default_follow_up_layout: follow_up_layout
       expect do
-        post pages_path, page: page_params.merge(liquid_layout_id: liquid_layout.id)
+        post pages_path, params: { page: page_params.merge(liquid_layout_id: liquid_layout.id) }
       end.to change { Page.count }.by 1
       page = Page.last
       expect(PageFollower.new_from_page(page).follow_up_path).to eq "/a/#{page.slug}/follow-up"
@@ -36,7 +36,7 @@ describe 'pages' do
     it 'has a blank follow-up url if liquid layout has no default follow-up url' do
       liquid_layout = create :liquid_layout, default_follow_up_layout: nil
       expect do
-        post pages_path, page: page_params.merge(liquid_layout_id: liquid_layout.id)
+        post pages_path, params: { page: page_params.merge(liquid_layout_id: liquid_layout.id) }
       end.to change { Page.count }.by 1
       page = Page.last
       expect(PageFollower.new_from_page(page).follow_up_path).to be_nil
