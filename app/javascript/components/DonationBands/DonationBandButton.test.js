d251fc40e070a1ed297c8fd15ff9a60a3084a5b7
Vincent Martinez <vincent@strict.so>
2017-06-23 19:28:37 +0100

    Update test for DonationBandButton and AmountSelection

---
+++ app/javascript/components/DonationBands/DonationBandButton.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { IntlProvider } from 'react-intl';
+import translations from '../../util/locales/translations-json';
+import DonationBandButton from './DonationBandButton';
+
+describe('Featured Amount', function() {
+  const onClick = jest.fn();
+
+  it('highlights the Button if it is a featured amount ', () => {
+    const wrapper = mount(
+      <IntlProvider locale="en" messages={translations.en}>
+        <DonationBandButton
+          amount={3}
+          featuredAmount={3}
+          currency="GBP"
+          onClick={onClick}
+        />
+      </IntlProvider>
+    );
+    const nodes = wrapper.find(DonationBandButton);
+    expect(nodes.at(0).text()).toEqual('£3');
+  });
+
+  it('shades all amounts that do not match the featured amounts', () => {
+    const wrapper = mount(
+      <IntlProvider locale="en" messages={translations.en}>
+        <DonationBandButton
+          amount={5}
+          featuredAmount={3}
+          currency="USD"
+          onClick={onClick}
+        />
+      </IntlProvider>
+    );
+    const nodes = wrapper.find(DonationBandButton);
+    expect(nodes.at(0).hasClass('DonationBandButton--shade')).toBeTruthy();
+  });
+});
--- app/javascript/components/DonationBands/DonationBands.test.js
+++ app/javascript/components/DonationBands/DonationBands.test.js
@@ -1,9 +1,9 @@
 // @flow
-import React from "react";
-import { mount } from "enzyme";
-import { IntlProvider } from "react-intl";
-import translations from "../../util/locales/translations-json";
-import DonationBands from "./DonationBands";
+import React from 'react';
+import { mount } from 'enzyme';
+import { IntlProvider } from 'react-intl';
+import translations from '../../util/locales/translations-json';
+import DonationBands from './DonationBands';
 
 const amounts = [1, 2, 3, 4, 5];
 
@@ -22,7 +22,7 @@ const component = (
   </IntlProvider>
 );
 
-it("renders all amounts with the currency symbol", () => {
+it('renders all amounts with the currency symbol', () => {
   const wrapper = mount(component);
   expect(wrapper.childAt(0).html()).toMatch(/£1/);
   expect(wrapper.childAt(1).html()).toMatch(/£2/);
@@ -31,72 +31,46 @@ it("renders all amounts with the currency symbol", () => {
   expect(wrapper.childAt(4).html()).toMatch(/£5/);
 });
 
-it("renders a custom input as the last element", () => {
+it('renders a custom input as the last element', () => {
   const wrapper = mount(component);
   expect(wrapper.childAt(5).containsMatchingElement(<input />)).toBeTruthy();
 });
 
-it("calls `selectAmount` when user clicks on an amount", () => {
+it('calls `selectAmount` when user clicks on an amount', () => {
   const wrapper = mount(component);
 
   selectAmount.mockClear();
 
-  wrapper.childAt(0).simulate("click");
+  wrapper.childAt(0).simulate('click');
   expect(selectAmount).toHaveBeenCalledWith(1);
 
   selectAmount.mockClear();
 });
 
-it("calls `selectAmount` when user clicks on an amount", () => {
+it('calls `selectAmount` when user clicks on an amount', () => {
   const wrapper = mount(component);
 
   selectAmount.mockClear();
 
-  wrapper.childAt(0).simulate("click");
+  wrapper.childAt(0).simulate('click');
   expect(selectAmount).toHaveBeenCalledWith(1);
 
   selectAmount.mockClear();
 });
 
-it("clears the input when the user clicks on a donation amount button", () => {
+it('clears the input when the user clicks on a donation amount button', () => {
   const wrapper = mount(component);
   const input = wrapper.childAt(5);
 
-  input.simulate("focus");
-  input.simulate("change", { target: { value: "123" } });
+  input.simulate('focus');
+  input.simulate('change', { target: { value: '123' } });
 
   const inputEl = input.get(0);
   if (!(inputEl instanceof HTMLInputElement)) {
-    throw new Error("Unexpected element type");
+    throw new Error('Unexpected element type');
   }
 
-  expect(inputEl.value).toBe("£123");
-  wrapper.childAt(0).simulate("click");
-  expect(inputEl.value).toBe("");
-});
-
-describe("Featured Amount", function() {
-  const wrapper = mount(
-    <IntlProvider locale="en" messages={translations.en}>
-      <DonationBands
-        amounts={amounts}
-        currency="GBP"
-        proceed={proceed}
-        selectAmount={selectAmount}
-        featuredAmount={3}
-      />
-    </IntlProvider>
-  );
-
-  it("highlights the featured amount if there is a match", () => {
-    const nodes = wrapper.find(".DonationBands-button--highlight");
-    expect(nodes.at(0).text()).toEqual("£3");
-  });
-
-  it("shades all amounts that do not match the featured amounts", () => {
-    const nodes = wrapper.find(".DonationBands-button--shade");
-    expect(nodes.length).toEqual(4);
-    expect(nodes.at(0).hasClass("DonationBands-button--shade")).toBeTruthy();
-    expect(nodes.at(3).hasClass("DonationBands-button--shade")).toBeTruthy();
-  });
+  expect(inputEl.value).toBe('£123');
+  wrapper.childAt(0).simulate('click');
+  expect(inputEl.value).toBe('');
 });
