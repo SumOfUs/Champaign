a8c179d259204f77d34a3ab4d55353d1cfce2999
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-07-04 12:47:43 -0300

    Fixed bug in camelizeKeys

--- app/javascript/util/util.js
+++ app/javascript/util/util.js
@@ -2,7 +2,8 @@ import { camelCase, isPlainObject, forEach, mapKeys, mapValues } from 'lodash';
 
 export function camelizeKeys<T>(obj: T): T {
   if (isPlainObject(obj)) {
-    return mapValues(mapKeys(obj, camelCase), camelizeKeys);
+    let aux = mapKeys(obj, (_, key: string) => camelCase(key));
+    return mapValues(aux, camelizeKeys);
   } else if (Array.isArray(obj)) {
     return obj.map((val: mixed) => camelizeKeys(val));
   }
