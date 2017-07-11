1bcee80389787e4de91d7dc15d5243c025b8b3d0
Neal Donnelly <NealJMD@gmail.com>
2017-07-10 18:52:34 -0700

    skip AK slug check when no AK credentials present

--- app/lib/action_kit/helper.rb
+++ app/lib/action_kit/helper.rb
@@ -2,9 +2,10 @@
 
 module ActionKit
   module Helper
-    extend self
+    module_function :check_petition_name_is_available
 
     def check_petition_name_is_available(name)
+      return true unless ActionKit::Client.configured?
       resp = ActionKit::Client.get('petitionpage', params: { _limit: 1, name: name })
       if resp.code == 200
         (JSON.parse(resp.body)['meta']['total_count']).zero?
