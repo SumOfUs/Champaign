2cb2c5d4d2c17a52bd9ede93b6fe4c53f50298a8
Neal Donnelly <NealJMD@gmail.com>
2017-07-11 18:42:34 -0700

    change where to exists? for readability

--- app/controllers/action_kit_controller.rb
+++ app/controllers/action_kit_controller.rb
@@ -5,7 +5,7 @@ class ActionKitController < ApplicationController
 
   def check_slug
     ak_valid = ActionKit::Helper.check_petition_name_is_available(params[:slug])
-    chmp_valid = Page.where(slug: params[:slug]).empty?
+    chmp_valid = !Page.exists?(slug: params[:slug])
 
     respond_to do |format|
       format.json { render json: { valid: (ak_valid && chmp_valid) } }
