e8245755727e1f7f9390832f63ead1c07685f346
Vincent Martinez <vincent@strict.so>
2017-07-17 17:11:41 +0100

    Require only used plugin packs

--- app/javascript/packs/call_tool.js
+++ app/javascript/packs/call_tool.js
@@ -2,19 +2,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { camelizeKeys } from '../util/util';
-import ComponentWrapper from '../components/ComponentWrapper';
+import Wrapper from '../components/ComponentWrapper';
 import CallToolView from '../call_tool/CallToolView';
+
+function mount(root, props, Component = CallToolView) {
+  render(
+    <Wrapper locale={props.locale} optimizelyHook={window.optimizelyHook}>
+      <Component {...props} />
+    </Wrapper>,
+    document.getElementById(root)
+  );
+}
 
 window.mountCallTool = (root: string, props: any) => {
   props = camelizeKeys(props);
 
-  render(
-    <ComponentWrapper
-      locale={props.locale}
-      optimizelyHook={window.optimizelyHook}
-    >
-      <CallToolView {...props} />
-    </ComponentWrapper>,
-    document.getElementById(root)
-  );
+  mount(root, props);
+
+  if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('../call_tool/CallToolView', () => {
+      console.log('new module:', require('../call_tool/CallToolView').default);
+      mount(root, props, require('../call_tool/CallToolView').default);
+    });
+  }
 };
