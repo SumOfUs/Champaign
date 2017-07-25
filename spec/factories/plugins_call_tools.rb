cb8853cf6bebb8169662b26a5282348fb6a693b2
Vincent Martinez <vincent@strict.so>
2017-07-25 17:30:21 +0100

    Get country name from ISO3166::Country instead of Faker
    Faker::Address.country sometimes returns country names that aren't
    understood by ISO3166::Country.find_country_by_name(), so use
    Faker::Address.country_code and get the country name from
    ISO3166::Country.find_country_by_alpha2()

--- spec/factories/plugins_call_tools.rb
+++ spec/factories/plugins_call_tools.rb
@@ -41,7 +41,9 @@ FactoryGirl.define do
     }
 
     trait :with_country do
-      country_name { Faker::Address.country }
+      country_name {
+        ISO3166::Country.find_country_by_alpha2(Faker::Address.country_code).name
+      }
     end
 
     trait :with_caller_id do
