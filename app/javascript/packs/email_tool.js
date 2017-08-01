caea4dfdb0632a44710e0450f2cd265cf0fe6a4b
Vincent Martinez <vincent@strict.so>
2017-08-01 20:22:00 +0100

    Rename EmailTarget to EmailTool and leave basic functionality

--- app/javascript/packs/email_tool.js
+++ app/javascript/packs/email_tool.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { camelizeKeys } from '../util/util';
 import ComponentWrapper from '../components/ComponentWrapper';
-import EmailTargetView from '../email_target/EmailTargetView';
+import EmailToolView from '../email_tool/EmailToolView';
 
 type Props = {
   locale: string,
@@ -18,12 +18,21 @@ type Props = {
   isSubmitting: boolean,
 };
 
-window.mountEmailTarget = (root: string, props: Props) => {
+function mount(root: string, props: Props) {
   const locale = window.champaign.personalization.member;
   render(
     <ComponentWrapper locale={props.locale}>
-      <EmailTargetView {...camelizeKeys(props)} />
+      <EmailToolView {...camelizeKeys(props)} />
     </ComponentWrapper>,
     document.getElementById(root)
   );
+}
+
+window.mountEmailTool = (root: string, props: Props) => {
+  mount(root, props);
+  if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('../email_tool/EmailToolView', () => {
+      mount(root, props, require('../email_tool/EmailToolView').default);
+    });
+  }
 };
