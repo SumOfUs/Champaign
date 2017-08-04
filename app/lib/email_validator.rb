09d4db759af0a17ad56744104c558e0ae980956b
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-08-04 14:58:29 -0300

    Fixed bug in EmailValidator

--- app/lib/email_validator.rb
+++ app/lib/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.match?(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
+    if value && !value.match?(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
       record.errors[attribute] << (options[:message] || 'is not an email')
     end
   end
