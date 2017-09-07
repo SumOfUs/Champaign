763b0fc9cafafd8b2f88cce9e901ba4fa5e3fa97
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-09-07 16:45:57 -0300

    Added targeting_mode field to EmailTool

--- spec/factories/registered_email_addresses.rb
+++ spec/factories/registered_email_addresses.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: registered_email_addresses
+#
+#  id    :integer          not null, primary key
+#  email :string
+#  name  :string
+#
+
 FactoryGirl.define do
   factory :registered_email_address do
     email { Faker::Internet.email }
