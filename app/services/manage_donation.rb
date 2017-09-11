aca349020b3af5cea2fbe245679168939cf04c6b
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-09-11 13:28:48 -0300

    Simplified ManageDonation to simply call ManageAction

--- app/services/manage_donation.rb
+++ app/services/manage_donation.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
 class ManageDonation
-  include ActionBuilder
-
   def self.create(params:)
-    new(params: params).create
-  end
-
-  def initialize(params:)
-    @params = params
-  end
-
-  def create
-    build_action(donation: true)
+    ManageAction.create(params, extra_params: { donation: true })
   end
 end
