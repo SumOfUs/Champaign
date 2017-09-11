2e7afc85997413e988b0e10a13d5b6f9f0df583e
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-09-11 15:16:29 -0300

    Merged ActionBuilder into ManageAction

--- app/services/manage_action.rb
+++ app/services/manage_action.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
-# Rename to ManageSignature
+
 class ManageAction
-  include ActionBuilder
   attr_reader :params
 
   def self.create(params, extra_params: {}, skip_queue: false, skip_counter: false)
@@ -23,10 +22,81 @@ class ManageAction
     end
   end
 
+  def build_action(extra_attrs = {})
+    @extra_attrs = extra_attrs
+    subscribed_member = !existing_member?
+    action = Action.create({
+      member: member,
+      page: page,
+      form_data: form_data,
+
+      # indicates if action subscribed the member
+      subscribed_member: subscribed_member
+    }.merge(extra_attrs))
+
+    ActionQueue::Pusher.push(:new_action, action) unless @skip_queue
+    Analytics::Page.increment(page.id, new_member: subscribed_member) unless @skip_counter
+
+    action
+  end
+
+  def previous_action
+    return nil unless existing_member?
+    @previous_action ||= Action.not_donation.where(member: member, page_id: page).first
+
+    # looks up other actions the user might have taken on this campaign
+    if page.campaign.present? && @previous_action.blank?
+      page.campaign.pages.each do |connected_page|
+        next if connected_page.id == page.id
+        @previous_action ||= Action.not_donation.where(member: member, page_id: connected_page.id).first
+      end
+    end
+    @previous_action
+  end
+
+  def existing_member
+    @existing_member ||= Member.find_by_email(@params[:email])
+  end
+
+  def existing_member?
+    existing_member ? true : false
+  end
+
+  def member
+    return @user if @user.present?
+    @user = existing_member || Member.new(email: @params[:email])
+
+    @params[:donor_status] = donor_status if donor_status.present?
+    MemberUpdater.run(@user, @params)
+
+    @user
+  end
+
   private
 
+  def donor_status
+    return unless donation?
+    return if @user.recurring_donor?
+    recurring_donation? ? 'recurring_donor' : 'donor'
+  end
+
+  def donation?
+    @extra_attrs && @extra_attrs[:donation]
+  end
+
+  def recurring_donation?
+    @params && @params[:is_subscription]
+  end
+
+  def form_data
+    @params.tap do |params|
+      email = MemberEmailGuesser.run(params)
+      params[:action_referrer_email] = email if email.present?
+    end
+  end
+
   def multiple_actions_allowed?
-    return true if is_donation? || previous_action.nil?
+    return true if donation? || previous_action.nil?
     page.allow_duplicate_actions?
   end
 
--- spec/services/action_builder_spec.rb
+++ spec/services/action_builder_spec.rb
@@ -2,21 +2,12 @@
 
 require 'rails_helper'
 
-describe ActionBuilder do
+describe ManageAction do
   let(:page) { create :page }
   let(:member) { create :member }
   let(:found_action) { Action.where(member: member, page: page).first }
 
-  # Create a class which includes the ActionBuilder.
-  class MockActionBuilder
-    include ActionBuilder
-
-    def initialize(params)
-      @params = params
-    end
-  end
-
-  subject { MockActionBuilder.new(page_id: page.id, email: member.email).build_action }
+  subject { ManageAction.create(page_id: page.id, email: member.email) }
 
   context 'with existing member' do
     it 'increments cache counter with new_member as false' do
@@ -32,13 +23,13 @@ describe ActionBuilder do
     it 'does not create a new member if the email address is not lower case' do
       member # lazy creation
       expect do
-        MockActionBuilder.new(page_id: page.id, email: member.email.upcase).build_action
+        ManageAction.create(page_id: page.id, email: member.email.upcase)
       end.not_to change { Member.count }
     end
   end
 
   context 'with new member' do
-    subject { MockActionBuilder.new(page_id: page.id, email: 'new@example.com').build_action }
+    subject { ManageAction.create(page_id: page.id, email: 'new@example.com') }
 
     it 'increments cache counter with new_member as true' do
       expect(Analytics::Page).to receive(:increment).with(page.id, new_member: true)
@@ -57,12 +48,12 @@ describe ActionBuilder do
   end
 
   it 'correctly finds the expected page' do
-    mab = MockActionBuilder.new(page_id: page.id)
-    expect(mab.page).to eq(page)
+    mab = ManageAction.new(page_id: page.id)
+    expect(mab.send(:page)).to eq(page)
   end
 
   it 'correctly finds created users' do
-    mab = MockActionBuilder.new(email: member.email)
+    mab = ManageAction.new(email: member.email)
     expect(mab.member).to eq(member)
   end
 
@@ -70,19 +61,19 @@ describe ActionBuilder do
     person = member
     not_real = 'Not a real country.'
     expect(person.country).to_not eq(not_real)
-    mab = MockActionBuilder.new(email: person.email, country: not_real)
+    mab = ManageAction.new(email: person.email, country: not_real)
     expect(mab.member.country).to eq(not_real)
   end
 
   it 'correctly builds and returns actions' do
-    mab = MockActionBuilder.new(page_id: page.id, email: member.email)
-    expect(mab.build_action).to eq(found_action)
+    action = ManageAction.create(page_id: page.id, email: member.email)
+    expect(action).to eq(found_action)
     expect(found_action).not_to be_blank
   end
 
   it 'correctly builds and finds previous actions' do
-    mab = MockActionBuilder.new(page_id: page.id, email: member.email)
-    mab.build_action
+    mab = ManageAction.new(page_id: page.id, email: member.email)
+    mab.create
     expect(mab.previous_action).to eq(found_action)
     expect(found_action).not_to be_blank
   end
@@ -91,7 +82,7 @@ describe ActionBuilder do
     let!(:page2) { create :page }
     let!(:page3) { create :page }
     let!(:page4) { create :page }
-    let(:mab) { MockActionBuilder.new(page_id: page.id, email: member.email) }
+    let(:mab) { ManageAction.new(page_id: page.id, email: member.email) }
 
     it 'returns nil if no previous action on any page' do
       expect(mab.previous_action).to eq nil
@@ -135,14 +126,13 @@ describe ActionBuilder do
       blerg: false,
       akid: '1234.514.lQVxcW'
     }
-    mab = MockActionBuilder.new(params)
-    expect { mab.build_action }.to change { Action.count }.by 1
+    mab = ManageAction.new(params)
+    expect { mab.create }.to change { Action.count }.by 1
     expect(Action.last.form_data).to match a_hash_including(params.stringify_keys)
   end
 
   describe 'donor_status' do
     let(:params) { { page_id: page.id, email: member.email } }
-    let(:mab) { MockActionBuilder.new(params) }
 
     describe 'when member is nondonor' do
       it 'it starts as nondonor' do
@@ -150,18 +140,18 @@ describe ActionBuilder do
       end
 
       it 'stays nondonor when action is not donation' do
-        mab.build_action
+        ManageAction.create(params)
         expect(member.reload.donor_status).to eq 'nondonor'
       end
 
       it 'becomes donor when action is non-recurring donation' do
-        mab.build_action(donation: true)
+        ManageAction.create(params, extra_params: { donation: true })
         expect(member.reload.donor_status).to eq 'donor'
       end
 
       it 'becomes recurring_donor when action is recurring donation' do
         params[:is_subscription] = true
-        mab.build_action(donation: true)
+        ManageAction.create(params, extra_params: { donation: true })
         expect(member.reload.donor_status).to eq 'recurring_donor'
       end
     end
@@ -176,18 +166,18 @@ describe ActionBuilder do
       end
 
       it 'stays donor when action is not donation' do
-        mab.build_action
+        ManageAction.create(params)
         expect(member.reload.donor_status).to eq 'donor'
       end
 
       it 'stays donor when action is non-recurring donation' do
-        mab.build_action(donation: true)
+        ManageAction.create(params, extra_params: { donation: true })
         expect(member.reload.donor_status).to eq 'donor'
       end
 
       it 'becomes recurring_donor when action is recurring donation' do
         params[:is_subscription] = true
-        mab.build_action(donation: true)
+        ManageAction.create(params, extra_params: { donation: true })
         expect(member.reload.donor_status).to eq 'recurring_donor'
       end
     end
@@ -202,18 +192,18 @@ describe ActionBuilder do
       end
 
       it 'stays recurring_donor when action is not donation' do
-        mab.build_action
+        ManageAction.create(params)
         expect(member.reload.donor_status).to eq 'recurring_donor'
       end
 
       it 'stays recurring_donor when action is non-recurring donation' do
-        mab.build_action(donation: true)
+        ManageAction.create(params, extra_params: { donation: true })
         expect(member.reload.donor_status).to eq 'recurring_donor'
       end
 
       it 'stays recurring_donor when action is recurring donation' do
         params[:is_subscription] = true
-        mab.build_action(donation: true)
+        ManageAction.create(params, extra_params: { donation: true })
         expect(member.reload.donor_status).to eq 'recurring_donor'
       end
     end
@@ -226,54 +216,54 @@ describe ActionBuilder do
 
     describe 'is not added if rid' do
       it 'is not included' do
-        action = MockActionBuilder.new(base_params).build_action
+        action = ManageAction.create(base_params)
         expect(action.form_data.keys).not_to include('action_referrer_email')
       end
 
       it 'is blank' do
-        action = MockActionBuilder.new(base_params.merge(rid: '')).build_action
+        action = ManageAction.create(base_params.merge(rid: ''))
         expect(action.form_data.keys).not_to include('action_referrer_email')
       end
 
       it "is an id of a member that doesn't exist" do
-        action = MockActionBuilder.new(base_params.merge(rid: 1_234_567_890)).build_action
+        action = ManageAction.create(base_params.merge(rid: 1_234_567_890))
         expect(action.form_data.keys).not_to include('action_referrer_email')
       end
 
       it 'is an id of a member with no email' do
         m2 = create :member, email: ''
-        action = MockActionBuilder.new(base_params.merge(rid: m2.id)).build_action
+        action = ManageAction.create(base_params.merge(rid: m2.id))
         expect(action.form_data.keys).not_to include('action_referrer_email')
       end
     end
 
     it 'is added to form_data if rid is the id of a member' do
       m2 = create :member, email: 'asdf@hjkl.com'
-      action = MockActionBuilder.new(base_params.merge(rid: m2.id)).build_action
+      action = ManageAction.create(base_params.merge(rid: m2.id))
       expect(action.form_data['action_referrer_email']).to eq 'asdf@hjkl.com'
     end
 
     it 'is added to form_data if referring_akid has the ak_user_id of a member' do
       create :member, email: 'qwer@hjkl.com', actionkit_user_id: ak_user_id
-      action = MockActionBuilder.new(base_params.merge(referring_akid: akid)).build_action
+      action = ManageAction.create(base_params.merge(referring_akid: akid))
       expect(action.form_data['action_referrer_email']).to eq 'qwer@hjkl.com'
     end
 
     it 'adds the email of a matching rid if both rid and referring_akid are present' do
       m2 = create :member, email: 'asdf@hjkl.com'
       create :member, email: 'qwer@hjkl.com', actionkit_user_id: ak_user_id
-      action = MockActionBuilder.new(base_params.merge(
+      action = ManageAction.create(base_params.merge(
         rid: m2.id, referring_akid: akid
-      )).build_action
+      ))
       expect(action.form_data['action_referrer_email']).to eq m2.email
     end
 
     it 'adds the email of a matching referrer_id if both rid and referrer_id are present' do
       m2 = create :member, email: 'asdf@hjkl.com'
       m3 = create :member, email: 'qwer@hjkl.com'
-      action = MockActionBuilder.new(base_params.merge(
+      action = ManageAction.create(base_params.merge(
         rid: m2.id, referrer_id: m3.id
-      )).build_action
+      ))
       expect(action.form_data['action_referrer_email']).to eq m3.email
     end
   end
