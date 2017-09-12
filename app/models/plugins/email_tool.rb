cb9486ecd299d004fff1d9a58d513f78d59b1cbc
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-09-12 20:37:49 -0300

    Updated annotations

--- app/models/plugins/email_tool.rb
+++ app/models/plugins/email_tool.rb
@@ -18,7 +18,7 @@
 #  updated_at            :datetime         not null
 #  use_member_email      :boolean          default("false")
 #  from_email_address_id :integer
-#  targeting_mode        :integer
+#  targeting_mode        :integer          default("0")
 #
 
 class Plugins::EmailTool < ApplicationRecord
