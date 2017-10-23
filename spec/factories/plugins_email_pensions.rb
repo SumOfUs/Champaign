9f522f8bf720f5bc35d5159641d150ac0f3dc59d
Rodrigo Pavano <rodrigopavano@gmail.com>
2017-10-23 16:44:56 -0300

    updated annotations

--- spec/factories/plugins_email_pensions.rb
+++ spec/factories/plugins_email_pensions.rb
@@ -4,20 +4,19 @@
 #
 # Table name: plugins_email_pensions
 #
-#  id                 :integer          not null, primary key
-#  ref                :string
-#  page_id            :integer
-#  active             :boolean          default("false")
-#  email_from         :string
-#  email_subjects     :string           default("{}"), is an Array
-#  email_body         :text
-#  created_at         :datetime
-#  updated_at         :datetime
-#  test_email_address :string
-#  email_body_header  :text
-#  email_body_footer  :text
-#  name_from          :string
-#  use_member_email   :boolean          default("false")
+#  id                    :integer          not null, primary key
+#  ref                   :string
+#  page_id               :integer
+#  active                :boolean          default("false")
+#  email_subjects        :string           default("{}"), is an Array
+#  email_body            :text
+#  created_at            :datetime
+#  updated_at            :datetime
+#  test_email_address    :string
+#  email_body_header     :text
+#  email_body_footer     :text
+#  use_member_email      :boolean          default("false")
+#  from_email_address_id :integer
 #
 
 FactoryGirl.define do
