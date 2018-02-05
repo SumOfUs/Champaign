5545bbbe151aeac4842098f444397ac86dc33756
tuuli <tuuli.pollanen@gmail.com>
2018-02-05 16:47:33 +0100

    use circleci hosted postgres image

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -10,7 +10,7 @@ export BRAINTREE_TOKEN_URL=$([ $CIRCLE_BRANCH == "master" ] && echo $PRODUCTION_
 bundle exec rake assets:download_and_precompile[$CUSTOM_ASSETS_URL,$CUSTOM_ASSETS_CREDENTIALS,$CIRCLE_BRANCH,$EXTERNAL_ASSET_PATHS]
 
 # Build docker image
-docker build -t soutech/champaign_web:$CIRCLE_SHA1 --build-arg ci=true ../
+docker build -t soutech/champaign_web:$CIRCLE_SHA1 --build-arg ci=true circleci-champaign/
 
 # Publish the image
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
