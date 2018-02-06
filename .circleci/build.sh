eabe519af704ba5fccc5a4bf3e6dd6c5ac64778d
tuuli <tuuli.pollanen@gmail.com>
2018-02-06 17:21:44 +0100

    caching for assets

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -8,11 +8,8 @@ export NODE_ENV=production
 # TODO: use $CIRCLE_STAGE or $CIRCLE_JOB to check for production deployment
 export BRAINTREE_TOKEN_URL=$([ $CIRCLE_BRANCH == "master" ] && echo $PRODUCTION_BRAINTREE_TOKEN_URL || echo $STAGING_BRAINTREE_TOKEN_URL)
 
-# Precompile assets
-bundle exec rake assets:download_and_precompile[$CUSTOM_ASSETS_URL,$CUSTOM_ASSETS_CREDENTIALS,$CIRCLE_BRANCH,$EXTERNAL_ASSET_PATHS]
-
 # Build docker image
-docker build -t soutech/champaign_web:$CIRCLE_SHA1 circleci-champaign/
+docker build -t soutech/champaign_web:$CIRCLE_SHA1 .
 
 # Publish the image
 docker login -u $DOCKER_USER -p $DOCKER_PASS
