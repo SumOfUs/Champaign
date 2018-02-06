6325e2b204280deb9cd24398e5270341b9640e5e
Vincent Martinez <eykosioux@gmail.com>
2018-02-06 17:57:38 +0000

    Use CDN to load vendored and rails-assets-*
    The endgame is moving all of these to be managed by NPM and versioned.
    At the moment, a few plugins depend on being available as globals, and
    some jQuery plugins are not very UMD friendly.
    
    Long term, we should have most of these plugins in package.json and
    loaded where needed, and only load jQuery (and perhaps lodash) from
    the CDN (but serving a local copy in development mode).

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -12,4 +13,4 @@ end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[globals.js member-facing.css]
+Rails.application.config.assets.precompile += %w[translations.js member-facing.css]
