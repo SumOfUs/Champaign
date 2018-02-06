7955ebe1cf5c0479235772967ceddcb3c0fe7a00
tuuli <tuuli.pollanen@gmail.com>
2018-02-06 11:02:38 +0100

    Fix mega derp

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -eu -o pipefail
 
 export RAILS_ENV=production
 export NODE_ENV=production
 
 # Set the url of the braintree token lambda. Production lambda if branch is master, otherwise staging.
+# TODO: use $CIRCLE_STAGE or $CIRCLE_JOB to check for production deployment
 export BRAINTREE_TOKEN_URL=$([ $CIRCLE_BRANCH == "master" ] && echo $PRODUCTION_BRAINTREE_TOKEN_URL || echo $STAGING_BRAINTREE_TOKEN_URL)
 
 # Precompile assets
@@ -13,5 +15,5 @@ bundle exec rake assets:download_and_precompile[$CUSTOM_ASSETS_URL,$CUSTOM_ASSET
 docker build -t soutech/champaign_web:$CIRCLE_SHA1 --build-arg ci=true circleci-champaign/
 
 # Publish the image
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push soutech/champaign_web
