04e4690f6f65eedc2b8e767fecd88c5bfba29ad5
tuuli <tuuli.pollanen@gmail.com>
2018-02-27 11:15:47 +0100

    Actually, scrap that.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-export RAILS_ENV=production
-export NODE_ENV=production
-
 # Set the url of the braintree token lambda. Production lambda if branch is master, otherwise staging.
-# TODO: use $CIRCLE_STAGE or $CIRCLE_JOB to check for production deployment
 export BRAINTREE_TOKEN_URL=$([ $CIRCLE_BRANCH == "master" ] && echo $PRODUCTION_BRAINTREE_TOKEN_URL || echo $STAGING_BRAINTREE_TOKEN_URL)
 
-# Build docker image
 docker build -t soutech/champaign_web:$CIRCLE_SHA1 .
 
-# Publish the image
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push soutech/champaign_web
