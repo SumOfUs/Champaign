d587da2371c86b69fec30c0f26a42165cc91a205
Omar Sahyoun <osahyoun@gmail.com>
2018-03-28 14:42:22 +0100

    Hack to have to set ecma5 on uglify plugin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,14 @@
 const environment = require('./environment');
 
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+environment.plugins.delete('UglifyJs');
+environment.plugins.set(
+  'UglifyJs',
+  new UglifyJSPlugin({
+    uglifyOptions: {
+      ecma: 5,
+    },
+  })
+);
+
 module.exports = environment.toWebpackConfig();
