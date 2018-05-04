4f70c1274dce5a4e03479cf34715a22bdfd145c9
Vincent Martinez <eykosioux@gmail.com>
2018-05-04 15:56:21 +0100

    Add redux state to sentry extra data

--- app/javascript/packs/sentry.js
+++ app/javascript/packs/sentry.js
@@ -1,4 +1,20 @@
 import Raven from 'raven-js';
+import $ from 'jquery';
+
 Raven.config(process.env.SENTRY_DSN, {
   release: process.env.CIRCLE_SHA1,
+  dataCallback: data => {
+    addReduxState(data);
+    console.log('sentry data:', data);
+    return data;
+  },
 }).install();
+
+function addReduxState(data) {
+  if (window.champaign.store) {
+    data.extra = {
+      ...data.extra,
+      reduxState: window.champaign.store.getState(),
+    };
+  }
+}
