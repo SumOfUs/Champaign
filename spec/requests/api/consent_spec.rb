f8bf24da712a4593a880a38c4be7a57b630c8978
Rodrigo Pavano <rodrigopavano@gmail.com>
2018-05-08 18:02:23 -0300

    Updated consents_controller and spec

--- spec/requests/api/consent_spec.rb
+++ spec/requests/api/consent_spec.rb
@@ -6,20 +6,16 @@ describe 'Api::Consent' do
   describe 'POST /api/stateless/members/:id/consent' do
     let!(:member) { create(:member, email: 'foo@example.com') }
 
-    context 'member exists' do
-      it 'records time consent given' do
-        now = Time.now.utc
-        Timecop.freeze(now) do
-          post "/api/stateless/members/#{member.id}/consent", params: { email: 'foo@example.com' }
-          expect(member.reload.consented_at.to_s).to eq(now.to_s)
-        end
+    context 'given member exists that matches the passed id and email' do
+      it 'updates the member' do
+        post "/api/stateless/members/#{member.id}/consent", params: { email: 'foo@example.com' }
+        expect(member.reload.consented).to be true
       end
     end
 
-    context 'member does not exist' do
+    context "given a member doesn't exist that matches the passed id and email" do
       it 'returns not found' do
         post "/api/stateless/members/#{member.id}/consent", params: { email: 'bar@example.com' }
-
         expect(response.status).to eq(404)
       end
     end
@@ -27,11 +23,11 @@ describe 'Api::Consent' do
 
   describe 'DELETE /api/stateless/consent' do
     context 'member exists' do
-      let!(:member) { create(:member, consented_at: Time.now, email: 'foo@example.com') }
+      let!(:member) { create(:member, consented: true, email: 'foo@example.com') }
 
       it 'records time consent given' do
         delete "/api/stateless/consent?email=#{member.email}"
-        expect(member.reload.consented_at).to be nil
+        expect(member.reload.consented).to be false
       end
     end
 
