25bab30d75c84f1e316973927d4810a617074236
Rodrigo Pavano <rodrigopavano@gmail.com>
2018-05-23 19:22:12 -0300

    Added js component to handle scrolling to ask for consent

--- app/javascript/member-facing/backbone/action_form.js
+++ app/javascript/member-facing/backbone/action_form.js
@@ -28,6 +28,7 @@ const ActionForm = Backbone.View.extend({
 
   events: {
     'click .action-form__clear-form': 'clearForm',
+    'change select[name=country]': 'updateActionUrl',
     'ajax:success': 'handleSuccess',
     'ajax:error': 'handleFailure',
     'ajax:send': 'disableButton',
@@ -51,12 +52,19 @@ const ActionForm = Backbone.View.extend({
   //    bucket: if passed, submitted to the server in the form
   //    location: a hash of location values inferred from the user's request
   //    skipPrefill: boolean, will not prefill if true
+  //    consentNeeded: when true and only for new members, the form will trigger a
+  //      validation if the selected country is in the EU and if consent hasnt been
+  //      given. If consent has been given or if the country is not in the EU
+  //      then a regular action is created.
   initialize(options = {}) {
     this.store = window.champaign.store;
     this.member = options.member;
     this.variant = options.variant || 'simple';
+    this.consentNeeded = options.consentNeeded;
     this.insertHiddenFields(options);
     this.applyDisplayModeToFields(options.member);
+    this.url = this.$el.attr('action');
+    this.updateActionUrl();
     if (!options.skipPrefill) {
       this.prefillAsPossible(options);
     }
@@ -65,6 +73,7 @@ const ActionForm = Backbone.View.extend({
     }
     this.$submitButton = this.$('.action-form__submit-button');
     this.buttonText = this.$submitButton.text();
+    this.existingMember = !!options.member.email;
     GlobalEvents.bindEvents(this);
     if (!this.isMemberPresent()) {
       this.store.dispatch(changeVariant(this.variant));
@@ -92,6 +101,19 @@ const ActionForm = Backbone.View.extend({
   handleEmailChange(event) {
     if (this.store)
       this.store.dispatch(changeMemberEmail(event.target.value) || null);
+  },
+
+  updateActionUrl() {
+    if (!this.consentNeeded) {
+      return;
+    }
+    var consent = window.champaign.store.getState().consent;
+
+    if (!this.existingMember && consent.isEU && consent.consented === null) {
+      this.$el.attr('action', this.url + '/validate');
+    } else {
+      this.$el.attr('action', this.url);
+    }
   },
 
   // Looks at the display-mode for each field and hides them accordingly
@@ -142,6 +164,11 @@ const ActionForm = Backbone.View.extend({
   },
 
   clearForm() {
+    this.existingMember = false;
+    if (this.consentNeeded) {
+      champaign.store.dispatch(resetState());
+      this.updateActionUrl();
+    }
     const $fields_holder = this.$('.form__group--prefilled');
     $fields_holder.removeClass('form__group--prefilled');
     $fields_holder
@@ -241,6 +268,10 @@ const ActionForm = Backbone.View.extend({
       if (options[field] && this.$el) {
         this.insertHiddenInput(field, options[field], this.$el);
       }
+    }
+
+    if (this.consentNeeded) {
+      this.insertHiddenInput('consented', options.member.consented, this.$el);
     }
   },
 
