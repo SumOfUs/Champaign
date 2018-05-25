e71587a4eeb5b7735a8d88943dd9105898ea43d8
Omar Sahyoun <osahyoun@gmail.com>
2018-05-25 17:35:45 +0100

    Removes before_action hook, 'get_pending_action'

--- app/controllers/api/pending_action_notifications_controller.rb
+++ app/controllers/api/pending_action_notifications_controller.rb
@@ -3,32 +3,31 @@
 class Api::PendingActionNotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :check_api_key
-  before_action :get_pending_action
 
   def delivered
-    @pending_action.update delivered_at: Time.now
+    pending_action.update delivered_at: Time.now
     head :ok
   end
 
   def opened
-    @pending_action.update opened_at: Time.now
+    pending_action.update opened_at: Time.now
     head :ok
   end
 
   def bounced
-    @pending_action.update bounced_at: Time.now
+    pending_action.update bounced_at: Time.now
     head :ok
   end
 
   def complaint
-    @pending_action.update complaint: true, bounced_at: Time.now
+    pending_action.update complaint: true, bounced_at: Time.now
     head :ok
   end
 
   def clicked
-    clicked = @pending_action.clicked
+    clicked = pending_action.clicked
     clicked << params[:url]
-    @pending_action.update clicked: clicked
+    pending_action.update clicked: clicked
     head :ok
   end
 
@@ -38,8 +37,8 @@ class Api::PendingActionNotificationsController < ApplicationController
     return head :forbidden unless valid_api_key?
   end
 
-  def get_pending_action
-    @pending_action = PendingAction.find params[:id]
+  def pending_action
+    @pending_action ||= PendingAction.find params[:id]
   end
 
   def valid_api_key?
