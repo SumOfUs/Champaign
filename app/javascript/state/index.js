dc3d6cee278a5cf77d5d0ec4744d55ae2f490ea9
Vincent Martinez <eykosioux@gmail.com>
2018-05-25 21:30:57 +0100

    Tweak configureStore to accept no arguments

--- app/javascript/state/index.js
+++ app/javascript/state/index.js
@@ -4,23 +4,24 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 import passToLogTracker from './pass_to_log_tracker';
 // flow types
-export type { Store } from 'redux';
 export type { AppState } from './reducers';
 export type { Fundraiser } from './fundraiser/types';
 export type { Member } from './member/reducer';
 export type { PaymentMethod } from './paymentMethods/reducer';
 
-export default (data: ChampaignGlobalObject): AppState => {
+export default (data?: ChampaignGlobalObject): AppState => {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const enhancers = composeEnhancers(applyMiddleware(thunk, passToLogTracker));
   const store = createStore(reducers, {}, enhancers);
 
-  store.dispatch({
-    type: '@@chmp:initialize',
-    payload: data,
-    skip_log: true,
-  });
+  if (data) {
+    store.dispatch({
+      type: '@@chmp:initialize',
+      payload: data,
+      skip_log: true,
+    });
+  }
 
   return store;
 };
