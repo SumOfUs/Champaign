4bd5ae0f4adec797d5c895cac2e278675118140b
Omar Sahyoun <osahyoun@gmail.com>
2018-06-11 11:42:00 +0100

    Mark pending actions with consented flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525151104) do
+ActiveRecord::Schema.define(version: 20180611102745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -424,6 +424,7 @@ ActiveRecord::Schema.define(version: 20180525151104) do
     t.datetime "bounced_at"
     t.boolean "complaint"
     t.string "clicked", default: [], array: true
+    t.boolean "consented"
     t.index ["page_id"], name: "index_pending_actions_on_page_id"
   end
 
