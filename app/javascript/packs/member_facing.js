ac73b7b0a964d8f655ee017283dca9b7597f9ecc
Vincent Martinez <eykosioux@gmail.com>
2018-06-18 16:51:13 +0100

    Add locale data in ComponentWrapper
    addLocaleData modifies the global context which seems to be different for each pack.

--- app/javascript/packs/member_facing.js
+++ app/javascript/packs/member_facing.js
@@ -5,7 +5,6 @@ import '../shared/pub_sub';
 import '../shared/show_errors';
 import '../member-facing/registration';
 import '../member-facing/track_shares';
-import '../util/intl-locale-data';
 
 require('lodash');
 require('backbone');
