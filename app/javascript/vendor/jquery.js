138c69489a9d9feb907394b9029bd3911402ca25
Vincent Martinez <eykosioux@gmail.com>
2018-06-22 12:43:19 +0100

    Move jquery plugins to vendor/jquery

--- app/javascript/vendor/jquery.js
+++ app/javascript/vendor/jquery.js
@@ -1,6 +1,12 @@
 // @flow
 import $ from 'jquery';
 
-if (!window.$) window.$ = $;
+if (!window.$) window.$ = window.jQuery = $;
+
+// jQuery plugins
+require('jquery-ui');
+require('jquery-ujs');
+require('selectize');
+require('jquery-sticky');
 
 export default $;
