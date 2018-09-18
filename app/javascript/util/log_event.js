3d951b4824e5373f4d9309546757e68ea183da81
tuuli <tuuli.pollanen@gmail.com>
2018-09-18 16:21:51 +0200

    Add mapped data to ecommerce events

--- app/javascript/util/log_event.js
+++ app/javascript/util/log_event.js
@@ -1,4 +1,6 @@
 // @flow
+const uuid = require('uuid/v1');
+
 export const logEvent = (eventName: string, ...payload: any) => {
   if (typeof window.mixpanel === 'undefined') return;
   if (typeof window.champaign === 'undefined') return;
@@ -22,26 +24,35 @@ const getEventData = (eventName: string, ...data: any) => {
     case 'action:submitted_success':
       return ['action', 'submitted_success'];
     case '@@chmp:consent:change_country':
-      return ['gdpr', 'change_country', data.countryCode];
+      return ['gdpr', 'change_country', data[0].countryCode];
     case '@@chmp:consent:change_consent':
-      return ['gdpr', 'change_consent', data.consented ? 'true' : 'false'];
+      return ['gdpr', 'change_consent', data[0].consented ? 'true' : 'false'];
     case 'change_amount':
-      return ['fundraising', 'change_amount', null, parseFloat(data.payload)];
+      return [
+        'fundraising',
+        'change_amount',
+        null,
+        parseFloat(data[0].payload),
+      ];
     case 'set_store_in_vault':
       return [
         'fundraising',
         'set_store_in_vault',
-        data.payload ? 'true' : 'false',
+        data[0].payload ? 'true' : 'false',
       ];
     case 'set_recurring':
-      return ['fundraising', 'set_recurring', data.payload ? 'true' : 'false'];
+      return [
+        'fundraising',
+        'set_recurring',
+        data[0].payload ? 'true' : 'false',
+      ];
     case 'fundraiser:transaction_submitted':
-      logEcommerce(data);
+      if (window.ga) logEcommerce(data);
       return [
         'fundraising',
         'transaction_submitted',
         null,
-        parseFloat(data.amount),
+        parseFloat(data[0].value),
       ];
     case 'change_step':
       return ['fundraising', 'change_step', data.payload];
@@ -64,49 +75,20 @@ const logToGa = (eventName: string, data: any) => {
   }
 };
 
-const logEcommerce = (...data) => {
-  console.log('logEcommerce', JSON.stringify(data));
-
-  //[
-  // {
-  // "value":10,
-  // "currency":"EUR",
-  // "content_category":"card",
-  // "recurring":true
-  // },
-
-  // {
-  // "storeInVault":false,
-  // "member":
-  //   {
-  //   "name":"tuuli",
-  //   "action_phone_number":"",
-  //   "email":"tuuli@sumofus.org",
-  //   "country":"SI",
-  //   "postal":"1000"
-  //   }
-  // }
-  // ]
-  //
-  ga('ecommerce:addTransaction', {
-    id: 1234,
-    affiliation: data.first.content_category,
+const logEcommerce = data => {
+  const UUID = uuid();
+  window.ga('ecommerce:addTransaction', {
+    id: UUID,
+    revenue: data[0].value,
+    currency: data[0].currency,
   });
 
-  //ga('ecommerce:addItem', {
-  //  'id': '1234',                     // Transaction ID. Required.
-  //  'name': 'Fluffy Pink Bunnies',    // Product name. Required.
-  //  'sku': 'DD23444',                 // SKU/code.
-  //  'category': 'Party Toys',         // Category or variation.
-  //  'price': '11.99',                 // Unit price.
-  //  'quantity': '1'                   // Quantity.
-  //});
-  //
-  //ga('ecommerce:addTransaction', {
-  //  'id': '1234',                     // Transaction ID. Required.
-  //  'affiliation': 'Acme Clothing',   // Affiliation or store name.
-  //  'revenue': '11.99',               // Grand Total.
-  //  'shipping': '5',                  // Shipping.
-  //  'tax': '1.29'                     // Tax.
-  //});
+  window.ga('ecommerce:addItem', {
+    id: UUID,
+    name: data[0].recurring == true ? 'recurring' : 'one-time',
+    category: data[0].content_category,
+    price: data[0].value,
+    quantity: 1,
+    currency: data[0].currency,
+  });
 };
