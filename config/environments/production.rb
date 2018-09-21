05b98a54be41ba8ab762a04f71c83449babb10ba
tuuli <tuuli.pollanen@gmail.com>
2018-09-21 12:26:04 +0200

    Enable api access to multi level sub domains

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
   # In production, we only accept CORS request from sumofus.org or its subdomains.
   config.middleware.insert_before 0, Rack::Cors, logger: (-> { Rails.logger }) do
     allow do
-      origins(%r{^(https?:\/\/)?([a-z0-9-]+\.)?sumofus\.org$}i)
+      origins(%r{^(https?:\/\/)?([a-z0-9-]+\.)*sumofus\.org$}i)
       resource '*',
                headers: :any,
                methods: %i[get post delete put patch options head],
