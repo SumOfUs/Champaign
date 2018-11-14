6c25074eda967c269af589e85a35b173da0153b1
tuuli <tuuli.pollanen@gmail.com>
2018-11-14 15:13:34 +0100

    make funding counter class initialize without amount

--- app/lib/funding_counter.rb
+++ app/lib/funding_counter.rb
@@ -7,29 +7,22 @@ class FundingCounter
     @amount = amount
   end
 
-  def self.update(page, amount, currency)
-    new(page, amount, currency).update
+  def self.update(page:, currency:, amount:)
+    new(page, currency, amount).update
   end
 
-  def self.convert(page, currency)
+  def self.convert(page:, currency:)
     new(page, currency).convert
   end
 
   def update
     return if @page.blank?
-    converted_amount = Money.new(in_cents(@amount), @currency).exchange_to('USD')
-    total_amount = @page.total_donations + converted_amount
-    @page.update_attributes(total_donations: total_amount)
+    original_amount = Money.from_amount(@page.total_donations, 'USD')
+    converted_amount = Money.from_amount(@amount, @currency).exchange_to('USD')
+    @page.update_attributes(total_donations: (original_amount + converted_amount).to_d)
   end
 
   def convert
-    Money.new(in_cents(@page.total_donations), 'USD').exchange_to(@currency)
-  end
-
-  private
-
-  def in_cents(amount)
-    # converts to rational to avoid round-off issues
-    (100 * amount.to_r).to_i
+    Money.from_amount(@page.total_donations, 'USD').exchange_to(@currency)
   end
 end
--- app/models/payment/go_cardless/transaction.rb
+++ app/models/payment/go_cardless/transaction.rb
@@ -94,6 +94,6 @@ class Payment::GoCardless::Transaction < ApplicationRecord
   end
 
   def increment_funding_counter
-    FundingCounter.update(page, amount, currency)
+    FundingCounter.update(page: page, currency: currency, amount: amount)
   end
 end
