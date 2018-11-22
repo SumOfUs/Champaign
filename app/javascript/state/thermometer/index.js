f7780631a1dd9d8e0eeee265e7e160f1146242ba
Vince Martinez Fasoro <eykosioux@gmail.com>
2018-11-22 17:34:14 +0000

    Bind Thermometer to redux state

--- app/javascript/state/thermometer/index.js
+++ app/javascript/state/thermometer/index.js
@@ -1,38 +1,61 @@
 // @flow
+import type { ChampaignGlobalObject } from '../../types';
+import { isEmpty, mapValues } from 'lodash';
 
 export type Action =
-  | { type: '@@chmp:thermometer:update', attrs: $Shape<State> }
+  | { type: '@@chmp:initialize', payload: ChampaignGlobalObject }
+  | { type: '@@chmp:thermometer:update', attrs: State }
   | { type: '@@chmp:thermometer:increment', temperature: number };
 
-export type State = {
-  donations: number,
-  goal: number,
-  currencyCode: string,
-  // showMarkers indicates that we want the percentage markers
-  // for 0, 25, 50, 75, 100 percentiles underneath the bar
-  showMarkers?: boolean,
-  // showPercentage indicates if the percentage should be shown
-  // in the thermometer bar.
-  showPercentage?: boolean,
-};
+export type State =
+  | {}
+  | {
+      active: boolean,
+      goals: { [currency: string]: number },
+      offset: number,
+      remainingAmounts: { [currency: string]: number },
+      title: ?string,
+      totalDonations?: { [currency: string]: number },
+    };
 
 const defaults: State = {
-  donations: 0,
-  goal: 100,
-  currencyCode: 'USD',
+  active: false,
+  goals: {},
+  offset: 0,
+  remainingAmounts: {},
+  title: '',
+  totalDonations: {},
 };
 
 export default function reducer(
   state?: State = defaults,
   action: Action
 ): State {
-  return state;
+  switch (action.type) {
+    case '@@chmp:initialize':
+      return getStateFromChampaign(action.payload);
+    default:
+      return state;
+  }
 }
 
-export function update(attrs: $Shape<State>): Action {
+export function update(attrs: State): Action {
   return { type: '@@chmp:thermometer:update', attrs };
 }
 
 export function increment(temperature: number): Action {
   return { type: '@@chmp:thermometer:increment', temperature };
+}
+
+function getStateFromChampaign(chmp: ChampaignGlobalObject): State {
+  const data = chmp.personalization.donationsThermometer;
+  if (isEmpty(data)) return { active: false };
+  return {
+    active: data.active,
+    offset: data.offset,
+    title: data.title,
+    goals: data.goals,
+    remainingAmounts: data.remaining_amounts,
+    totalDonations: data.total_donations,
+  };
 }
