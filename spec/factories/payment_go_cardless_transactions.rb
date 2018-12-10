a027d630f1d2085002273a1e4646dc6560af887a
Vince Martinez Fasoro <eykosioux@gmail.com>
2018-12-10 14:53:22 +0000

    Use VCR.use_cassette around transaction service specs

--- spec/factories/payment_go_cardless_transactions.rb
+++ spec/factories/payment_go_cardless_transactions.rb
@@ -26,7 +26,7 @@ FactoryBot.define do
   factory :payment_go_cardless_transaction, class: 'Payment::GoCardless::Transaction' do
     go_cardless_id { "PM#{Faker::Number.number(6)}" }
     amount { 23.19 }
-    currency { 'EUR' }
+    currency { 'USD' }
     status { :submitted }
     association :page, factory: :page
 
--- spec/services/transaction_service_spec.rb
+++ spec/services/transaction_service_spec.rb
@@ -3,21 +3,22 @@
 require 'rails_helper'
 
 describe TransactionService do
+  around(:each) do |example|
+    VCR.use_cassette('money_from_oxr') do
+      example.run
+    end
+  end
+
   before do
-    # Braintree one off transactions
     %w[USD CAD AUD NZD GBP EUR CHF].each do |currency|
       create_list :payment_braintree_transaction, 2, amount: 100, currency: currency, created_at: Date.today
     end
 
-    # Braintree one off transactions from 2 months ago
     %w[USD CAD AUD NZD GBP EUR CHF].each do |currency|
       create_list :payment_braintree_transaction, 2, amount: 100, currency: currency, created_at: 2.months.ago
     end
 
-    # GoCadless one off transactions
     create_list :payment_go_cardless_transaction, 2, amount: 100, currency: 'EUR', created_at: Date.today
-
-    # GoCadless one off transactions from 2 months ago
     create_list :payment_go_cardless_transaction, 2, amount: 100, currency: 'EUR', created_at: 2.months.ago
   end
 
@@ -52,7 +53,7 @@ describe TransactionService do
 
     it 'excludes subscriptions' do
       %w[AUD GBP EUR].each do |currency|
-        FactoryBot.create :payment_go_cardless_transaction, :with_subscription, amount: 100, currency: currency
+        create :payment_go_cardless_transaction, :with_subscription, amount: 100, currency: currency
       end
       totals = TransactionService.count_go_cardless
       expect(totals).to include('EUR' => 400.to_d)
@@ -66,16 +67,14 @@ describe TransactionService do
 
   describe '.count' do
     it 'aggregates GoCardless and Braintree transactions' do
-      totals = TransactionService.count
       expected = { 'EUR' => 800.to_d, 'AUD' => 400.to_d }
-      expect(totals).to include expected
+      expect(TransactionService.count).to include expected
     end
   end
 
   describe '.count_in_usd' do
     it 'converts .count totals to USD and returns a total' do
-      totals = TransactionService.count_in_usd
-      expect(totals).to be_within(5).of(30.00)
+      expect(TransactionService.count_in_usd).to be_within(5).of(30.00)
     end
   end
 end
