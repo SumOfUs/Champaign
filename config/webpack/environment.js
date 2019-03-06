6b8f4678842fb5a24b2aa86ab82229f97b48ef1e
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-03-06 19:48:29 +0000

    Simplify dotenv for webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,13 @@
 const { environment } = require('@rails/webpacker');
+const _ = require('lodash');
 const dotenv = require('dotenv');
 const webpack = require('webpack');
 const custom = require('./custom.js');
 
-const dotenvFiles = [
-  `.env.${process.env.NODE_ENV}.local`,
-  `.env.${process.env.NODE_ENV}`,
-  '.env.local',
-  '.env',
-];
-
-dotenvFiles.forEach(dotenvFile => {
-  dotenv.config({ path: dotenvFile, silent: true });
-});
+Object.assign(process.env, dotenv.config().parsed);
 
 environment.config.merge(custom);
+
 environment.plugins.append(
   'Environment',
   new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env)))
