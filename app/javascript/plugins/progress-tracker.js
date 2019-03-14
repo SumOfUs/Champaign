79cd9a2b973cd23ac5347e471462af0b6957d489
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-03-14 12:05:07 +0000

    Hide main header before showing overlay progress tracker

--- app/javascript/plugins/progress-tracker.js
+++ app/javascript/plugins/progress-tracker.js
@@ -8,11 +8,20 @@ export default class ProgressTracker {
   }
 
   addEventListeners() {
+    const $header = $('.header-logo');
     ee.on('action:submitted_success', this.show);
     ee.on('action:submitted_success', () => this.tick('signed'));
     ee.on('fundraiser:transaction_success', () => this.tick('donated'));
-    window.addEventListener('share', () => this.tick('shared'), false);
+    window.addEventListener(
+      'share',
+      () => {
+        $header.hide();
+        this.tick('shared');
+      },
+      false
+    );
     $('.two-step__decline').on('click', () => {
+      $header.hide();
       this.cross('shared');
     });
   }
