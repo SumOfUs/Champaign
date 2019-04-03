c580b33531ec8c37b21d09512e6cdae1c7c451a6
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-04-03 20:06:07 +0100

    Set money gem's default locale backend to :i18n

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,6 +1,9 @@
 require 'money'
 require 'money_oxr/bank'
 
+# Currency formatting depends on the locale, not the currency
+Money.locale_backend = :i18n
+
 Money.default_bank = MoneyOXR::Bank.new(
   app_id: Settings.oxr_app_id,
   cache_path: 'tmp/oxr.json',
