3306d5144e67794ce7450e4accba308b5fecf5ce
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-06-18 12:23:50 +0100

    [wip] Petition (component) automatically mounting

--- app/javascript/plugins/petition/index.js
+++ app/javascript/plugins/petition/index.js
@@ -1,5 +1,43 @@
 // @flow
+import React from 'react';
+import { render } from 'react-dom';
+import { Plugin } from '../index';
+import { PetitionComponent } from './PetitionComponent';
 
-export default () => {
-  console.log('set up petition plugin');
+type PetitionOptions = {
+  el: HTMLElement,
+  config: any, // todo
+};
+
+/*
+ */
+export class Petition extends Plugin {
+  _additionalData: any;
+  constructor(options: PetitionOptions) {
+    super(options);
+    this.render();
+  }
+
+  updateForm(fieldName: string, value: string) {
+    console.info('Petition#updateForm not implemented');
+    this.render();
+  }
+
+  submit() {
+    console.info('Petition#submit not implemented');
+  }
+
+  render() {
+    const el = this.el;
+    if (el) {
+      render(<PetitionComponent date={Date.now()} />, el);
+    }
+  }
+}
+export const init = (options: any) => {
+  return new Petition({
+    el: options.el,
+    namespace: 'petition',
+    config: options.config,
+  });
 };
