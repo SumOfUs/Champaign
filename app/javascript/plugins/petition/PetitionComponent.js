f518e36b86acaf5cd56d576c897d1b0e4076fa02
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-06-19 17:35:12 +0100

    [wip] add redux store to new petition plugin

--- app/javascript/plugins/petition/PetitionComponent.js
+++ app/javascript/plugins/petition/PetitionComponent.js
@@ -1,13 +1,26 @@
 // @flow
 import React from 'react';
 import Button from '../../components/Button/Button';
+import WelcomeMember from '../../components/WelcomeMember/WelcomeMember';
+import type { Member } from '../../state';
 
-export function PetitionComponent(props: any) {
-  console.log('rendering PetitionComponent', props.date);
+type Props = {
+  member: Member,
+  onResetMember: () => void,
+  onSubmit: () => void,
+  config: any,
+};
+export function PetitionComponent(props: Props) {
+  const { member, config } = props;
   return (
     <div className="PetitionComponent">
-      <h3>Petition Form</h3>
-      <p>{props.date}</p>
+      {props.member && (
+        <WelcomeMember
+          member={props.member}
+          resetMember={props.onResetMember}
+        />
+      )}
+      {!props.member && <p>Form goes here</p>}
       <Button>Sign Petition</Button>
     </div>
   );
