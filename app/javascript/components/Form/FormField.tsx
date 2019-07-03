6c6a467b3dee3a6556bb8ab7585118ebd57e489a
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-07-03 10:47:01 +0100

    Fix TypeScript errors in FormField specs

--- app/javascript/components/Form/FormField.tsx
+++ app/javascript/components/Form/FormField.tsx
@@ -4,27 +4,27 @@ import FormGroup from './FormGroup';
 import FieldTypes from './FieldTypes';
 
 export type Choice = {
-  label: string,
-  value: string,
-  id: string,
+  label: string;
+  value: string;
+  id: string;
 };
 
 export type Field = {
-  id: string, // it comes from Champaign but we omit it when passing it down
-  choices: Choice[],
-  data_type: string,
-  default_value: string | void,
-  display_mode: 'all_members' | 'recognized_members_only' | 'new_members_only',
-  form_id: number,
-  label: string,
-  name: string,
-  position: number,
-  required: boolean,
-  visible: boolean | void,
+  id: string; // it comes from Champaign but we omit it when passing it down
+  choices: Choice[];
+  data_type: string;
+  default_value: string | void;
+  display_mode: string;
+  form_id: number;
+  label: string;
+  name: string;
+  position: number;
+  required: boolean;
+  visible: boolean | void;
 };
 
 type Props = Field & {
-  onChange?: (value: string | number | string[]) => void,
+  onChange?: (value: string | number | string[]) => void;
 };
 export default function FormField(props: Props) {
   const FieldType = FieldTypes[props.data_type];
