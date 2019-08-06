42669c72624ceb615b76963cc2b0d54b7343deee
Vince Martinez Fasoro <eykosioux@gmail.com>
2019-08-06 17:01:11 +0100

    Sentry update + changes to support IE11
    - Move (external) sou-assets/javascripts into app/javascript/legacy
    - Update .browserslistrc
    - Tell tsconfig to compile to es5 (ts-loader does not pass through babel?)
    - Remove browser-polyfills (polyfill.io) as we already provide polyfills
    - Downgrade jQuery from 3.x to 2.x (3.x throwing an error in IE 11)
    - Clean up member_facing.js, cleaner imports

--- app/javascript/packs/member_facing.js
+++ app/javascript/packs/member_facing.js
@@ -1,44 +1,26 @@
-require('whatwg-fetch');
-require('../legacy/member-facing/registration');
-require('../legacy/member-facing/track_shares');
-require('../util/event_tracking');
-
-import { mapValues, pick } from 'lodash';
-import flatten from 'flat';
-import URI from 'urijs';
-import configureStore from '../state';
-import redirectors from '../legacy/member-facing/redirectors';
 import { FeaturesHelper } from '../state/features';
-import DonationsThermometer from '../plugins/donations_thermometer';
-import ProgressTracker from '../plugins/progress-tracker';
-import modules from '../modules/';
-import cookieConsent from '../modules/cookie-consent';
 import * as scrollToElement from '../modules/transition';
-import * as legacy from '../legacy/member-facing/';
+import configureStore from '../state';
+import cookieConsent from '../modules/cookie-consent';
+import DonationsThermometer from '../plugins/donations_thermometer';
+import modules from '../modules/';
+import ProgressTracker from '../plugins/progress-tracker';
+import redirectors from '../legacy/member-facing/redirectors';
+import URI from 'urijs';
 
-window.URI = URI;
+require('whatwg-fetch'); // TODO use fetch-ponyfill
+require('../legacy/member-facing/');
+require('../util/event_tracking');
 
 const store = configureStore(window.champaign);
 
-if (process.env.EXTERNAL_ASSETS_JS_PATH) {
-  require(process.env.EXTERNAL_ASSETS_JS_PATH);
-}
-
-window.champaign = window.champaign || {};
-
-// Legacy components (Backbone)
-legacy.setup(window.champaign);
-
-// Add modules to champaign.modules
+window.URI = URI;
+window.champaign.store = store;
 window.champaign.modules = modules;
-
-Object.assign(window.champaign, {
-  DonationsThermometer,
-  ProgressTracker,
-  redirectors,
-  store,
-  features: new FeaturesHelper(store),
-});
+window.champaign.DonationsThermometer = DonationsThermometer;
+window.champaign.features = new FeaturesHelper(store);
+window.champaign.ProgressTracker = ProgressTracker;
+window.champaign.redirectors = redirectors;
 
 document.addEventListener('DOMContentLoaded', function() {
   cookieConsent.init(window.champaign.personalization.location.country);
