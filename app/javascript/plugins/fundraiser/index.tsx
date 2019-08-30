898d815d822034ae9a5d56e715036ae63692e2ba
subbiah <subbiah@f22labs.com>
2019-08-30 18:19:42 +0530

    resetMember functionality added for fundraiser instance

--- app/javascript/plugins/fundraiser/index.tsx
+++ app/javascript/plugins/fundraiser/index.tsx
@@ -6,6 +6,7 @@ import ComponentWrapper from '../../components/ComponentWrapper';
 import {
   changeAmount,
   changeCurrency,
+  resetMember,
   setPaymentType,
   setRecurring,
   setStoreInVault,
