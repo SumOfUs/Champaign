61bf2c78a4c3a757b7f4ebe8211c399edfa0a703
Shivashankar <shiva@sumofus.org>
2019-09-24 19:22:16 +0530

    Added source params

--- app/services/manage_donation.rb
+++ app/services/manage_donation.rb
@@ -2,6 +2,15 @@
 
 class ManageDonation
   def self.create(params:, extra_params: {})
+    page = Page.find(params[:page_id])
+    akit_donation_page_id = page.ak_donation_resource_uri.to_s.split('/').last
+
+    source = params[:source] || 'website'
+    if page.donation_followup?
+      source = "post-action-#{akit_donation_page_id}-#{source}"
+      params[:source] = source
+    end
+
     ManageAction.create(params, extra_params: { donation: true }.merge(extra_params.clone))
   end
 end
