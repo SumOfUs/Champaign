afe3877ae19c03074be23876cb38152a877766cb
Shivashankar <shiva@sumofus.org>
2019-10-16 17:09:12 +0530

    Show Direct debit option for all possible countries in Ameria, Australia and Europe

--- app/javascript/util/directDebitDecider.js
+++ app/javascript/util/directDebitDecider.js
@@ -1,14 +1,36 @@
 import { isObject, isMatch, find } from 'lodash';
 
 const SUPPORTED_COUNTRIES = [
-  { country: 'GB', recurring: true },
-  { country: 'NL', recurring: true },
-  { country: 'FR', recurring: true },
+  { country: 'GB' },
+  { country: 'NL' },
+  { country: 'FR' },
   { country: 'DE' },
   { country: 'AT' },
   { country: 'ES' },
   { country: 'AU' },
+  { country: 'BE' },
+  { country: 'CY' },
+  { country: 'EE' },
+  { country: 'IE' },
+  { country: 'IT' },
+  { country: 'LV' },
+  { country: 'LU' },
+  { country: 'MC' },
+  { country: 'PT' },
+  { country: 'SM' },
+  { country: 'SI' },
+  { country: 'CA' },
+  { country: 'US' },
 ];
+
+// countries not added in the list
+// as Gocardless mention the countries
+// have huge payment failure rate
+
+// { country: 'FI' },
+// { country: 'GR' },
+// { country: 'LT' },
+// { country: 'SK' }
 
 export function isDirectDebitSupported(data) {
   return isObject(find(SUPPORTED_COUNTRIES, filter => isMatch(data, filter)));
