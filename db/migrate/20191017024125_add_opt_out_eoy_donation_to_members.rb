22bfe12396cc6eb19a1a7d6ec4665af24595a53e
Shivashankar <shiva@sumofus.org>
2019-10-22 21:15:05 +0530

    Removed opt_out_eoy_donation index and its respective migrations for members

--- db/migrate/20191017024125_add_opt_out_eoy_donation_to_members.rb
+++ db/migrate/20191017024125_add_opt_out_eoy_donation_to_members.rb
@@ -1,6 +1,5 @@
 class AddOptOutEoyDonationToMembers < ActiveRecord::Migration[5.2]
   def change
     add_column :members, :opt_out_eoy_donation, :integer, default: 0
-    add_index :members, :opt_out_eoy_donation
   end
 end
--- db/migrate/20191021161824_drop_index_opt_out_eoy_donation.rb
+++
@@ -1,9 +0,0 @@
-class DropIndexOptOutEoyDonation < ActiveRecord::Migration[5.2]
-  def up
-    remove_index :members, :opt_out_eoy_donation
-  end
-
-  def down
-    add_index :members, :opt_out_eoy_donation
-  end
-end
