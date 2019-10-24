9d94c8f350dea06fa22f8f21f2eda8deaf46501b
Tuuli <tuuli.pollanen@gmail.com>
2019-10-24 11:11:06 +0200

    Limit use of calling tool to members with a valid AKID.

--- app/controllers/api/calls_controller.rb
+++ app/controllers/api/calls_controller.rb
@@ -4,27 +4,42 @@ class Api::CallsController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
 
   def create
-    success = verify_recaptcha(action: params[:recaptcha_action], minimum_score: 0.5)
+    verified_member = verify_member
+    unless verified_member[:success?]
+      render(json: { errors: verified_member[:errors], name: 'call' }, status: :unprocessable_entity) && return
+    end
 
-    if success
-      service = CallCreator.new(call_params, tracking_params)
-
-      if service.run
-        head :no_content
-      else
-        render json: { errors: service.errors, name: 'call' }, status: :unprocessable_entity
-      end
-    else
+    unless verify_recaptcha(action: params[:recaptcha_action], minimum_score: 0.5)
       error = [
         {
           recaptcha: I18n.t('call_tool.errors.recaptcha_fail')
         }
       ]
-      render json: { errors: error, name: 'call' }, status: :unprocessable_entity
+      render(json: { errors: error, name: 'call' }, status: :unprocessable_entity) && return
+    end
+
+    service = CallCreator.new(call_params, tracking_params)
+
+    if service.run
+      head :no_content
+    else
+      render json: { errors: service.errors, name: 'call' }, status: :unprocessable_entity
     end
   end
 
   private
+
+  def verify_member
+    if params[:akid].blank? || Member.find_by_akid(params[:akid]).blank?
+      return { success?: false, errors: [
+        {
+          akid: 'Sorry, but for safety reasons, the calling tool is limited to recognized '\
+          'members who enter the website through a campaign email.'
+        }
+      ] }
+    end
+    { success?: true, errors: [] }
+  end
 
   def call_params
     params.require(:call)
--- spec/requests/api/calls_spec.rb
+++ spec/requests/api/calls_spec.rb
@@ -13,54 +13,54 @@ describe 'API::Calls' do
     let(:target) { Plugins::CallTool.find_by_page_id(page.id).targets.sample }
 
     context 'given valid params' do
-      let(:params) do
-        {
-          call: {
-            member_phone_number: '+1 343-700-3482',
-            target_id: target.id
-          }
-        }
-      end
-
-      it 'returns successfully' do
-        post "/api/pages/#{page.id}/call", params: params
-        expect(response).to have_http_status(:no_content)
-      end
-
-      it 'creates a call' do
-        expect do
-          post "/api/pages/#{page.id}/call", params: params
-        end.to change(Call, :count).by(1)
-
-        call = Call.last
-        expect(call.page_id).to eq(page.id)
-        expect(call.member_phone_number).to eq('13437003482')
-        expect(call.target).to eq target
-      end
-
-      it 'creates a call on Twilio' do
-        calls = double
-        allow(Twilio::REST::Client).to receive_message_chain(:new, :calls).and_return(calls)
-        expect(calls)
-          .to receive(:create)
-          .with(hash_including(from: call_tool.caller_phone_number.number,
-                               to: '13437003482',
-                               url: %r{/twilio/calls/\d+/start}))
-
-        post "/api/pages/#{page.id}/call", params: params
-      end
-
       context 'given a valid akid is passed' do
-        let(:params_with_akid) { params.merge(akid: '1234.5678.tKK7gX') }
+        let(:params) do
+          {
+            call: {
+              member_phone_number: '+1 343-700-3482',
+              target_id: target.id
+            },
+            akid: '1234.5678.tKK7gX'
+          }
+        end
+
+        it 'returns successfully' do
+          post "/api/pages/#{page.id}/call", params: params
+          expect(response).to have_http_status(:no_content)
+        end
+
+        it 'creates a call' do
+          expect do
+            post "/api/pages/#{page.id}/call", params: params
+          end.to change(Call, :count).by(1)
+
+          call = Call.last
+          expect(call.page_id).to eq(page.id)
+          expect(call.member_phone_number).to eq('13437003482')
+          expect(call.target).to eq target
+        end
+
+        it 'creates a call on Twilio' do
+          calls = double
+          allow(Twilio::REST::Client).to receive_message_chain(:new, :calls).and_return(calls)
+          expect(calls)
+            .to receive(:create)
+            .with(hash_including(from: call_tool.caller_phone_number.number,
+                                 to: '13437003482',
+                                 url: %r{/twilio/calls/\d+/start}))
+
+          post "/api/pages/#{page.id}/call", params: params
+        end
+
         let!(:member) { create(:member, actionkit_user_id: '5678') }
 
         it 'returns successfully' do
-          post "/api/pages/#{page.id}/call", params: params_with_akid
+          post "/api/pages/#{page.id}/call", params: params
           expect(response).to have_http_status(:no_content)
         end
 
         it 'creates a call and action assigning the recognized member' do
-          post "/api/pages/#{page.id}/call", params: params_with_akid
+          post "/api/pages/#{page.id}/call", params: params
           call = Call.last
           expect(call.member).to eq(member)
           expect(call.action.member).to eq(member)
@@ -69,23 +69,43 @@ describe 'API::Calls' do
     end
 
     context 'given invalid params' do
-      let(:params) do
-        {
-          call: {
-            member_phone_number: 'wrong number',
-            target_id: target.id
+      context 'with akid' do
+        let(:params) do
+          {
+            call: {
+              member_phone_number: 'wrong number',
+              target_id: target.id
+            },
+            akid: '1234.5678.tKK7gX'
           }
-        }
+        end
+
+        it 'returns 422 Unprocessable Entity' do
+          post "/api/pages/#{page.id}/call", params: params
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'returns the error messages' do
+          post "/api/pages/#{page.id}/call", params: params
+          expect(response_json['errors']).to be_present
+        end
       end
 
-      it 'returns 422 Unprocessable Entity' do
-        post "/api/pages/#{page.id}/call", params: params
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
+      context 'without akid' do
+        let(:params) do
+          {
+            call: {
+              member_phone_number: '+1 343-700-3482',
+              target_id: target.id
+            }
+          }
+        end
 
-      it 'returns the error messages' do
-        post "/api/pages/#{page.id}/call", params: params
-        expect(response_json['errors']).to be_present
+        it 'returns an error message saying that the use of the tool is limited' do
+          post "/api/pages/#{page.id}/call", params: params
+          expect(response_json['errors'].to_s)
+            .to include 'limited to recognized members who enter the website through a campaign email'
+        end
       end
     end
   end
