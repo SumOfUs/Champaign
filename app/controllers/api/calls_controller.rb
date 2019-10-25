10cfe98c428f427cecdbbc0964e82cbfeca15ac7
Tuuli <tuuli.pollanen@gmail.com>
2019-10-25 09:35:09 +0200

    Add translations

--- app/controllers/api/calls_controller.rb
+++ app/controllers/api/calls_controller.rb
@@ -4,15 +4,15 @@ class Api::CallsController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
 
   def create
-    verified_member = verify_member
-    unless verified_member[:success?]
-      render(json: { errors: verified_member[:errors], name: 'call' }, status: :unprocessable_entity) && return
+    member = Member.find_by_akid(params[:akid])
+    if member.blank?
+      error = [{ akid: I18n.t('call_tool.errors.akid') }]
+      render(json: { errors: error, name: 'call' }, status: 403) && return
     end
 
-    if call_spammer(verified_member[:member], Page.find(params[:page_id]))
+    if call_spammer(member, Page.find(params[:page_id]))
       error = [
-        { akid: 'Thank you for making a difference! Unfortunately, to prevent fraud, we only allow five calls '\
-        'per member on the same page.' }
+        { akid: I18n.t('call_tool.errors.too_many_calls') }
       ]
       render(json: { errors: error, name: 'call' }, status: 403) && return
     end
@@ -40,19 +40,6 @@ class Api::CallsController < ApplicationController
   def call_spammer(member, page)
     # checks whether the member has more than five successful call actions on the page
     Call.where(member_id: member.id, page_id: page.id).not_failed.count >= 5
-  end
-
-  def verify_member
-    member = Member.find_by_akid(params[:akid])
-    if params[:akid].blank? || member.blank?
-      return { success?: false, errors: [
-        {
-          akid: 'Sorry, but for safety reasons, the calling tool is limited to recognized '\
-          'members who enter the website through a campaign email.'
-        }
-      ] }
-    end
-    { success?: true, errors: [], member: member }
   end
 
   def call_params
