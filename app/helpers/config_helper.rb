0e1b1b9f78c743eca8a23726be20d6cc7752fca2
Omar Sahyoun <osahyoun@gmail.com>
2019-10-29 16:37:40 +0000

    Adds reCAPTCHA_V2 to call tool

--- app/helpers/config_helper.rb
+++ app/helpers/config_helper.rb
@@ -6,7 +6,8 @@ module ConfigHelper
       env: Rails.env.to_s,
       default_currency: Settings.default_currency,
       facebook: Settings.facebook.to_hash.slice(:pixel_id),
-      recaptcha3: Settings.recaptcha3.to_hash.slice(:site_key)
+      recaptcha3: Settings.recaptcha3.to_hash.slice(:site_key),
+      recaptcha2: Settings.recaptcha2.to_hash.slice(:site_key)
     }.deep_transform_keys! do |key|
       key.to_s.camelize(:lower)
     end
