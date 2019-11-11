c83347f8617cbea13d8c2094fc1b4f23d01dfad1
Tuuli <tuuli.pollanen@gmail.com>
2019-11-11 11:07:47 +0100

    Clean up

--- app/helpers/config_helper.rb
+++ app/helpers/config_helper.rb
@@ -8,7 +8,6 @@ module ConfigHelper
       facebook: Settings.facebook.to_hash.slice(:pixel_id),
       recaptcha3: Settings.recaptcha3.to_hash.slice(:site_key),
       recaptcha2: Settings.recaptcha2.to_hash.slice(:site_key),
-      # this you can get from window.champaign.configuration
       eoy_thermometer: eoy_thermometer_config
     }.transform_keys! do |key|
       key.to_s.camelize(:lower)
@@ -16,7 +15,6 @@ module ConfigHelper
   end
 
   def eoy_thermometer_config
-    # needs to have goals (hash with currencies as keys), percentage, total_donations (hash with currencies as keys)
     start_date = Date.new(2019, 11, 1)
     end_date = Date.new(2019, 12, 31)
     # end of year goal in cents
@@ -30,7 +28,6 @@ module ConfigHelper
         total_donations: total_donations,
         goals: goals,
         offset: 0,
-        # TODO: set title
         title: '',
         percentage: goals[:USD].zero? ? 0 : (total_donations[:USD] / goals[:USD] * 100)
       }
