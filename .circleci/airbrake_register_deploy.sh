5973b95fbee0b91cc96f0d77feeda828b4082d93
Vince Martínez <eyko@users.noreply.github.com>
2019-11-20 20:01:13 +0000

    Improve AWS artifact/config labels and filenames
    AWS ElasticBeanstalk config archive now uses a better naming pattern,
    making it easier to identify which artifacts were deployed to what
    environment. The build now uses `git describe --tags` to create a
    version number, and appends the app name (champaign) and the branch name
    to the label. The final label and archive name looks something like:
    
        file:  champaign-master-v3.0-1-g9de34f90-config.zip
        label: champaign-master-v3.0-1-g9de34f90
    
    Broken down in parts:
    
        {app_name}-{branch_name}-{last_git_tag}-{commits_since_last_tag}-{short_git_hash}

--- .circleci/airbrake_register_deploy.sh
+++ .circleci/airbrake_register_deploy.sh
@@ -3,11 +3,12 @@
 ENVIRONMENT=production
 REPOSITORY=https://github.com/SumOfUs/Champaign
 USERNAME=CircleCI
+REVISION=$(git describe --tags)
 
 echo "Registering deploy with Airbrake"
 echo "Airbrake Project ID: ${AIRBRAKE_PROJECT_ID}"
 
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d '{"environment":"'${ENVIRONMENT}'","username":"'${USERNAME}'","repository":"'${REPOSITORY}'","revision":"'${CIRCLE_SHA1}'"}' \
+  -d '{"environment":"'${ENVIRONMENT}'","username":"'${USERNAME}'","repository":"'${REPOSITORY}'","revision":"'${REVISION}'"}' \
   "https://airbrake.io/api/v4/projects/${AIRBRAKE_PROJECT_ID}/deploys?key=${AIRBRAKE_PROJECT_KEY}"
