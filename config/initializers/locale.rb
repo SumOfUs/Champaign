1ecf5be97007422aad1432faf56f44253de86c23
Vince Martínez <eyko@users.noreply.github.com>
2019-11-20 18:28:08 +0000

    Deprecate external assets
    External assets have been moved to the `vendor/theme` folder, with
    a few changes:
    
    * stylesheets/* and images/* are now included individually, so paths
      don't need to be prepended with `images/` or `stylesheets/` anymore.
    * There was some unnecessary logic around loading translations for
      tests that has now been removed: tests have been updated to expect
      the right translations, which makes tests "easier to reason about"™

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
-# include the locale files from external asset directories, if relevant
+# include the locale files from theme directory
 Rails.application.config.after_initialize do
-  if Settings.external_assets_path.present? && Settings.external_translation_path.present?
-    Settings.external_assets_path.try(:split, ':').each do |directory|
-      files = Dir[File.join(directory, Settings.external_translation_path, '*.{rb,yml}')]
-      I18n.load_path += files
-    end
-    # in the test environement, we want the translations from the Champaign repo to override those in
-    # the asset repo because some of the integration specs involve VCR casettes including language
-    # from the Champaign translations
-    if Rails.env.test?
-      champaign_locales = Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
-      I18n.load_path += champaign_locales
-    end
-  end
+  I18n.load_path += Dir[File.join(Rails.root, 'vendor', 'theme', 'translations', '*.{rb,yml}')]
 end
