b5470ba620db89a5d408b343522176d21bd8ffed
Vince Martínez <eyko@users.noreply.github.com>
2019-12-04 11:34:52 +0000

    Try using our own connected thermometer

--- app/javascript/modules/eoy-thermometer/index.tsx
+++ app/javascript/modules/eoy-thermometer/index.tsx
@@ -1,5 +1,12 @@
+import { connect } from 'react-redux';
 import { Thermometer } from '../../components/Thermometer';
 import DonationsThermometer from '../../plugins/donations_thermometer';
+import { IAppState } from '../../types';
+
+const ConnectedThermometer = connect((state: IAppState, ownProps) => ({
+  ...ownProps,
+  currency: state.fundraiser.currency,
+}))(Thermometer);
 
 export const init = options => {
   const element = document.getElementById('eoy-thermometer');
@@ -10,9 +17,10 @@ export const init = options => {
   return new DonationsThermometer({
     // I have to pass both config and props. Config is specified in the plugin interface, and the logic for the
     // component itself requires either store or props.
-    // component: Thermometer,
-    config: options,
+    component: ConnectedThermometer,
+    store: window.champaign.store,
     props: options,
+    config: options,
     el: element,
   });
 };
