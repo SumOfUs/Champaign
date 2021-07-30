4dec3aced3be80e5982b78566044e0ce7c18d7d0
Omar Sahyoun <omar@sumofus.org>
2021-07-30 11:55:23 +0100

    Integrating nl locale

--- app/javascript/components/ComponentWrapper.js
+++ app/javascript/components/ComponentWrapper.js
@@ -7,6 +7,7 @@ import deLocaleData from 'react-intl/locale-data/de';
 import frLocaleData from 'react-intl/locale-data/fr';
 import esLocaleData from 'react-intl/locale-data/es';
 import ptLocaleData from 'react-intl/locale-data/pt';
+import nlLocaleData from 'react-intl/locale-data/nl';
 import loadTranslations from '../util/TranslationsLoader';
 
 function WrapInStore(options) {
@@ -32,6 +33,7 @@ export default class ComponentWrapper extends Component {
   }
 
   render() {
+    console.log(nlLocaleData);
     return (
       <IntlProvider
         locale={this.props.locale}
@@ -51,4 +53,5 @@ addLocaleData([
   ...frLocaleData,
   ...esLocaleData,
   ...ptLocaleData,
+  ...nlLocaleData,
 ]);
