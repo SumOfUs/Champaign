009e1df55026772164ff702d67f5fb035defaee3
Omar Sahyoun <omar@sumofus.org>
2021-09-03 17:24:34 +0100

    Route for payment methods

--- config/routes.rb
+++ config/routes.rb
@@ -229,6 +229,7 @@ Rails.application.routes.draw do
 
     resources :members, except: [:show]
     get 'members/payment_methods', to: 'members#payment_methods'
+    get 'members/set_payment_methods', to: 'members#set_payment_methods'
     get 'members/:id', to: 'members#show', constraints: { id: %r{[^/]+} }
 
     post 'members/forget', to: 'members#forget'
