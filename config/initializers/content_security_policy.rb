a167ccb7244355bd09cd4b410856572056c74ddb
Subbiah <49471498+subbiahf22@users.noreply.github.com>
2022-04-13 14:49:01 +0530

    Updated the code to support iframe on partner org (#1920)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,3 @@
 Rails.application.config.content_security_policy do |policy|
-  policy.frame_ancestors :self, 'pronto.sumofus.org'
+  policy.frame_ancestors :self, 'pronto.sumofus.org', 'apiboficial.org'
 end
