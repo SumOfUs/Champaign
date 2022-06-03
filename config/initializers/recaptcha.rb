8d68eab44493fb0c09de03b58777b6edf644d13d
yeseniamolinab <yesi@sumofus.org>
2022-06-03 16:41:37 -0600

    do not fetch secrets for test and development envs

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 require './app/lib/secrets_manager'
-recaptcha_secrets = SecretsManager.get_value('recaptchaThree')
+recaptcha_secrets = SecretsManager.get_value('recaptchaThree') if Rails.env == 'production'
 
 Recaptcha.configure do |config|
-  config.site_key = recaptcha_secrets['siteKey']
-  config.secret_key = recaptcha_secrets['secretKey']
+  config.site_key = recaptcha_secrets.nil? ? Settings.recaptcha3.site_key : recaptcha_secrets['siteKey']
+  config.secret_key = recaptcha_secrets.nil? ? Settings.recaptcha3.secret_key : recaptcha_secrets['secretKey']
 end
