5443818f2c96e04914d7e12ebd0483a93cffba13
yeseniamolinab <yesi@sumofus.org>
2022-06-09 13:19:22 -0600

    pr feedback

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-require './app/lib/secrets_manager'
-braintree_secrets = SecretsManager.get_value('braintree') if Rails.env == 'production'
-
 Braintree::Configuration.environment =  (Settings.braintree.environment || :sandbox).to_sym
 Braintree::Configuration.logger      =  Logger.new('log/braintree.log')
-Braintree::Configuration.merchant_id =  braintree_secrets.nil? ? Settings.braintree.merchant_id : braintree_secrets['merchantId'] # rubocop:disable Metrics/LineLength
-Braintree::Configuration.public_key  =  braintree_secrets.nil? ? Settings.braintree.public_key : braintree_secrets['publicKey'] # rubocop:disable Metrics/LineLength
-Braintree::Configuration.private_key =  braintree_secrets.nil? ? Settings.braintree.private_key : braintree_secrets['privateKey'] # rubocop:disable Metrics/LineLength
+Braintree::Configuration.merchant_id =  Settings.braintree.merchant_id
+Braintree::Configuration.public_key  =  Settings.braintree.public_key
+Braintree::Configuration.private_key =  Settings.braintree.private_key
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,4 @@
-require './app/lib/secrets_manager'
-recaptcha_secrets = SecretsManager.get_value('recaptchaThree') if Rails.env == 'production'
-
 Recaptcha.configure do |config|
-  config.site_key = recaptcha_secrets.nil? ? Settings.recaptcha3.site_key : recaptcha_secrets['siteKey']
-  config.secret_key = recaptcha_secrets.nil? ? Settings.recaptcha3.secret_key : recaptcha_secrets['secretKey']
+  config.site_key = Settings.recaptcha3.site_key
+  config.secret_key = Settings.recaptcha3.secret_key
 end
