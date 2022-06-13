fbce58e5ca009c9f42c37fa224c9438bb5f7c51c
yeseniamolinab <yesi@sumofus.org>
2022-06-13 17:02:19 -0600

    Undo debugging changes

--- app/lib/secrets_manager.rb
+++ app/lib/secrets_manager.rb
@@ -10,6 +10,8 @@ module SecretsManager
       JSON.parse(secrets_manager.get_secret_value(
         secret_id: secret_name(secret_id)
       ).secret_string)
+    rescue StandardError
+      {}
     end
 
     private
